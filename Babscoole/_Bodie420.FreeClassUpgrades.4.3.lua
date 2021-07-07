c95de79a2cb60befe25ca17c1038853974588169
Babscoole <82686995+Babscoole@users.noreply.github.com>
2021-07-07 11:44:43 -0600

    Babscoole's mod scripts

---
+++ Babscoole/_Bodie420.FreeClassUpgrades.4.3.lua
@@ -0,0 +1,147 @@
+COST_MULTIPLIER = "0"
+
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "_Bodie420.FreeClassUpgrades.4.3.pak",
+["MOD_DESCRIPTION"]			= "",
+["MOD_AUTHOR"]				= "Bodie420",
+["LUA_AUTHOR"]				= "Babscoole",
+["NMS_VERSION"]				= "4.3",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "METADATA\REALITY\TABLES\COSTTABLE.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_STATIONKEY",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_SUIT_SLOT",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_PET_CUSTOMISE",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_PET_SLOT",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_PET_ADOPT",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_PET_EGG_READY",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_PET_MILK",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_PET_RIDE",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_PET_TREAT",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_LIB_VAULT_FC",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_LIB_VAULT_FU",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_LIB_VAULT_FR",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_LIB_VAULT_TC",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_WEAP_UPGRADE",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","C_REPAIRPART",}, 
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DontCharge",			"True"}, 	-- Original "False"									
+							}
+						},							
+						{
+							["PRECEDING_KEY_WORDS"] = "Costs",
+							["MATH_OPERATION"] 		= "*",
+							["REPLACE_TYPE"] 		= "ALL",
+							["VALUE_MATCH"] 		= "",
+							["VALUE_MATCH_TYPE"] 	= "",
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"IGNORE",	COST_MULTIPLIER }
+							}
+						},
+												{
+							["SPECIAL_KEY_WORDS"] = {"Id","NPC_BUILD1",}, 							
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"CannotAffordOSDMsg",			"You Broke Fool!"}, 	-- Original ""									
+							}
+						},	
+					}
+				}
+			}
+		}
+	}	
+}
---
+++ Babscoole/_Bodie420.PlayerGlobals.4.3.lua
@@ -0,0 +1,75 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "_Bodie420.PlayerGlobals.4.3.pak",
+["MOD_DESCRIPTION"]			= "",
+["MOD_AUTHOR"]				= "Bodie420",
+["LUA_AUTHOR"]				= "Babscoole",
+["NMS_VERSION"]				= "4.3",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "GCPLAYERGLOBALS.GLOBAL.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["INTEGER_TO_FLOAT"] = "FORCE",							
+							["VALUE_CHANGE_TABLE"] 	= 	
+							{
+								{"FingerButtonClickTime",  "0.02"},  --Original 0.2
+								{"HandInteractionLightTime",  "0.03"},  --Original 0.3
+								{"DoPlayerAppearInVehicleEffect",  "True"},  --Original False
+								{"HolsterOnHip",  "True"},  --Original False
+								{"GroundWalkSpeed",  "8.8"},  --Original 4.4
+								{"GroundRunSpeed",  "16"},  --Original 8
+								{"GroundWalkSpeedLowG",  "3.2"},  --Original 1.6
+								{"GroundRunSpeedLowG",  "7"},  --Original 3.5
+								{"JetpackUnderwaterDrainRate",  "0.00004"},  --Original 0.4
+								{"JetpackUnderwaterFillRate",  "4"},  --Original 1
+								{"UnderwaterMaxSpeed",  "8"},  --Original 4
+								{"UnderwaterMaxJetpackSpeed",  "16"},  --Original 8
+								{"FreeJetpackRange",  "90"},  --Original 3
+								{"FreeJetpackRangePrime",  "99"},  --Original 5
+								{"RocketBootsBoostTankDrainSpeed",  "0.0000006"},  --Original 6
+								{"RocketBootsDriftTankDrainSpeed",  "0.000001"},  --Original 1
+								{"JetpackMinLevel",  "-0.5"},  --Original 0.5
+								{"JetpackMaxSpeed",  "10"},  --Original 5
+								{"SpaceJetpackDrainRate",  "0.000003"},  --Original 0.3
+								{"UseEnergy",  "False"},  --Original True
+								{"InventoryDamage",  "False"},  --Original True
+								{"ObjectScanTime",  "0.01"},  --Original 3
+								{"MeleeStaminaDrain",  "0.0002"},  --Original 0.2
+							}
+						},
+						{
+							["PRECEDING_KEY_WORDS"] = {"JetpackTankTimes"},
+							["LINE_OFFSET"] 	= "1",
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{ "IGNORE", 					"99" }, 		--Original "4"
+							}	
+						},						
+						{
+							["PRECEDING_KEY_WORDS"] = {"JetpackTankTimes"},
+							["LINE_OFFSET"] 	= "2",
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{ "IGNORE", 					"99" }, 		--Original "6"
+							}	
+						},						
+						{
+							["PRECEDING_KEY_WORDS"] = {"JetpackTankTimes"},
+							["LINE_OFFSET"] 	= "3",
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{ "IGNORE", 					"99" }, 		--Original "8"
+							}	
+						},						
+					}
+				}
+			}
+		}
+	}	
+}
