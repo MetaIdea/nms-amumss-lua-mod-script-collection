fe584cdd3ac5defa8a70d2817fff7420f8d192fb
Babscoole <82686995+Babscoole@users.noreply.github.com>
2021-07-11 13:39:05 -0600

    Saa044's's mod scripts

---
+++ Saa044/_CSO v3.3 [No Metrics Lines].lua
@@ -0,0 +1,33 @@
+
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+	["MOD_FILENAME"]	= "_CSO v3.3 [No Metrics Lines].pak",
+	["MOD_DESCRIPTION"]	= "No more white lines in front of the ship at pulse speed.",
+	["MOD_AUTHOR"]		= "NeptuneX3",
+	["NMS_VERSION"]		= "3.10",		-- NMS version when first scripted
+	["MODIFICATIONS"]	= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] = 
+			{
+			
+
+				{
+					["MBIN_FILE_SOURCE"] = "GCSPACESHIPGLOBALS.GLOBAL.MBIN",
+					["EXML_CHANGE_TABLE"] = 
+					{
+						{
+							["VALUE_CHANGE_TABLE"] = 
+							{
+								{"MiniWarpLinesNum", 0},
+							},
+
+						},
+					},
+				},
+				
+				
+			},
+		},
+	},
+}
