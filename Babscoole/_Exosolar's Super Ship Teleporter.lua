64d13217182a5c5254be67e1ec2810d37ec9602e
Babscoole <82686995+Babscoole@users.noreply.github.com>
2021-08-26 06:36:09 -0600

    Babscoole's mod scripts

---
+++ Babscoole/_Exosolar's Super Ship Teleporter.lua
@@ -0,0 +1,29 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "_Exosolar's Super Ship Teleporter.pak", 
+["MOD_AUTHOR"]				= "Exosolar",
+["LUA_AUTHOR"]				= "Babscoole",
+["NMS_VERSION"]				= "3.53",
+["MOD_DESCRIPTION"]			= "",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "METADATA\REALITY\TABLES\NMS_REALITY_GCTECHNOLOGYTABLE.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["SPECIAL_KEY_WORDS"] = {"ID", "SHIP_TELEPORT"},
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"Bonus",	"1005"}
+							}
+						}
+					}
+				}	
+			}
+		}
+	}	
+}
