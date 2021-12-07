0569d46ad39a8bffdb476413f499bf336c18de62
Babscoole <82686995+Babscoole@users.noreply.github.com>
2021-12-07 00:04:50 -0700

    +

---
+++ Babscoole/DUD Personal Mods/_MOD_DUD_Flashlight_v3701.lua
@@ -0,0 +1,33 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "_MOD_DUD_Flashlight_v3701.pak",
+["MOD_AUTHOR"]				= "jasondude7116",
+["LUA_AUTHOR"]				= "Babscoole",
+["NMS_VERSION"]				= "3.75",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "GCGAMEPLAYGLOBALS.GLOBAL.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["INTEGER_TO_FLOAT"] = "FORCE",	
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"SpaceSpeedReadoutMultiplier",	"1"},
+								{"TorchFoV",					"50"},
+								{"TorchStrength",				"8"},
+								{"TorchDimFoV",					"25"},
+								{"UndergroundTorchFoV",			"50"},
+								{"UndergroundTorchStrength",	"8"},						
+							}
+						},						
+					},
+				},								
+			},
+		},
+	},	
+}
