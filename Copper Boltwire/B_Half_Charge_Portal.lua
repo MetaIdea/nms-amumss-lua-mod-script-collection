d0e70e384ceedac65a77ff0c1b60f885def5f87b
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-01-23 14:44:38 -0700

    +

---
+++ Copper Boltwire/B_Half_Charge_Portal.lua
@@ -0,0 +1,35 @@
+
+--[[ Summery Description:
+This will reduce the cost of portals by half...
+]]--
+
+
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+    ["MOD_FILENAME"]    = "B_Half_Charge_Portal.pak",
+    ["MOD_AUTHOR"]      = "Lenni", -- Altered by Copper B.
+    ["NMS_VERSION"]     = "3.75",
+    ["MODIFICATIONS"]   =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            { 
+                {
+                    ["MBIN_FILE_SOURCE"]    = {"MODELS/PLANETS/BIOMES/COMMON/BUILDINGS/PORTAL/PORTAL/ENTITIES/BUTTON.ENTITY.MBIN",},
+                    ["EXML_CHANGE_TABLE"]   =
+                    {
+                        {
+                            ["REPLACE_TYPE"]        = "ALL",
+                            ["VALUE_CHANGE_TABLE"]  = 
+                            {               -- Presumably, this will reduce the cost of portals by half... i don't really know if that's what this does...
+                                            -- Testing would be needed... 
+                                { "MinRandAmount",  "50" }, --Original "0"
+                                { "MaxRandAmount",  "50" }, --Original "0"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }    
+}    
