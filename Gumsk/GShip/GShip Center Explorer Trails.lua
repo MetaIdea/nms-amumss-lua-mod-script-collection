2ee4463b2f99379e332b777fcee86bc9bc4b740e
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-02-13 21:44:26 -0700

    Add Gumsk GShip Stuff

---
+++ Gumsk/GShip/GShip Center Explorer Trails.lua
@@ -0,0 +1,14 @@
+Author = "Gumsk"
+ModName = "GShip"
+ModNameSub = "Center Explorer Trails"
+BaseDescription = "Centers the trails on explorer engines"
+GameVersion = "353"
+ModVersion = "a"
+
+NMS_MOD_DEFINITION_CONTAINER = {
+["MOD_FILENAME"]	= ModName.." "..ModNameSub.." "..GameVersion..ModVersion..".pak",
+["MOD_DESCRIPTION"]	= BaseDescription,
+["MOD_AUTHOR"]		= Author,
+["NMS_VERSION"]		= GameVersion,
+["MODIFICATIONS"]	= {
+}}
