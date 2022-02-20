1ddb35c4f894aaf636a2e3a06669a06b82e8a4cb
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-02-20 13:15:53 -0700

    Updated lMonk Hide or Fade Notifications

--- lMonk/_Published/UI Notifications Hide.lua
+++ lMonk/_Published/UI Notifications Hide.lua
@@ -1,17 +1,15 @@
----------------------------------------------
-local desc = [[
-  Hide lower-right HUD notification popups
-]]-------------------------------------------
-
+--[[┎──────────────────────────────────────────────────────────
+	┃ Hide lower-right HUD notification popups
+────┸──────────────────────────────────────────────────────--]]
 NMS_MOD_DEFINITION_CONTAINER = {
 	MOD_FILENAME 		= '_MOD.lMonk.Hide notifications.pak',
 	MOD_AUTHOR			= 'lMonk',
-	NMS_VERSION			= 3.75,
-	MOD_DESCRIPTION		= desc,
+	NMS_VERSION			= '3.62',
+	MOD_DESCRIPTION		= 'Hide lower-right HUD notification popups',
 	MODIFICATIONS 		= {{
 	MBIN_CHANGE_TABLE 	= {
 	{
-		MBIN_FILE_SOURCE	= 'UI/HUD/HUDNOTIFICATIONPANEL.MBIN',
+		MBIN_FILE_SOURCE	= 'UI\HUD\HUDNOTIFICATIONPANEL.MBIN',
 		EXML_CHANGE_TABLE	= {
 			{
 				SPECIAL_KEY_WORDS	= {'ID', 'TITLE'},
