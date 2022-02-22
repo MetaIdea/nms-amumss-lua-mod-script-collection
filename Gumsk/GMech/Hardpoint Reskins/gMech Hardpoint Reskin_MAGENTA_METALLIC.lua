21a02fd3e992c85a00b06e55bed41e411b4af853
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-02-21 21:34:41 -0700

    Gumsk GMech HardPointReskins

---
+++ Gumsk/GMech/Hardpoint Reskins/gMech Hardpoint Reskin_MAGENTA_METALLIC.lua
@@ -0,0 +1,38 @@
+Color = "MAGENTA"
+-- RedAmount = "1.0"
+-- GreenAmount = "0.5"
+-- BlueAmount = "0"
+
+
+Author = "Gumsk"
+ModName = "gMech Hardpoint Reskin"
+GameVersion = "382"
+ModVersion = "a"
+ModNameSub = Color.." METALLIC"
+BaseDescription = "Changes color of hardpoint exomech parts to "..Color
+
+-- FileSource1 = "MODELS\COMMON\ROBOTS\FRIENDLYDRONE\FRIENDLYDRONE_MAT.MATERIAL.MBIN"
+
+NMS_MOD_DEFINITION_CONTAINER = {
+	["MOD_FILENAME"]	= ModName.." "..ModNameSub.." "..GameVersion..ModVersion..".pak",
+	["MOD_DESCRIPTION"]	= BaseDescription,
+	["MOD_AUTHOR"]		= Author,
+	["NMS_VERSION"]		= GameVersion,
+	["ADD_FILES"] = {
+		{
+			["FILE_DESTINATION"] 		= "TEXTURES\COMMON\ROBOTS\SENTINELTRIM.DDS",
+			["EXTERNAL_FILE_SOURCE"] 	= "MECHHARDPOINT_"..Color..".DDS",
+		},
+		{
+			["FILE_DESTINATION"] 		= "TEXTURES\COMMON\ROBOTS\SENTINELTRIM.MASKS.DDS",
+			["EXTERNAL_FILE_SOURCE"] 	= "SENTINELTRIM.MASKS_METALLIC.DDS",
+		},
+	},
+	["MODIFICATIONS"]	= {
+		{
+			["MBIN_CHANGE_TABLE"] = {
+
+			}
+		},
+	}
+}
