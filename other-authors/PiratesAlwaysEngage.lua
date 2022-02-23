b28c8c5a7b014e374c9daa4445107d1d42d478be
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-02-23 00:54:40 -0700

    Update PirateAlwaysEngage

--- other-authors/PiratesAlwaysEngage.lua
+++ other-authors/PiratesAlwaysEngage.lua
@@ -2,11 +2,11 @@ NMS_MOD_DEFINITION_CONTAINER =
 {
 ["MOD_FILENAME"] 			= "PiratesAlwaysEngage.pak",
 ["MOD_AUTHOR"]				= "DeathWrench",
-["NMS_VERSION"]				= "1.0",
+["NMS_VERSION"]				= "1.1",
 ["MODIFICATIONS"] 			= 
 	{
 		{
-			["PAK_FILE_SOURCE"] 	= "NMSARC.59B126E2.pak",
+			--["PAK_FILE_SOURCE"] 	= "NMSARC.59B126E2.pak",
 			["MBIN_CHANGE_TABLE"] 	= 
 			{ 
 				{
@@ -18,6 +18,8 @@ NMS_MOD_DEFINITION_CONTAINER =
 							["VALUE_CHANGE_TABLE"] 	= 
 							{
 								{"PirateInterestOverride",			"1"},		-- Original "0"
+								--{"WarpsBetweenBattles", 			"0"},			-- Number of warps since last space battle before next will occur | 5 | 605
+								--{"HoursBetweenBattles",			"0"},-- Number of hours since last space battle before next will occur | 3 | 606
 								--doesn't work{"PirateInterestLevels",			"9999999999"},	
 								--{"UseTradingCostTable",				"true"}, 	-- Original "false"	
 							}
