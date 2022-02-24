639899e3b496352a85ddd9a3aa1cd670804bf743
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-02-23 21:10:52 -0700

    2.82 updates

--- other-authors/ExosuitTech48Slots.lua
+++ other-authors/ExosuitTech48Slots.lua
@@ -1,41 +1,41 @@
-NMS_MOD_DEFINITION_CONTAINER = 
+NMS_MOD_DEFINITION_CONTAINER= 
 {
-  ["MOD_FILENAME"] 			= "ExosuitTech48Slots.pak",
-  ["MOD_DESCRIPTION"]		= "Enables the exosuit technology tab to be upgraded to 48 slots.",
-  ["MOD_AUTHOR"]			= "RealGenuineHuman",
-  ["NMS_VERSION"]			= "3.82",
-  ["MODIFICATIONS"] 		=
-	{
-		{
-			["MBIN_CHANGE_TABLE"] = 
-			{ 
-				{
-					["MBIN_FILE_SOURCE"] 	= "METADATA\GAMESTATE\DEFAULTINVENTORYBALANCE.MBIN",
-					["EXML_CHANGE_TABLE"] = 
-					{
-						{
-							["VALUE_CHANGE_TABLE"] 	= 
-							{
-								{"PlayerPersonalInventoryTechWidth",	"8"},
-                                {"PlayerPersonalInventoryTechHeight",   "6"},
-							}
-						},
-					}
-				},
-                {
-					["MBIN_FILE_SOURCE"] 	= "METADATA\GAMESTATE\DEFAULTINVENTORYBALANCESURVIVAL.MBIN",
-					["EXML_CHANGE_TABLE"] = 
-					{
-						{
-							["VALUE_CHANGE_TABLE"] 	= 
-							{
-								{"PlayerPersonalInventoryTechWidth",	"8"},
-                                {"PlayerPersonalInventoryTechHeight",   "6"},
-							}
-						},
-					}
-				},
-			}
-		},
-	}
+  ["MOD_FILENAME"]      = "ExosuitTech48Slots.pak",
+  ["MOD_DESCRIPTION"]   = "Enables the exosuit technology tab to be upgraded to 48 slots.",
+  ["MOD_AUTHOR"]        = "RealGenuineHuman",
+  ["NMS_VERSION"]       = "3.82",
+  ["MODIFICATIONS"]     =
+  {
+    {
+      ["MBIN_CHANGE_TABLE"] = 
+      { 
+        {
+          ["MBIN_FILE_SOURCE"]   = "METADATA\GAMESTATE\DEFAULTINVENTORYBALANCE.MBIN",
+          ["EXML_CHANGE_TABLE"]  = 
+          {
+            {
+              ["VALUE_CHANGE_TABLE"]	= 
+              {
+                {"PlayerPersonalInventoryTechWidth",  "8"},
+                {"PlayerPersonalInventoryTechHeight", "6"},
+              }
+            }
+          }
+        },
+        {
+          ["MBIN_FILE_SOURCE"]	= "METADATA\GAMESTATE\DEFAULTINVENTORYBALANCESURVIVAL.MBIN",
+          ["EXML_CHANGE_TABLE"] = 
+          {
+            {
+              ["VALUE_CHANGE_TABLE"]	= 
+              {
+                {"PlayerPersonalInventoryTechWidth",	"8"},
+                {"PlayerPersonalInventoryTechHeight", "6"},
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
