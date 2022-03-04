e4433b253f43414bed9f6d6970ac2aa203ba0446
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-03-04 07:28:39 -0700

    3.84 updates

--- other-authors/ExosuitTech48Slots.lua
+++ other-authors/ExosuitTech48Slots.lua
@@ -1,36 +1,29 @@
-NMS_MOD_DEFINITION_CONTAINER= 
-{
-  ["MOD_FILENAME"]      = "ExosuitTech48Slots.pak",
-  ["MOD_DESCRIPTION"]   = "Enables the exosuit technology tab to be upgraded to 48 slots.",
-  ["MOD_AUTHOR"]        = "RealGenuineHuman",
-  ["NMS_VERSION"]       = "3.82",
-  ["MODIFICATIONS"]     =
-  {
+NMS_MOD_DEFINITION_CONTAINER = {
+  ["MOD_FILENAME"]    = "ExosuitTech48Slots.pak",
+  ["MOD_DESCRIPTION"] = "Enables the exosuit technology tab to be upgraded to 48 slots.",
+  ["MOD_AUTHOR"]      = "RealGenuineHuman",
+  ["NMS_VERSION"]     = "3.84",
+  ["MODIFICATIONS"]   = {
     {
-      ["MBIN_CHANGE_TABLE"] = 
-      { 
+      ["MBIN_CHANGE_TABLE"] = {
         {
-          ["MBIN_FILE_SOURCE"]   = "METADATA\GAMESTATE\DEFAULTINVENTORYBALANCE.MBIN",
-          ["EXML_CHANGE_TABLE"]  = 
-          {
+          ["MBIN_FILE_SOURCE"]  = "METADATA/GAMESTATE/DEFAULTINVENTORYBALANCE.MBIN",
+          ["EXML_CHANGE_TABLE"] = {
             {
-              ["VALUE_CHANGE_TABLE"]	= 
-              {
-                {"PlayerPersonalInventoryTechWidth",  "8"},
-                {"PlayerPersonalInventoryTechHeight", "6"},
+              ["VALUE_CHANGE_TABLE"] = {
+                { "PlayerPersonalInventoryTechWidth", "8" },
+                { "PlayerPersonalInventoryTechHeight", "6" },
               }
             }
           }
         },
         {
-          ["MBIN_FILE_SOURCE"]	= "METADATA\GAMESTATE\DEFAULTINVENTORYBALANCESURVIVAL.MBIN",
-          ["EXML_CHANGE_TABLE"] = 
-          {
+          ["MBIN_FILE_SOURCE"] = "METADATA/GAMESTATE/DEFAULTINVENTORYBALANCESURVIVAL.MBIN",
+          ["EXML_CHANGE_TABLE"] = {
             {
-              ["VALUE_CHANGE_TABLE"]	= 
-              {
-                {"PlayerPersonalInventoryTechWidth",	"8"},
-                {"PlayerPersonalInventoryTechHeight", "6"},
+              ["VALUE_CHANGE_TABLE"] = {
+                { "PlayerPersonalInventoryTechWidth", "8" },
+                { "PlayerPersonalInventoryTechHeight", "6" },
               }
             }
           }
