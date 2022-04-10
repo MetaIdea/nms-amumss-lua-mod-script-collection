fce5592b52d69dc783024eea1d37136269f2ae6a
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-04-10 08:26:10 -0600

    Update 000CleanMultiplayer.cs

--- Jackty89/NMSMB CS Files/000CleanMultiplayer.cs
+++ Jackty89/NMSMB CS Files/000CleanMultiplayer.cs
@@ -34,7 +34,18 @@ namespace cmk.NMS.Scripts.Mod
 				"MODELS\\PLANETS\\BIOMES\\COMMON\\BUILDINGS\\PROPS\\MESSENGER\\ENTITIES\\MESSENGER.ENTITY.MBIN"
 			);
 			//removes mesh
-			mbin.Components = new();
+			
+			var interaction = CloneMbin(mbin.Components[1]);
+			var emptyNode = CloneMbin(mbin.Components[2]);
+			//interaction.ActivationCost = 1;
+			//interaction.InteractDistance = 0.01f;
+			//interaction.InteractAngle = 1;
+			interaction = new GcInteractionComponentData();
+			
+			mbin.Components.Clear();
+			mbin.Components.Add(interaction);
+			mbin.Components.Add(emptyNode);
+			mbin.Components.Add(emptyNode);
 		}
 		
 		protected void HideLines()
@@ -42,19 +53,16 @@ namespace cmk.NMS.Scripts.Mod
 			var mbin = ExtractMbin<TkSceneNodeData>(
 				"MODELS\\EFFECTS\\LINES\\LINERENDERER.SCENE.MBIN"
 			);
-			//mbin.Attributes[0].Value = "MATERIALS/LINE3D.EMPTY.MATERIAL.MBIN";			
-			//mbin.Attributes[0].Value = "";
+			mbin.Attributes[0].Value = "MATERIALS/LINE3D.EMPTY.MATERIAL.MBIN";			
 			mbin.Attributes[1].Value = "1";
 		}
 		
 		protected void Line3DEdit()
 		{
-			var mbin = ExtractMbin<TkMaterialData>(
-				"MATERIALS\\LINE3D.MATERIAL.MBIN"
-			);
+			var copyMbin = CloneMbin<TkMaterialData>("MATERIALS/LINE3D.MATERIAL.MBIN", "MATERIALS/LINE3D.EMPTY.MATERIAL.MBIN");
 			
-			mbin.Uniforms[0].Values = new();
-			mbin.Samplers = new();		
+			copyMbin.Uniforms[0].Values = new();
+			copyMbin.Samplers = new();		
 		}
 	}
 }
