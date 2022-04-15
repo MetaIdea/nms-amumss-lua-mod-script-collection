874a270eaea13b8c7e6c4b7539338a85aa5be0bc
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-04-15 06:44:11 -0600

    Create Anti-Ophidiophobia.lua

---
+++ Babscoole/Anti-Ophidiophobia.lua
@@ -0,0 +1,33 @@
+DelCreature = {"SEASNAKE","FLYINGSNAKE","SANDWORM"}
+
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "Anti-Ophidiophobia.pak",
+["MOD_AUTHOR"]				= "Babscoole",
+["NMS_VERSION"]				= "3.85",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "METADATA\SIMULATION\ECOSYSTEM\CREATUREDATATABLE.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+					
+					},
+				},								
+			},
+		},
+	},	
+}
+
+local DelCreatureTable = NMS_MOD_DEFINITION_CONTAINER["MODIFICATIONS"][1]["MBIN_CHANGE_TABLE"][1]["EXML_CHANGE_TABLE"]
+for i=1,#DelCreature do
+	local value = DelCreature[i]
+    DelCreatureTable[#DelCreatureTable +1 ] = 
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id",value},	
+							["REMOVE"] = "SECTION"
+						}					
+end
