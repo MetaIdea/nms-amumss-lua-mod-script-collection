34bfe33062517618ef2075c06e15768dbd708f21
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-04-16 12:24:29 -0600

    Add SolarShipSails

---
+++ Mjstral-aka-MetaIdea/SolarShipAlwaysFolded.lua
@@ -0,0 +1,41 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "SolarShipAlwaysFolded.pak",
+["MOD_AUTHOR"]				= "Mjstral",
+["NMS_VERSION"]				= "3.85+",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["PAK_FILE_SOURCE"] 	= "NMSARC.515F1D3.pak",
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "MODELS\COMMON\SPACECRAFT\SAILSHIP\SAILSHIPPARTS\SAILSHIPANIMS\SAILSHIP_SHARED\ENTITIES\ROOTJNT.ENTITY.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Anim","TAKEOFF"},
+							["VALUE_CHANGE_TABLE"] 	= {{"Filename", ""},}
+						},
+						-- {
+							-- ["SPECIAL_KEY_WORDS"] 	= {"Anim","LANDING"},
+							-- ["VALUE_CHANGE_TABLE"] 	= {{"Filename", ""},}
+						-- },
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Anim","LOWALTITUDE"},
+							["VALUE_CHANGE_TABLE"] 	= {{"Filename", ""},}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Anim","TURNLEFT"},
+							["VALUE_CHANGE_TABLE"] 	= {{"Filename", ""},}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Anim","TURNRIGHT"},
+							["VALUE_CHANGE_TABLE"] 	= {{"Filename", ""},}
+						},
+					}
+				}
+			}
+		}
+	}	
+}
