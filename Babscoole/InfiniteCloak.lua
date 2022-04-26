17e282a6ae2398256fe56b840d7fc1808e2b54b0
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-04-25 23:26:28 -0600

    Create InfiniteCloak.lua

---
+++ Babscoole/InfiniteCloak.lua
@@ -0,0 +1,31 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "InfiniteCloak.pak",
+["MOD_DESCRIPTION"]			= "",
+["MOD_AUTHOR"]				= "RedFalcons-MDX",
+["LUA_AUTHOR"]				= "Babscoole",
+["NMS_VERSION"]				= "3.88",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "GCPLAYERGLOBALS.GLOBAL.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["INTEGER_TO_FLOAT"] = "FORCE",							
+							["VALUE_CHANGE_TABLE"] 	= 	
+							{
+								{"StealthMinLevel",  "0"},  --Original 0.15
+								{"StealthDrainRate",  "0"},  --Original 15
+								{"StealthRechargeRate",  "10000"},  --Original 12
+							}
+						},											
+					}
+				}
+			}
+		}
+	}	
+}
