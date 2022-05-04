d0b65efc05dc21dfc3fd61e6d52c19a15d742d66
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-05-04 11:41:25 -0600

    Create _Passive_Sentinels.lua

---
+++ Babscoole/_Passive_Sentinels.lua
@@ -0,0 +1,31 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "_Passive_Sentinels.pak",
+["MOD_DESCRIPTION"]			= "",
+["MOD_AUTHOR"]				= "degraaaff",
+["LUA_AUTHOR"]				= "Babscoole",
+["NMS_VERSION"]				= "3.88",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "GCROBOTGLOBALS.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["INTEGER_TO_FLOAT"] = "FORCE",								
+							["VALUE_CHANGE_TABLE"] 	= 	
+							{
+								{"DroneCrimeWitnessLimit",  "30000"},  -- Original  3
+								{"DroneCrimeWitnessInvestigateDistance",  "0"},  -- Original  30
+								{"DroneCrimeProximityMultiplier",  "0"},  -- Original  3
+							}
+						},
+					}
+				},
+			}
+		}
+	}	
+}
