f36db934ca1420bae945843764121c6dd6dd856d
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-05-10 07:31:27 -0600

    adds

---
+++ lMonk/_Publish/Sailship proc lights.lua
@@ -0,0 +1,23 @@
+------------------------------------------------------------------
+local desc = [[
+  Sailship: Restore procedural lights who match the sail's color
+]]----------------------------------------------------------------
+
+NMS_MOD_DEFINITION_CONTAINER = {
+	MOD_FILENAME 		= '_MOD.lMonk.sailship procedural lights.pak',
+	MOD_AUTHOR			= 'lMonk',
+	NMS_VERSION			= 3.89,
+	MOD_DESCRIPTION		= desc,
+	MODIFICATIONS 		= {{
+	MBIN_CHANGE_TABLE	= {
+	{
+		MBIN_FILE_SOURCE	= 'MODELS/COMMON/SPACECRAFT/SAILSHIP/SAILSHIP_PROC/HQLIGHT_MAT11.MATERIAL.MBIN',
+		EXML_CHANGE_TABLE	= {
+			{
+				VALUE_CHANGE_TABLE 	= {
+					{'Map', 'TEXTURES/COMMON/SPACECRAFT/FIGHTERS/SAILS/RECTLIGHTPROC.BASE.DDS'}
+				}
+			}
+		}
+	}
+}}}}
