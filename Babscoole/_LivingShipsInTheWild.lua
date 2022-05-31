cb84deb064d3af60e32a631299c59ef4bcda9895
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-05-31 08:17:08 -0600

    Create _LivingShipsInTheWild.lua

---
+++ Babscoole/_LivingShipsInTheWild.lua
@@ -0,0 +1,30 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+    ["MOD_FILENAME"]    = "_LivingShipsInTheWild.pak",
+    ["MOD_DESCRIPTION"] = "allows you to find living ships in the wild",
+    ["MOD_AUTHOR"]      = "Fartbottum10",
+    ["LUA_AUTHOR"]      = "Babscoole",	
+    ["NMS_VERSION"]     = "3.91",
+    ["MODIFICATIONS"]   =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"]  = "GCSOLARGENERATIONGLOBALS.GLOBAL.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["PRECEDING_KEY_WORDS"] = {"SpaceshipWeightings",},
+                            ["REPLACE_TYPE"]        = "ALL",
+                            ["VALUE_CHANGE_TABLE"]  =
+                            {
+                                {"Alien","50"},
+                            },
+                        },
+                    }
+                }
+            }
+        }
+    }
+}
