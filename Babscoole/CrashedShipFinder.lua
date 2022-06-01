5d10d1bd006f8f59b09f528428ca7f1eebf7f9d3
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-06-01 00:18:31 -0600

    Adds

---
+++ Babscoole/CrashedShipFinder.lua
@@ -0,0 +1,62 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+  ["MOD_FILENAME"] 			= "CrashedShipFinder.pak",
+  ["MOD_DESCRIPTION"]		= "Planetary Chart B (Emergency Cartographic Data) increased chances of finding Crashed Ship",
+  ["MOD_AUTHOR"]			= "Fartbottum10",   
+  ["LUA_AUTHOR"]			= "Babscoole", 
+  ["NMS_VERSION"]			= "3.91",
+  ["MODIFICATIONS"] 		= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] = 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"]  = {"METADATA\REALITY\TABLES\REWARDTABLE.MBIN"},
+					["EXML_CHANGE_TABLE"] = 
+					{
+						{  --ABANDONED
+						    ["SPECIAL_KEY_WORDS"] = {"Id","R_STARCHART_B","List","GcRewardTableItemList.xml",},
+							["PRECEDING_KEY_WORDS"] = {"List","GcRewardTableItem.xml",},
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"PercentageChance",	"15"},
+							},
+						},											
+						{  --DISTRESS
+						    ["SPECIAL_KEY_WORDS"] = {"Id","R_STARCHART_B","List","GcRewardTableItemList.xml",},
+							["PRECEDING_KEY_WORDS"] = {"List","GcRewardTableItem.xml","GcRewardTableItem.xml",},
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"PercentageChance",	"50"},
+							},
+						},
+						{  --DISTRESS_NPC
+						    ["SPECIAL_KEY_WORDS"] = {"Id","R_STARCHART_B","List","GcRewardTableItemList.xml",},
+							["PRECEDING_KEY_WORDS"] = {"List","GcRewardTableItem.xml","GcRewardTableItem.xml","GcRewardTableItem.xml",},	
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"PercentageChance",	"10"},
+							},
+						},
+						{  --CRASHED_FREIGHTER
+						    ["SPECIAL_KEY_WORDS"] = {"Id","R_STARCHART_B","List","GcRewardTableItemList.xml",},
+							["PRECEDING_KEY_WORDS"] = {"List","GcRewardTableItem.xml","GcRewardTableItem.xml","GcRewardTableItem.xml","GcRewardTableItem.xml",},
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"PercentageChance",	"20"},
+							},
+						},						
+						{  --OBSERVATORY
+						    ["SPECIAL_KEY_WORDS"] = {"Id","R_STARCHART_B","List","GcRewardTableItemList.xml",},
+							["PRECEDING_KEY_WORDS"] = {"List","GcRewardTableItem.xml","GcRewardTableItem.xml","GcRewardTableItem.xml","GcRewardTableItem.xml","GcRewardTableItem.xml",},
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"PercentageChance",	"5"},
+							},
+						},						
+					},
+				},
+			}
+		}
+	}
+}
