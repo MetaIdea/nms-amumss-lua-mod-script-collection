b9931ee65dbdd797a138064a0a1531db8532a4f3
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-06-01 09:17:24 -0600

    updates

---
+++ Babscoole/ScaleAll.lua
@@ -0,0 +1,27 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "ScaleAll.pak", 
+["MOD_AUTHOR"]				= "Babscoole",
+["NMS_VERSION"]				= "3.88",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "METADATA\REALITY\TABLES\BASEBUILDINGOBJECTSTABLE.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["REPLACE_TYPE"] 		= "ALL",
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"CanScale",   					"True"},					
+							}
+						}
+					}
+				},
+			}
+		}			
+	}
+}
---
+++ Babscoole/ScaleBioDome.lua
@@ -0,0 +1,27 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "ScaleBioDome.pak",
+["MOD_AUTHOR"]				= "Babscoole",
+["NMS_VERSION"]				= "3.84",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "METADATA\REALITY\TABLES\BASEBUILDINGOBJECTSTABLE.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["SPECIAL_KEY_WORDS"] = {"ID","BIOROOM",},
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"CanScale",		"True"},
+							}
+						},
+					}
+				},
+			}
+		},
+	}	
+}
