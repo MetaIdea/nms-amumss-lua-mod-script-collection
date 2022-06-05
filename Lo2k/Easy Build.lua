261d226002c7eae6a687920d5973c2c8ba655a9b
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-06-05 09:29:47 -0600

    updates

---
+++ Lo2k/Easy Build.lua
@@ -0,0 +1,29 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "Easy Build.pak", 
+["MOD_AUTHOR"]				= "Lo2k",
+["NMS_VERSION"]				= "3.91",
+["MOD_DESCRIPTION"]			= "This mods reduces snapping distance",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "GCBUILDINGGLOBALS.GLOBAL.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["INTEGER_TO_FLOAT"] 	= "FORCE",
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"BuildingPlacementDefaultMinDistance",	"-2.0"}, -- original 3.0, -3.0 to place items at your feet
+								{"BuildingPlacementDefaultMinDistanceVR",	"-2.0"},  -- original 0.0
+							}
+						}
+					}
+				}
+			}
+		}
+	}	
+}
