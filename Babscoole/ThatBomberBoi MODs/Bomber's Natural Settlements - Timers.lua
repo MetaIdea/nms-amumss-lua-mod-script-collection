a2518a0e82bc241ba1ec0f8a8498727bc1dff4fa
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-06-06 23:17:00 -0600

    updates

---
+++ Babscoole/ThatBomberBoi MODs/Bomber's Natural Settlements - Timers.lua
@@ -0,0 +1,41 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+	["MOD_FILENAME"] 			= "BNS-Timers 3.91.pak",
+	["MOD_DESCRIPTION"]			= "",
+	["MOD_AUTHOR"]				= "ThatBomberBoi",
+	["NMS_VERSION"]				= "3.91",
+	["MODIFICATIONS"] 			=
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	=
+			{
+                {
+					["MBIN_FILE_SOURCE"] 	= "GCSETTLEMENTGLOBALS.MBIN",
+					["EXML_CHANGE_TABLE"] 	=
+					{
+                        {
+                            ["PRECEDING_KEY_WORDS"] = {"SettlementBuildingTimes"},
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"Settlement_LandingZone", "30"},
+                                {"Settlement_Bar", "30"},
+                                {"Settlement_Tower", "30"},
+                                {"Settlement_Market", "30"},
+                                {"Settlement_Small", "30"},
+                                {"Settlement_SmallIndustrial", "30"},
+                                {"Settlement_Medium", "30"},
+                                {"Settlement_Large", "30"},
+                                {"Settlement_Monument", "30"},
+                                {"Settlement_SheriffsOffice", "30"},
+                                {"Settlement_Double", "30"},
+                                {"Settlement_Farm", "30"},
+                                {"Settlement_Factory", "30"},
+                                {"Settlement_Clump", "30"}
+                            }
+                        }
+                    }
+				}
+            }
+        }
+    }
+}
---
+++ Babscoole/ThatBomberBoi MODs/Community Mission Debugger.lua
@@ -0,0 +1,29 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "CMD 3.91.pak",
+["MOD_AUTHOR"]				= "ThatBomberBoi",
+["LUA_AUTHOR"]				= "Babscoole",
+["NMS_VERSION"]				= "3.91",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	=  "METADATA\SIMULATION\MISSIONS\MISSIONCOMMUNITYDATA.MBIN",				
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["REPLACE_TYPE"] 		= "ALL",			
+							["VALUE_MATCH"] 		= "False",  						
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"ShowTimeToDeadline",	"True"},
+							}
+						}
+					}
+				}
+			}
+		}
+	}	
+}
