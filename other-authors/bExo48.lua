795b39c0076dbf1cdb933d17f48dfa01d6b0dc95
Xander Faern <xander@bladehawke.com>
2022-06-07 22:33:26 -0700

    Formatting fixes

--- other-authors/bExo48.lua
+++ other-authors/bExo48.lua
@@ -7,43 +7,43 @@ local ModBatch = "bInventoryTweaks"
 local BatchVersion = "a"
 
 NMS_MOD_DEFINITION_CONTAINER = {
-	["MOD_FILENAME"] = ModName .. " " .. GameVersion .. ModVersion .. ".pak",
+  ["MOD_FILENAME"] = ModName .. " " .. GameVersion .. ModVersion .. ".pak",
 --	["MOD_BATCHNAME"] = ModBatch .. " " .. GameVersion .. BatchVersion .. ".pak",
     ["MOD_DESCRIPTION"] = ModDesc,
     ["MOD_AUTHOR"] = AuthorName,
     ["NMS_VERSION"] = GameVersion,
-	["MODIFICATIONS"] = {
-		{
-			["MBIN_CHANGE_TABLE"] = {
-				{
-					["MBIN_FILE_SOURCE"] = "METADATA/REALITY/TABLES/INVENTORYTABLE.MBIN",
-					["EXML_CHANGE_TABLE"] = {
-						{
-							["PRECEDING_KEY_WORDS"] = {"GenerationDataPerSizeType", "VehicleSmall"},
-							["VALUE_CHANGE_TABLE"] = {
-								{"MinSlots", "48"},
-								{"MaxSlots", "48"}
-							}
-						},
-						{
-							["PRECEDING_KEY_WORDS"] = {"GenerationDataPerSizeType", "VehicleMedium"},
-							["VALUE_CHANGE_TABLE"] = {
-								{"MinSlots", "48"},
-								{"MaxSlots", "48"}
-							}
-						},
-						{
-							["PRECEDING_KEY_WORDS"] = {"GenerationDataPerSizeType", "VehicleLarge"},
-							["VALUE_CHANGE_TABLE"] = {
-								{"MinSlots", "48"},
-								{"MaxSlots", "48"}
-							}
-						}
-					} -- total 6 actions
-				}
-			}
-		}
-	}
+  ["MODIFICATIONS"] = {
+    {
+      ["MBIN_CHANGE_TABLE"] = {
+        {
+          ["MBIN_FILE_SOURCE"] = "METADATA/REALITY/TABLES/INVENTORYTABLE.MBIN",
+          ["EXML_CHANGE_TABLE"] = {
+            {
+              ["PRECEDING_KEY_WORDS"] = {"GenerationDataPerSizeType", "VehicleSmall"},
+              ["VALUE_CHANGE_TABLE"] = {
+                {"MinSlots", "48"},
+                {"MaxSlots", "48"}
+              }
+            },
+            {
+              ["PRECEDING_KEY_WORDS"] = {"GenerationDataPerSizeType", "VehicleMedium"},
+              ["VALUE_CHANGE_TABLE"] = {
+                {"MinSlots", "48"},
+                {"MaxSlots", "48"}
+              }
+            },
+            {
+              ["PRECEDING_KEY_WORDS"] = {"GenerationDataPerSizeType", "VehicleLarge"},
+              ["VALUE_CHANGE_TABLE"] = {
+                {"MinSlots", "48"},
+                {"MaxSlots", "48"}
+              }
+            }
+          } -- total 6 actions
+        }
+      }
+    }
+  }
 }
 
 --NOTE: ANYTHING NOT in table NMS_MOD_DEFINITION_CONTAINER IS IGNORED AFTER THE SCRIPT IS LOADED
