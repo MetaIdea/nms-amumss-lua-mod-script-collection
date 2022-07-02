9a2deee26b63eebf8fde2e3ac874420705928c32
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-07-02 12:41:55 -0600

    updates

--- other-authors/shoemakers_flight_adjustments_extended_hyperdrive_range.lua
+++ other-authors/shoemakers_flight_adjustments_extended_hyperdrive_range.lua
@@ -23,58 +23,78 @@ NMS_MOD_DEFINITION_CONTAINER =
                                 {"HoverLandReachedDistance","8"}, -- Original 10
                                 {"LandingPushNoseUpFactor", "0.02"}, -- Original 0.15
                                 {"MiniWarpLinesNum", "0"}, -- Original 4
-								{"DockingRotateSpeed", "10"}, -- Original 1
+								{"DockingRotateSpeed", "3"}, -- Original 1
                             }
                         },
                         {
                             ["PRECEDING_KEY_WORDS"] = {"SpaceEngine"},
 							["REPLACE_TYPE"]		= "ALL",
+							["INTEGER_TO_FLOAT"] 	= "FORCE",
 							["VALUE_CHANGE_TABLE"]	= 
                             {
+								{"LowSpeedTurnDamper", "0.01"}, -- Do not change this
                                 {"MaxSpeed", "720"}, -- Original 180
                                 {"BoostThrustForce", "1000"}, -- Original 500
                                 {"BoostMaxSpeed", "2400"}, -- Original 1200
                                 {"ReverseBrake", "0.6"}, -- Original 0.5
                                 {"OverSpeedBrake", "2"}, -- Original 3
+								{"TurnStrength", "2"}, -- Original 1
+								{"TurnBrakeMin", "0.5"}, -- Original 1
+								{"TurnBrakeMax", "0.5"}, -- Original 1
                             }
                         },
                         {
                             ["PRECEDING_KEY_WORDS"] = {"PlanetEngine"},
 							["REPLACE_TYPE"]		= "ALL",
+							["INTEGER_TO_FLOAT"] 	= "FORCE",
 							["VALUE_CHANGE_TABLE"]	= 
                             {
+								{"LowSpeedTurnDamper", "0.01"}, -- Do not change this
                                 {"MaxSpeed", "500"}, -- Original 125
                                 {"MinSpeed", "1"}, -- Original 10
                                 {"BoostThrustForce", "200"}, -- Original 100
                                 {"BoostMaxSpeed", "1200"}, -- Original 155
                                 {"ReverseBrake", "1"}, -- Original 1
                                 {"OverSpeedBrake", "2"}, -- Original 3
+								{"TurnStrength", "2"}, -- Original 1
+								{"TurnBrakeMin", "1"}, -- Original 2
+								{"TurnBrakeMax", "1.5"}, -- Original 3
                             }
                         },
                         {
                             ["PRECEDING_KEY_WORDS"] = {"CombatEngine"},
 							["REPLACE_TYPE"]		= "ALL",
+							["INTEGER_TO_FLOAT"] 	= "FORCE",
 							["VALUE_CHANGE_TABLE"]	= 
                             {
+								{"LowSpeedTurnDamper", "0.01"}, -- Do not change this
                                 {"MaxSpeed", "160"}, -- Original 80
                                 {"MinSpeed", "1"}, -- Original 10
                                 {"BoostThrustForce", "1000"}, -- Original 500
                                 {"BoostMaxSpeed", "2400"}, -- Original 1200
                                 {"ReverseBrake", "0.6"}, -- Original 0.5
                                 {"OverSpeedBrake", "2"}, -- Original 3
+								{"TurnStrength", "0.65"}, -- Original 1.3
+								{"TurnBrakeMin", "0.5"}, -- Original 1
+								{"TurnBrakeMax", "2"}, -- Original 4
                             }
                         },
                         {
                             ["PRECEDING_KEY_WORDS"] = {"AtmosCombatEngine"},
 							["REPLACE_TYPE"]		= "ALL",
+							["INTEGER_TO_FLOAT"] 	= "FORCE",
 							["VALUE_CHANGE_TABLE"]	= 
                             {
+								{"LowSpeedTurnDamper", "0.01"}, -- Do not change this
                                 {"MaxSpeed", "160"}, -- Original 180
                                 {"MinSpeed", "1"}, -- Original 10
                                 {"BoostThrustForce", "200"}, -- Original 100
                                 {"BoostMaxSpeed", "1200"}, -- Original 155
                                 {"ReverseBrake", "0.6"}, -- Original 0.5
                                 {"OverSpeedBrake", "2"}, -- Original 3
+								{"TurnStrength", "0.65"}, -- Original 1.3
+								{"TurnBrakeMin", "0.5"}, -- Original 1
+								{"TurnBrakeMax", "2"}, -- Original 4
                             }
                         }
                     }
