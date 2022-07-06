be0732cdc6ee137f8ba4022d8bf7b4eec51007c6
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-07-06 11:01:33 -0600

    Update E3_Like_Scanner.lua

--- Babscoole/E3_Like_Scanner.lua
+++ Babscoole/E3_Like_Scanner.lua
@@ -1,10 +1,10 @@
 NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"] 			= "E3_Like_Scanner.pak",
-["MOD_AUTHOR"]				= "Kuborr",
-["LUA_AUTHOR"]				= "Babscoole",
-["NMS_VERSION"]				= "3.93",
-["MODIFICATIONS"] 			= 
+["MOD_FILENAME"] 	= "E3_Like_Scanner.pak",
+["MOD_AUTHOR"]		= "Kuborr",
+["LUA_AUTHOR"]		= "Babscoole",
+["NMS_VERSION"]		= "3.93",
+["MODIFICATIONS"] 	= 
 	{
 		{
 			["MBIN_CHANGE_TABLE"] 	= 
@@ -29,25 +29,25 @@ NMS_MOD_DEFINITION_CONTAINER =
 								{"PulseTime",					"7"}, 	-- Original "1"							
 							}
 						},
-                        {
-                            ["SPECIAL_KEY_WORDS"] = {"ScannerColour1","Colour.xml",},
+						{
+							["SPECIAL_KEY_WORDS"] = {"ScannerColour1","Colour.xml",},
 							["INTEGER_TO_FLOAT"] = "FORCE",
-                            ["VALUE_CHANGE_TABLE"] = 
+							["VALUE_CHANGE_TABLE"] = 
 							{
-                                {"R", 	"1"},		-- Original "0.3"
-                                {"G", 	"0.8869"},	-- Original "0.9"								
-                                {"B", 	"0"},		-- Original "1"
-                            }
-                        },
-                        {
-                            ["SPECIAL_KEY_WORDS"] = {"ScannerColour2","Colour.xml",},
+								{"R", 	"1"},		-- Original "0.3"
+								{"G", 	"0.8869"},	-- Original "0.9"								
+								{"B", 	"0"},		-- Original "1"
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"ScannerColour2","Colour.xml",},
 							["INTEGER_TO_FLOAT"] = "FORCE",
-                            ["VALUE_CHANGE_TABLE"] = 
+							["VALUE_CHANGE_TABLE"] = 
 							{
-                                {"G", 	"0.8869"},	-- Original "0.5"								
-                                {"B", 	"0"},		-- Original "0.2"
-                            }
-                        },
+								{"G", 	"0.8869"},	-- Original "0.5"								
+								{"B", 	"0"},		-- Original "0.2"
+    							}
+						},
 					}
 				}
 			}
