31fd4dabefbd443015f4b19d7e7f96e2014b0948
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-07-07 09:16:11 -0600

    updates

---
+++ Babscoole/FasterAnalysis_1second.lua
@@ -0,0 +1,30 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "FasterAnalysis_1second.pak",
+["MOD_AUTHOR"]				= "Kuborr",
+["LUA_AUTHOR"]				= "Babscoole",
+["NMS_VERSION"]				= "3.93",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "GCGAMEPLAYGLOBALS.GLOBAL.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["PRECEDING_KEY_WORDS"] = "",
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"BinocMinScanTime",					"1"},		-- Original "3.9"
+								{"BinocScanTime",						"1"}, 		-- Original "3.9"
+								{"BinocCreatureScanTime",				"1"},		-- Original "3.2"							
+							}
+						},
+					}
+				}
+			}
+		}
+	}	
+}
---
+++ Babscoole/FasterAnalysis_3seconds.lua
@@ -0,0 +1,30 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "FasterAnalysis_3seconds.pak",
+["MOD_AUTHOR"]				= "Kuborr",
+["LUA_AUTHOR"]				= "Babscoole",
+["NMS_VERSION"]				= "3.93",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "GCGAMEPLAYGLOBALS.GLOBAL.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["PRECEDING_KEY_WORDS"] = "",
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"BinocMinScanTime",					"3"},		-- Original "3.9"
+								{"BinocScanTime",						"3"}, 		-- Original "3.9"
+								{"BinocCreatureScanTime",				"3"},		-- Original "3.2"							
+							}
+						},
+					}
+				}
+			}
+		}
+	}	
+}
