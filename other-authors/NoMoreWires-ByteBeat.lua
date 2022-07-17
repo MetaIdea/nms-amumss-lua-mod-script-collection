baeef06eafc639c94e63158675a7a730df3e9b8b
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-07-16 21:31:52 -0600

    updates

---
+++ other-authors/NoMoreWires-ByteBeat.lua
@@ -0,0 +1,31 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "NoMoreWires-Byte.pak",
+["MOD_AUTHOR"]				= "Pyronox Omega",
+["NMS_VERSION"]				= "3.91",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= 
+						{
+							"MODELS\PLANETS\BIOMES\COMMON\BUILDINGS\PARTS\BUILDABLEPARTS\UTILITYPARTS\CONDUIT_BYTEBEAT\BYTEWIRESCROLLEFFECTMAT.MATERIAL.MBIN"
+						    
+						},	
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"Map",	""},				
+							}
+						},
+						
+					}
+				},		
+			}
+		}
+	}	
+}
---
+++ other-authors/NoMoreWires-portals.lua
@@ -0,0 +1,27 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "NoMoreWires-teleport.pak",
+["MOD_AUTHOR"]				= "Pyronox Omega",
+["NMS_VERSION"]				= "3.91",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "MODELS\PLANETS\BIOMES\COMMON\BUILDINGS\PARTS\BUILDABLEPARTS\UTILITYPARTS\CONDUIT_PORTALWIRE\TELEPORTERSCROLLEFFECTMAT.MATERIAL.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"Map",	""},
+								{"Shader",	""},								
+							}
+						},
+					}
+				},		
+			}
+		}
+	}	
+}
