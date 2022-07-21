a1d2d781c6eba6e3e7f228fd5428e3e89ace36d9
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-07-21 14:18:48 -0600

    Endurance Updates

--- Lo2k/Multi Pass 4.9.lua
+++ Lo2k/Multi Pass 4.9.lua
@@ -1,8 +1,9 @@
 NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"] 			= "Multi Pass 4.8.pak", 
+["MOD_FILENAME"] 			= "Multi Pass 4.9.pak", 
 ["MOD_AUTHOR"]				= "Lo2k",
-["NMS_VERSION"]				= "3.90",
+["LUA_AUTHOR"]				= "Lo2k",
+["NMS_VERSION"]				= "3.94",
 ["MOD_DESCRIPTION"]			= "This mods makes gold/silver/bronze atlas passes",
 ["MODIFICATIONS"] 			= 
 	{
