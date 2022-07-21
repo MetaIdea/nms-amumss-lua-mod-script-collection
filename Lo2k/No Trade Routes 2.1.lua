007d9e85802384c8de63a69b10ca27124e852d4a
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-07-20 19:21:58 -0600

    Endurance Updates

--- Lo2k/No Trade Routes 2.1.lua
+++ Lo2k/No Trade Routes 2.1.lua
@@ -1,8 +1,9 @@
 NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"] 			= "No Trade Routes 2.0.pak", 
+["MOD_FILENAME"] 			= "No Trade Routes 2.1.pak", 
 ["MOD_AUTHOR"]				= "Lo2k",
-["NMS_VERSION"]				= "3.89",
+["LUA_AUTHOR"]				= "Lo2k",
+["NMS_VERSION"]				= "3.94",
 ["MOD_DESCRIPTION"]			= "This mod removes space trade route white lines",
 ["MODIFICATIONS"] 			= 
 	{
