8aecb28d5b6d6d99dd985e4b8616eecd7fb09a91
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-07-25 17:55:04 -0600

    Endurance Updates

--- Babscoole/MusicAndSilence MODs/LargerHerds_HighDensity_v1.2.lua
+++ Babscoole/MusicAndSilence MODs/LargerHerds_HighDensity_v1.2.lua
@@ -2,10 +2,10 @@ MULTIPLIER = 3
 
 NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"]  = "LargerHerds_HighDensity_v1.1.pak",
+["MOD_FILENAME"]  = "LargerHerds_HighDensity_v1.2.pak",
 ["MOD_AUTHOR"]    = "MusicAndSilence",
 ["LUA_AUTHOR"]    = "Babscoole",
-["NMS_VERSION"]   = "3.95",
+["NMS_VERSION"]   = "3.97",
 ["MODIFICATIONS"] = 
 	{
 		{
@@ -15,25 +15,6 @@ NMS_MOD_DEFINITION_CONTAINER =
 					["MBIN_FILE_SOURCE"] =
 					{					
 						"METADATA\SIMULATION\ECOSYSTEM\AIR\AIRTABLEBIGBIRD.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEBONE.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEROBOT.MBIN",						
-					},	
-					["EXML_CHANGE_TABLE"] = 
-					{
-						{
-							["PRECEDING_KEY_WORDS"] = {"GcCreatureRoleDescription.xml"},
-							["REPLACE_TYPE"] = "ALL",
-							["VALUE_CHANGE_TABLE"] = 
-							{
-								{"MinGroupSize", "3" },
-								{"MaxGroupSize", "5" },					
-							}
-						},
-					}
-				},
-				{
-					["MBIN_FILE_SOURCE"] =
-					{					
 						"METADATA\SIMULATION\ECOSYSTEM\AIR\AIRTABLEBUSY.MBIN",
 						"METADATA\SIMULATION\ECOSYSTEM\AIR\AIRTABLECOMMON.MBIN",
 						"METADATA\SIMULATION\ECOSYSTEM\AIR\AIRTABLECOMMONLIZARD.MBIN",
@@ -70,13 +51,37 @@ NMS_MOD_DEFINITION_CONTAINER =
 					["MBIN_FILE_SOURCE"] =
 					{					
 						"METADATA\SIMULATION\ECOSYSTEM\CAVE\CAVETABLECOMMON.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\CAVE\CAVETABLECOMMON.MBIN",						
-					},					
+						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\CAVE\CAVETABLECOMMON.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLEBUSY.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLECOMMON.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLEGIANT.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLEPREDATORS.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLEPREHISTORIC.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLEPREYBLOBS.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLESMALLPREDATORS.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLESPARSE.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\UNUSED\GROUNDTABLEALLCOW.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\UNUSED\GROUNDTABLEDINOSAURS.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\UNUSED\GROUNDTABLEGRUNTLAND.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEALIEN.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEBONE.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEBUTTERFLY.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEHERBIVOREGIANT.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEHERBIVORELARGE.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEHERBIVOREMED.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEHERBIVORESMALL.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEMAYBEBUTTERFLY.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEPLAYERPREDATORLARGE.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEPLAYERPREDATORMED.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEPREDATORLARGE.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEPREDATORMED.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEROBOT.MBIN",						
+					},						
 					["EXML_CHANGE_TABLE"] = 
 					{
 						{
 							["PRECEDING_KEY_WORDS"] = {"GcCreatureRoleDescription.xml"},
-							["SECTION_ACTIVE"] = {1, 3,},
+							["REPLACE_TYPE"] = "ALL",
 							["MATH_OPERATION"] = "*",
 							["VALUE_CHANGE_TABLE"] = 
 							{
@@ -85,15 +90,6 @@ NMS_MOD_DEFINITION_CONTAINER =
 							}
 						},
 						{
-							["PRECEDING_KEY_WORDS"] = {"GcCreatureRoleDescription.xml"},
-							["SECTION_ACTIVE"] = {2,},
-							["VALUE_CHANGE_TABLE"] = 
-							{
-								{"MinGroupSize", "3" },
-								{"MaxGroupSize", "5" },					
-							}
-						},
-						{
 							["SPECIAL_KEY_WORDS"] = {"Density","GcCreatureGenerationDensity.xml"},
 							["LINE_OFFSET"] = "+1",
 							["REPLACE_TYPE"] = "ALL",
@@ -105,45 +101,9 @@ NMS_MOD_DEFINITION_CONTAINER =
 					}
 				},
 				{
-					["MBIN_FILE_SOURCE"] = 
-					{
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\AIR\AIRTABLESANDWORM.MBIN",
-					},
-					["EXML_CHANGE_TABLE"] = 
-					{
-						{
-							["PRECEDING_KEY_WORDS"] = {"GcCreatureRoleDescription.xml"},
-							["REPLACE_TYPE"] = "ALL",
-							["VALUE_CHANGE_TABLE"] = 
-							{
-								{"MinGroupSize", "3" },
-								{"MaxGroupSize", "5" },					
-							}
-						},
-						{
-							["REPLACE_TYPE"] = "ALL",
-							["VALUE_CHANGE_TABLE"] = 
-							{
-								{"CreatureType", "SandWorm" },				
-							}
-						},
-					}
-				},
-				{
 					["MBIN_FILE_SOURCE"] =
 					{					
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\FIEND\GROUNDTABLEFIEND.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLEPREYBLOBS.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLESMALLPREDATORS.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLESPARSE.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\UNUSED\GROUNDTABLEALLCOW.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\UNUSED\GROUNDTABLEDINOSAURS.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\UNUSED\GROUNDTABLEGRUNTLAND.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEBUTTERFLY.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEHERBIVOREMED.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEHERBIVORESMALL.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEMAYBEBUTTERFLY.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEPREDATORMED.MBIN",						
+						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\AIR\AIRTABLESANDWORM.MBIN",			
 					},						
 					["EXML_CHANGE_TABLE"] = 
 					{
@@ -154,65 +114,14 @@ NMS_MOD_DEFINITION_CONTAINER =
 							["VALUE_CHANGE_TABLE"] = 
 							{
 								{"MinGroupSize", MULTIPLIER},
-								{"MaxGroupSize", MULTIPLIER},				
+								{"MaxGroupSize", MULTIPLIER},					
 							}
 						},
 						{
-							["SPECIAL_KEY_WORDS"] = {"Density","GcCreatureGenerationDensity.xml"},
-							["LINE_OFFSET"] = "+1",
 							["REPLACE_TYPE"] = "ALL",
 							["VALUE_CHANGE_TABLE"] = 
 							{
-								{"Density", "VeryDense"},				
-							}
-						},
-					}
-				},
-				{
-					["MBIN_FILE_SOURCE"] =
-					{					
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEHERDMED.MBIN",					
-					},						
-					["EXML_CHANGE_TABLE"] = 
-					{
-						{
-							["PRECEDING_KEY_WORDS"] = {"GcCreatureRoleDescription.xml"},
-							["REPLACE_TYPE"] = "ALL",
-							["VALUE_CHANGE_TABLE"] = 
-							{
-								{"MinGroupSize", "12"},
-								{"MaxGroupSize", "24"},					
-							}
-						},
-						{
-							["SPECIAL_KEY_WORDS"] = {"Density","GcCreatureGenerationDensity.xml"},
-							["LINE_OFFSET"] = "+1",
-							["REPLACE_TYPE"] = "ALL",
-							["VALUE_CHANGE_TABLE"] = 
-							{
-								{"Density", "VeryDense"},				
-							}
-						},
-					}
-				},				
-				{
-					["MBIN_FILE_SOURCE"] =
-					{					
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLEBUSY.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLECOMMON.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLEGIANT.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLEPREDATORS.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\DEPRECATE\GROUND\GROUNDTABLEPREHISTORIC.MBIN",		
-					},						
-					["EXML_CHANGE_TABLE"] = 
-					{
-						{
-							["SPECIAL_KEY_WORDS"] = {"Density","GcCreatureGenerationDensity.xml"},
-							["LINE_OFFSET"] = "+1",
-							["REPLACE_TYPE"] = "ALL",
-							["VALUE_CHANGE_TABLE"] = 
-							{
-								{"Density", "VeryDense"},				
+								{"CreatureType", "Weird" },				
 							}
 						},
 					}
@@ -230,7 +139,9 @@ NMS_MOD_DEFINITION_CONTAINER =
 						"METADATA\SIMULATION\ECOSYSTEM\GROUND\WEIRD\GROUNDTABLEWEIRDBUTTERFLY.MBIN",
 						"METADATA\SIMULATION\ECOSYSTEM\GROUND\WEIRD\GROUNDTABLEWEIRDCRYSTAL.MBIN",
 						"METADATA\SIMULATION\ECOSYSTEM\GROUND\WEIRD\GROUNDTABLEWEIRDCRYSTALPREDATOR.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\WEIRD\GROUNDTABLEWEIRDFLOAT.MBIN",						
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\WEIRD\GROUNDTABLEWEIRDFLOAT.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\WEIRD\GROUNDTABLEWEIRDROLL.MBIN",
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\WEIRD\GROUNDTABLEWEIRDROLLPREDATOR.MBIN",						
 					},						
 					["EXML_CHANGE_TABLE"] = 
 					{
@@ -245,19 +156,19 @@ NMS_MOD_DEFINITION_CONTAINER =
 							}
 						},
 						{
+							["REPLACE_TYPE"] = "ALL",
+							["VALUE_CHANGE_TABLE"] = 
+							{
+								{"CreatureType", "MiniDrone" },				
+							}
+						},
+						{
 							["SPECIAL_KEY_WORDS"] = {"Density","GcCreatureGenerationDensity.xml"},
 							["LINE_OFFSET"] = "+1",
 							["REPLACE_TYPE"] = "ALL",
 							["VALUE_CHANGE_TABLE"] = 
 							{
 								{"Density", "VeryDense"},				
-							}
-						},
-						{
-							["REPLACE_TYPE"] = "ALL",
-							["VALUE_CHANGE_TABLE"] = 
-							{
-								{"CreatureType", "Weird" },				
 							}
 						},
 					}
@@ -283,67 +194,35 @@ NMS_MOD_DEFINITION_CONTAINER =
 							["REPLACE_TYPE"] = "ALL",
 							["VALUE_CHANGE_TABLE"] = 
 							{
-								{"CreatureType", "SeaSnake" },				
+								{"CreatureType", "SpaceFloater" },				
 							}
 						},
 					}
 				},
 				{
-					["MBIN_FILE_SOURCE"] =
-					{					
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEALIEN.MBIN"				
-					},						
+					["MBIN_FILE_SOURCE"] = 
+					{
+						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEHERDMED.MBIN",
+					},
 					["EXML_CHANGE_TABLE"] = 
 					{
-						{
-							["SPECIAL_KEY_WORDS"] = {"Density","GcCreatureGenerationDensity.xml"},
-							["LINE_OFFSET"] = "+1",
-							["REPLACE_TYPE"] = "ALL",
-							["VALUE_CHANGE_TABLE"] = 
-							{
-								{"Density", "VeryDense"},				
-							}
-						},
 						{
 							["PRECEDING_KEY_WORDS"] = {"GcCreatureRoleDescription.xml"},
 							["SECTION_ACTIVE"] = {1,},
 							["MATH_OPERATION"] = "*",
 							["VALUE_CHANGE_TABLE"] = 
 							{
-								{"MinGroupSize", "3"},
-								{"MaxGroupSize", "5"},					
-							}
-						},
-						{
-							["PRECEDING_KEY_WORDS"] = {"GcCreatureRoleDescription.xml"},
-							["SECTION_ACTIVE"] = {2, 3, 4,},
-							["MATH_OPERATION"] = "*",
-							["VALUE_CHANGE_TABLE"] = 
-							{
 								{"MinGroupSize", MULTIPLIER},
-								{"MaxGroupSize", MULTIPLIER},					
+								{"MaxGroupSize", MULTIPLIER},				
 							}
 						},
-					}
-				},
-				{
-					["MBIN_FILE_SOURCE"] =
-					{					
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEHERBIVOREGIANT.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEHERBIVORELARGE.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEPLAYERPREDATORLARGE.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEPLAYERPREDATORMED.MBIN",
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\GROUNDTABLEPREDATORLARGE.MBIN",						
-					},					
-					["EXML_CHANGE_TABLE"] = 
-					{
 						{
 							["PRECEDING_KEY_WORDS"] = {"GcCreatureRoleDescription.xml"},
-							["REPLACE_TYPE"] = "ALL",
+							["SECTION_ACTIVE"] = {2,},
 							["VALUE_CHANGE_TABLE"] = 
 							{
-								{"MinGroupSize", "3"},
-								{"MaxGroupSize", "5"},					
+								{"MinGroupSize", 	"12" },
+								{"MaxGroupSize",	"24" },					
 							}
 						},
 						{
@@ -353,56 +232,6 @@ NMS_MOD_DEFINITION_CONTAINER =
 							["VALUE_CHANGE_TABLE"] = 
 							{
 								{"Density", "VeryDense"},				
-							}
-						},
-					}
-				},
-				{
-					["MBIN_FILE_SOURCE"] =
-					{					
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\WEIRD\GROUNDTABLEWEIRDROLL.MBIN",					
-					},						
-					["EXML_CHANGE_TABLE"] = 
-					{
-						{
-							["PRECEDING_KEY_WORDS"] = {"GcCreatureRoleDescription.xml"},
-							["REPLACE_TYPE"] = "ALL",
-							["MATH_OPERATION"] = "*",
-							["VALUE_CHANGE_TABLE"] = 
-							{
-								{"MinGroupSize", MULTIPLIER},
-								{"MaxGroupSize", MULTIPLIER},					
-							}
-						},
-						{
-							["REPLACE_TYPE"] = "ALL",
-							["VALUE_CHANGE_TABLE"] = 
-							{
-								{"CreatureType", "Weird" },				
-							}
-						},
-					}
-				},
-				{
-					["MBIN_FILE_SOURCE"] =
-					{					
-						"METADATA\SIMULATION\ECOSYSTEM\GROUND\WEIRD\GROUNDTABLEWEIRDROLLPREDATOR.MBIN",						
-					},						
-					["EXML_CHANGE_TABLE"] = 
-					{
-						{
-							["PRECEDING_KEY_WORDS"] = {"GcCreatureRoleDescription.xml"},
-							["VALUE_CHANGE_TABLE"] = 
-							{
-								{"MinGroupSize", "3"},
-								{"MaxGroupSize", "9"},					
-							}
-						},
-						{
-							["REPLACE_TYPE"] = "ALL",
-							["VALUE_CHANGE_TABLE"] = 
-							{
-								{"CreatureType", "Weird" },				
 							}
 						},
 					}
