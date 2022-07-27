ba79029e7ed4060b85c68cf4b8f21668d4de1e7d
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-07-27 02:48:32 -0600

    Endurance Updates

--- other-authors/Planetary_Flight_Fixes_2_4.lua
+++ other-authors/Planetary_Flight_Fixes_2_4.lua
@@ -33,9 +33,9 @@ COSTMULT = 		0.2	  -- This sets the ship launch fuel cost; The lower the number,
 							
 NMS_MOD_DEFINITION_CONTAINER =
 {
-  ["MOD_FILENAME"]             = "Planetary_Flight_Fixes_2_3.pak", 
+  ["MOD_FILENAME"]             = "_Planetary_Flight_Fixes_2_4.pak", 
   ["MOD_AUTHOR"]               = "chronicallybored",
-  ["NMS_VERSION"]              = "3.91",
+  ["NMS_VERSION"]              = "3.97",
   ["MOD_DESCRIPTION"]          = "Fixes various aspects of the flight system while inside a planets atmosphere",
   ["MODIFICATIONS"]            = 
 	{
