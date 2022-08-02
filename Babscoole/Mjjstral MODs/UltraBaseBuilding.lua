0d582a1c3fe9a5e2ac3b74ea78c28518bdf57585
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-08-02 11:50:57 -0600

    3.98 updates

--- Babscoole/Mjjstral MODs/UltraBaseBuilding.lua
+++ Babscoole/Mjjstral MODs/UltraBaseBuilding.lua
@@ -1430,7 +1430,7 @@ NMS_MOD_DEFINITION_CONTAINER =
 ["MOD_FILENAME"]  = "0-UltraBaseBuilding.pak",
 ["MOD_AUTHOR"]    = "Mjjstral and Babscoole",
 ["Contributors"]  = "Ignacio and GhostRick",
-["NMS_VERSION"]   = "3.97",
+["NMS_VERSION"]   = "3.98",
 ["ADD_FILES"]     = ADD_FILES_TABLE,
 ["MODIFICATIONS"] =
 	{
