93efb8a37a5ff66587e35ffb67586df12c40efbf
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-08-11 10:01:51 -0600

    Create Better Hotspots.lua

---
+++ Babscoole/Better Hotspots.lua
@@ -0,0 +1,453 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"]  = "BetterHotspots.pak",
+["MOD_AUTHOR"]    = "Sypheren",
+["LUA_AUTHOR"]    = "Babscoole",
+["NMS_VERSION"]   = "3.84",
+["MODIFICATIONS"] = 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] = 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"]  = "METADATA\SIMULATION\SCANNING\REGIONHOTSPOTSTABLE.MBIN",
+					["EXML_CHANGE_TABLE"] = 
+					{
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Power","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+1",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "1"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Power","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+2",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "2"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Power","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+3",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "6"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Power","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+4",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "10"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Power","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+1",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "450"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Power","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+2",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "660"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Power","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+3",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "750"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Power","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+4",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "900"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+1",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "1"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+2",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "2"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+3",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "6"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+4",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "10"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+1",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "3"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+2",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "4.5"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+3",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "6"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+4",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "7.5"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"mineral2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+1",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "1"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"mineral2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+2",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "2"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"mineral2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+3",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "6"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"mineral2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+4",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "10"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"mineral2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+1",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "3"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"mineral2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+2",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "4.5"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"mineral2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+3",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "6"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"mineral2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+4",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "7.5"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral3","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+1",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "1"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral3","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+2",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "2"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral3","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+3",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "6"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral3","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+4",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "10"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral3","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+1",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "3"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral3","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+2",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "4.5"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral3","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+3",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "6"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Mineral3","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+4",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "7.5"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+1",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "1"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+2",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "2"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+3",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "6"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+4",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "10"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+1",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "3"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+2",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "4.5"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+3",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "6"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas1","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+4",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "7.5"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+1",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "1"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+2",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "2"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+3",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "6"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassWeightings",},
+							["LINE_OFFSET"] = "+4",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE" , "10"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+1",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "3"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+2",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "4.5"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+3",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"IGNORE", "6"},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] 	= {"Gas2","GcRegionHotspotData.xml",},
+							["PRECEDING_KEY_WORDS"] = {"ClassStrengths",},
+							["LINE_OFFSET"] = "+4",
+							["VALUE_CHANGE_TABLE"] =
+							{
+								{"IGNORE", "7.5"},
+							}
+						},							
+					}
+				},
+			}
+		},
+	}	
+}
