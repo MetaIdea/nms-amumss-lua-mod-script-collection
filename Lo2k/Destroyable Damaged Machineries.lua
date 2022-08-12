bff2ac2aaf9e59a9a733665e2e45d95e4a5fba47
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-08-12 14:41:06 -0600

    Create Destroyable Damaged Machineries.lua

---
+++ Lo2k/Destroyable Damaged Machineries.lua
@@ -0,0 +1,30 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "Destroyable Damaged Machineries.pak", 
+["MOD_AUTHOR"]				= "Lo2k",
+["LUA_AUTHOR"]				= "Lo2k",
+["NMS_VERSION"]				= "3.98",
+["MOD_DESCRIPTION"]			= "This mod makes damaged machineries destroyable",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= {"METADATA\SIMULATION\SOLARSYSTEM\BIOMES\OBJECTS\LEVELONEOBJECTS\FULL.MBIN", "METADATA\SIMULATION\SOLARSYSTEM\BIOMES\OBJECTS\LEVELONEOBJECTS\FULLSAFE.MBIN"},
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["SPECIAL_KEY_WORDS"] = {"Filename", "MODELS/PLANETS/BIOMES/COMMON/BUILDINGS/DEBRIS/DEBRISLARGE_COMMON.SCENE.MBIN",},
+							["SECTION_UP"] = 1,
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"DestroyedByTerrainEdit", "True"},
+							}
+						},
+					},
+				}	
+			}
+		}
+	}	
+}
