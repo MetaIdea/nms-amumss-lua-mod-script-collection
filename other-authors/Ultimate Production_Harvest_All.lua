154b432fa31e123c3a4af2e4f47b22f5e1c505ee
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-08-13 05:11:54 -0600

    Create Ultimate Production_Harvest_All.lua

---
+++ other-authors/Ultimate Production_Harvest_All.lua
@@ -0,0 +1,46 @@
+Author = "PyronoxOmega"
+ModName = "Ultimate_Production_harvest_All"
+GameVersion = "3.97"
+ModVersion = "1"
+Description = "Freighter Production Enhancement"
+
+
+FileSource3 ="MODELS\PLANETS\BIOMES\COMMON\BUILDINGS\PARTS\BUILDABLEPARTS\FREIGHTERBASE\ROOMS\PLANTROOM\PARTS\FLOOR0\ENTITIES\INTERACTION.ENTITY.MBIN"
+
+
+
+
+
+NMS_MOD_DEFINITION_CONTAINER = {
+["MOD_FILENAME"]	= ModName..""..GameVersion..ModVersion..".pak",
+["MOD_DESCRIPTION"]	= Description,
+["MOD_AUTHOR"]		= Author,
+["NMS_VERSION"]		= GameVersion,
+["MODIFICATIONS"] = 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] = 
+			{ 
+				
+				--Hasrvest all freighter farm in 1 go-- 
+				{
+					["MBIN_FILE_SOURCE"]  = FileSource3,							
+					["EXML_CHANGE_TABLE"] = 
+					{ 
+						{
+							["PRECEDING_KEY_WORDS"] = {"GcHarvestPlantAction.xml"},
+							["REPLACE_TYPE"]    = "ALL", 
+							["VALUE_CHANGE_TABLE"] =
+							{		
+								{"Radius","1e+7"},	
+															
+							}
+						}
+					}
+				},
+				
+				
+			}
+		},
+	}	
+}
