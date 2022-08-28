078277532da02bb89cda9f8e11dc99cbd6b2503d
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-08-28 11:47:07 -0600

    3.99 updates

---
+++ Silent369/_Silent369.BgDarkUI.lua
@@ -0,0 +1,40 @@
+local modfilename = "BgDarkUI"
+local lua_author  = "Silent"
+local lua_version = "v1.1"
+local mod_author  = "Silent369"
+local nms_version = "3.99x"
+local description = "Changes Start/Options Backgrounds and Transparent UI"
+
+--credit to Lo2k (https://www.nexusmods.com/nomanssky/mods/1706)
+--for the centered atlas icon fix, it annoyed me too :)
+
+NMS_MOD_DEFINITION_CONTAINER =
+{
+    ["MOD_FILENAME"]         = "_"..modfilename..lua_version..".pak",
+    ["LUA_AUTHOR"]           = lua_author,
+    ["MOD_AUTHOR"]           = mod_author,
+    ["NMS_VERSION"]          = nms_version,
+    ["MOD_DESCRIPTION"]      = description,
+    ["MODIFICATIONS"]        =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"]   =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"]    = {"UI\BOOT\TWOLINEBUTTON.MBIN"},
+                    ["EXML_CHANGE_TABLE"]   =
+                    {
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {"ID", "ICON"},
+                            ["VALUE_CHANGE_TABLE"]  =
+                            {
+                                {"PositionX",       "50"},  -- Original "41.364212"
+                                {"Horizontal",  "Center"},  -- Original "Left"
+                            }
+                        },
+                    }
+                }
+            }
+        }
+    }
+}
