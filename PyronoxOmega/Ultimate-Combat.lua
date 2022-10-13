04ef10e2e593667f365bf2c89f20b285b6d55914
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-10-13 10:48:12 -0600

    Create Ultimate-Combat.lua

---
+++ PyronoxOmega/Ultimate-Combat.lua
@@ -0,0 +1,104 @@
+Author = "PyronoxOmega"
+ModName = "Ultimate_Combat"
+GameVersion = "4"
+ModVersion = "1.0"
+Description = "Weapons overhaull"
+Filesource = "METADATA/REALITY/DEFAULTREALITY.MBIN"
+Damage = "28360"
+NMS_MOD_DEFINITION_CONTAINER = {
+	["MOD_FILENAME"] = ModName .. "" .. GameVersion .. ModVersion .. ".pak",
+	["MOD_DESCRIPTION"] = Description,
+	["MOD_AUTHOR"] = Author,
+	["NMS_VERSION"] = GameVersion,
+	["MODIFICATIONS"] = {
+		{
+			["MBIN_CHANGE_TABLE"] = {
+				{
+					["MBIN_FILE_SOURCE"] = Filesource,
+					["INTEGER_TO_FLOAT"] = "FORCE",
+					["REPLACE_TYPE"] = "ALL",
+					["EXML_CHANGE_TABLE"] = {
+						{
+							["PRECEDING_KEY_WORDS"] = {"ShipWeapons", "Laser"},
+							["VALUE_CHANGE_TABLE"] = {
+								{"AutoAimAngle", "3"},
+								{"AutoAimExtraAngle", "15"},
+								{"ShowOverheatSwitch", "False"},
+								{"OverheatCoolTime", "0"},
+								{"CoolRate", "1"}
+							}
+						},
+						{
+							["PRECEDING_KEY_WORDS"] = {"ShipWeapons", "Projectile"},
+							["VALUE_CHANGE_TABLE"] = {
+								{"AutoAimAngle", "4"},
+								{"AutoAimExtraAngle", "20"},
+								{"ShowOverheatSwitch", "False"},
+								{"OverheatCoolTime", "0"},
+								{"CoolRate", "1"}
+							}
+						},
+						{
+							["PRECEDING_KEY_WORDS"] = {"ShipWeapons", "Shotgun"},
+							["VALUE_CHANGE_TABLE"] = {
+								{"AutoAimAngle", "4"},
+								{"AutoAimExtraAngle", "20"},
+								{"ShowOverheatSwitch", "False"},
+								{"OverheatCoolTime", "0"},
+								{"CoolRate", "1"}
+							}
+						},
+						{
+							["PRECEDING_KEY_WORDS"] = {"ShipWeapons", "Minigun"},
+							["VALUE_CHANGE_TABLE"] = {
+								{"AutoAimAngle", "5"},
+								{"AutoAimExtraAngle", "20"},
+								{"ShowOverheatSwitch", "False"},
+								{"OverheatCoolTime", "0"},
+								{"CoolRate", "1"}
+							}
+						},
+						{
+							["PRECEDING_KEY_WORDS"] = {"ShipWeapons", "Plasma"},
+							["VALUE_CHANGE_TABLE"] = {
+								{"AutoAimAngle", "4"},
+								{"AutoAimExtraAngle", "20"},
+								{"ShowOverheatSwitch", "False"},
+								{"OverheatCoolTime", "0"},
+								{"CoolRate", "1"}
+							}
+						},
+						 {
+							["PRECEDING_KEY_WORDS"] = {"ShipWeapons", "Missile"},
+							["VALUE_CHANGE_TABLE"] = {
+								{"AutoAimAngle", "3"},
+								{"AutoAimExtraAngle", "15"},
+								{"ShowOverheatSwitch", "False"},
+								{"OverheatCoolTime", "0"},
+								{"CoolRate", "1"}
+							}
+						},
+						{
+							["PRECEDING_KEY_WORDS"] = {"ShipWeapons", "Rocket"},
+							["VALUE_CHANGE_TABLE"] = {
+								{"AutoAimExtraAngle", "20"},
+								{"ShowOverheatSwitch", "False"},
+								{"OverheatCoolTime", "0"},
+								{"CoolRate", "1"}
+							}
+						},
+						{
+							["PRECEDING_KEY_WORDS"] = "",
+							--["MATH_OPERATION"] 		= "*",--
+							["REPLACE_TYPE"] = "ALL",
+							["VALUE_MATCH"] = "",
+							["VALUE_CHANGE_TABLE"] = {
+								{"Multiplier", Damage}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
