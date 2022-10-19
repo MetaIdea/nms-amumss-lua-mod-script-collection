a2cde7768ee27a89a0899c419cc4bc3957ae85f0
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-10-19 00:47:16 -0600

    4.04 Updates

--- Babscoole/JasonDudes Mods/_MOD_DUD_PetRider_v4031.lua
+++ Babscoole/JasonDudes Mods/_MOD_DUD_PetRider_v4031.lua
@@ -1,9 +1,9 @@
 NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"]  = "_MOD_DUD_PetRider_v3911.pak",
+["MOD_FILENAME"]  = "_MOD_DUD_PetRider_v4031.pak",
 ["MOD_AUTHOR"]    = "jasondude7116",
 ["LUA_AUTHOR"]    = "Babscoole",
-["NMS_VERSION"]   = "3.91",
+["NMS_VERSION"]   = "4.04",
 ["MODIFICATIONS"] = 
 	{
 		{
