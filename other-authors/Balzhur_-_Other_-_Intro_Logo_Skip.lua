44ac81ac14c091b654e0caa4fd19820a3718caf3
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-10-22 12:32:09 -0600

    Create Balzhur_-_Other_-_Intro_Logo_Skip.lua

---
+++ other-authors/Balzhur_-_Other_-_Intro_Logo_Skip.lua
@@ -0,0 +1,52 @@
+ModAuthor	= "Balzhur"
+LuaAuthor	= "Nexus site, Balzhur"
+Maintainer	= ""
+ModName		= "Intro Logo Skip"
+ModCategory	= "Other"
+ModDescription	= "Skip intro logos"
+GameVersion	= "3.66"
+ModVersion	= "1.0"
+
+NMS_MOD_DEFINITION_CONTAINER = {
+	["MOD_FILENAME"]	= ModAuthor.." - "..ModCategory.." - "..ModName.."-"..ModVersion..".pak",
+	["MOD_DESCRIPTION"]	= ModDescription,
+	["MOD_MAINTENANCE"]	= Maintainer,
+	["MOD_AUTHOR"]		= ModAuthor,
+	["LUA_AUTHOR"]		= LuaAuthor,
+	["NMS_VERSION"]		= GameVersion,
+	["MODIFICATIONS"]	= {{
+		["MBIN_CHANGE_TABLE"] = {{
+			["MBIN_FILE_SOURCE"]	= "METADATA\UI\BOOTLOGOPC.MBIN",
+			["EXML_CHANGE_TABLE"] 	= {{
+				["PRECEDING_KEY_WORDS"]	= {"DisplayTime"},
+				["REPLACE_TYPE"]	= "ALL",
+				["LINE_OFFSET"]		= "1",
+				["VALUE_CHANGE_TABLE"]	= {
+					{"IGNORE", "0"}		-- Original "3"
+				}
+			},{
+				["PRECEDING_KEY_WORDS"]	= {"DisplayTime"},
+				["REPLACE_TYPE"]	= "ALL",
+				["LINE_OFFSET"]		= "2",
+				["VALUE_CHANGE_TABLE"]	= {
+					{"IGNORE", "0"}		-- Original "2"
+				}
+			},{
+				["PRECEDING_KEY_WORDS"]	= {"DisplayTime"},
+				["REPLACE_TYPE"]	= "ALL",
+				["LINE_OFFSET"]		= "3",
+				["VALUE_CHANGE_TABLE"]	= {
+					{"IGNORE", "0"}		-- Original "1"
+				}
+			},{
+				["PRECEDING_KEY_WORDS"]	= {"DisplayTime"},
+				["REPLACE_TYPE"]	= "ALL",
+				["LINE_OFFSET"]		= "4",
+				["VALUE_CHANGE_TABLE"]	= {
+					{"IGNORE", "0"}		-- Original "1"
+				}
+			}}
+		}}
+	}}
+}
+-- The END of mod definition container
