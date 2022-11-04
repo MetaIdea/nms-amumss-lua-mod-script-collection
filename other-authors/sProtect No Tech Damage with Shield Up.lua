19d3b346d74497cc5282a18f4d37cbd3f9decded
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-11-04 08:02:29 -0600

    Create sProtect No Tech Damage with Shield Up.lua

---
+++ other-authors/sProtect No Tech Damage with Shield Up.lua
@@ -0,0 +1,32 @@
+Author = "Syzzle"
+ModName = "sProtect"
+ModNameSub = "No Tech Damage with Shield Up"
+BaseDescription = "Makes so your tech won't be breaking until your shield is down."
+GameVersion = "4.05"
+ModVersion = "v1"
+FileSource1 = "GCGAMEPLAYGLOBALS.GLOBAL.MBIN"
+
+NMS_MOD_DEFINITION_CONTAINER = {
+	["MOD_FILENAME"]	= ModName.." "..ModNameSub.." "..GameVersion..ModVersion..".pak",
+	["MOD_DESCRIPTION"]	= BaseDescription,
+	["MOD_AUTHOR"]		= Author,
+	["NMS_VERSION"]		= GameVersion,
+	["MODIFICATIONS"]	= {
+		{
+			["MBIN_CHANGE_TABLE"] = {
+				{
+					["MBIN_FILE_SOURCE"] = FileSource1,
+					["EXML_CHANGE_TABLE"] = {
+						{
+							["VALUE_CHANGE_TABLE"] = {
+								{"TechDamageChanceShieldLevelMin",0},		-- Original 0.01
+								{"TechDamageChanceShieldLevelMax",0},		-- Original 0.75
+								{"TechDamageChanceToSelectPrimary",1},		-- Original 0.1
+							},
+						},
+					},
+				},
+			}
+		}
+	}
+}
