bafd44978c59bf617198c9ed4011f4c436633e00
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-11-09 20:53:34 -0700

    DUD's LUA updates

--- Babscoole/JasonDudes Mods/_MOD_DUD_Asteroid_v4061.lua
+++ Babscoole/JasonDudes Mods/_MOD_DUD_Asteroid_v4061.lua
@@ -1,9 +1,9 @@
 NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"]    = "_MOD_DUD_Asteroid_v4031.pak", 
+["MOD_FILENAME"]    = "_MOD_DUD_Asteroid_v4061.pak", 
 ["MOD_AUTHOR"]      = "jasondude7116",
 ["LUA_AUTHOR"]      = "Babscoole",
-["NMS_VERSION"]     = "4.04",
+["NMS_VERSION"]     = "4.06",
 ["MOD_DESCRIPTION"] = "",
 ["MODIFICATIONS"]   = 
 	{
--- Babscoole/JasonDudes Mods/_MOD_DUD_TractorBeam_v4061.lua
+++ Babscoole/JasonDudes Mods/_MOD_DUD_TractorBeam_v4061.lua
@@ -1,9 +1,9 @@
 NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"]    = "_MOD_DUD_TractorBeam_v4031.pak", 
+["MOD_FILENAME"]    = "_MOD_DUD_TractorBeam_v4061.pak", 
 ["MOD_AUTHOR"]      = "jasondude7116",
 ["LUA_AUTHOR"]      = "Babscoole",
-["NMS_VERSION"]     = "4.05",
+["NMS_VERSION"]     = "4.06",
 ["MOD_DESCRIPTION"] = "",
 ["MODIFICATIONS"]   = 
 	{
