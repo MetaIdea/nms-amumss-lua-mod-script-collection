f1dae49b324bc807b9f3a160372ce148ec2c1afd
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-11-13 10:58:18 -0700

    4.06 updates

--- other-authors/shoemakers_exosuit_adjustments.lua
+++ other-authors/shoemakers_exosuit_adjustments.lua
@@ -3,8 +3,8 @@ NMS_MOD_DEFINITION_CONTAINER =
     ["MOD_FILENAME"] = "shoemakers Exosuit Adjustments.pak",
     ["MOD_AUTHOR"] = "shoemakerjones",
     ["LUA_AUTHOR"] = "shoemakerjones",
-    ["NMS_VERSION"] = "4.05",
-    ["MOD_DESCRIPTION"] = "Makes significant changes to they way movement is handled.",
+    ["NMS_VERSION"] = "4.06",
+    ["MOD_DESCRIPTION"] = "v1.1 - Makes significant changes to they way movement is handled.",
     ["MODIFICATIONS"] = 
     {
         {
@@ -17,34 +17,37 @@ NMS_MOD_DEFINITION_CONTAINER =
                         {
                             ["VALUE_CHANGE_TABLE"] = 
                             {
-                                {"GroundWalkSpeed", "1.6"},
-                                {"GroundRunSpeed", "8"},
-                                {"AnimWalkSpeed", "0.1"},
-                                {"JetpackUnderwaterFillRate", "4"},
-                                {"UnderwaterMaxJetpackEscapeSpeed", "16"},
-                                {"UnderwaterJetpackEscapeForce", "35"},
-                                {"FreeJetpackRange", "8"},
-                                {"FreeJetpackRangePrime", "8"},
-                                {"FreeJetpackRangeNonTerrain", "8"},
-                                {"MeleeBoostAirForce", "7"},
-                                {"MeleeToAirBoostInitialImpulse", "12"},
-                                {"MaxTimeInMeleeBoost", "2.8"},
-                                {"JetpackDrainHorizontalFactor", "1.38"},
-                                {"JetpackForce", "43"}, 
-                                {"JetpackBrake", "8.2"},
-                                {"JetpackMaxSpeed", "13"},
-                                {"JetpackMaxUpSpeed", "45"},
-                                {"JetpackUpForce", "50"},
-                                {"SpaceJetpackForce", "55"},
-                                {"SpaceJetpackUpForce", "45"},
-                                {"SpaceJetpackMaxSpeed", "17"},
-                                {"JetpackIgnitionForce", "80"},
-                                {"JetpackIgnitionTime", "0.1"},
-                                {"JetpackFillRate", "6.5"},
-                                {"JetpackFillRateHardMode", "1.5"},
-                                {"JetpackFillRateMidair", "4.5"},
-                                {"SpaceJetpackDrainRate", "0.12"},
-                                {"AbandonedFreighterRechargeMod", "1"},
+                                {"GroundWalkSpeed", "1.6"}, -- "4.4"
+                                {"GroundRunSpeed", "6"}, -- "8"
+                                {"AnimWalkSpeed", "0.1"}, -- "1.5"
+
+                                {"JetpackUnderwaterFillRate", "2"}, -- "1"
+                                {"UnderwaterMaxJetpackEscapeSpeed", "16"}, -- "8"
+                                {"UnderwaterJetpackEscapeForce", "40"}, -- "20"
+
+                                {"MeleeBoostAirForce", "6"}, -- "3"
+                                {"MeleeToAirBoostInitialImpulse", "10"}, -- "5"
+                                {"MaxTimeInMeleeBoost", "2.8"}, -- "1.4"
+
+                                {"JetpackDrainHorizontalFactor", "3.75"}, -- "2.5"
+                                {"JetpackForce", "47"},  -- "31"
+                                {"JetpackBrake", "3.3"}, -- "2.2"
+                                {"JetpackMaxSpeed", "7.5"}, -- "5"
+                                {"JetpackMaxUpSpeed", "45"}, -- "30"
+                                {"JetpackUpForce", "45"}, -- "30"
+                                {"JetpackIgnitionForce", "100"}, -- "60"
+                                {"JetpackIgnitionTime", "0.1"}, -- "0.4"
+                                {"JetpackFillRate", "1.0"}, -- "0.5"
+                                {"JetpackFillRateMidair", "1.0"}, -- "0.25"
+
+                                {"SpaceJetpackForce", "60"}, -- "40"
+                                {"SpaceJetpackUpForce", "45"}, -- "30"
+                                {"SpaceJetpackIgnitionForce", "23"}, -- "15"
+                                {"SpaceJetpackMaxSpeed", "15"}, -- "10"
+                                {"SpaceJetpackDrainRate", "0.12"}, -- "0.3"
+                                
+                                {"AbandonedFreighterRechargeMod", "1"}, -- "0.33"
+                                {"AutoSaveMaxTime", "15"}, -- "60"
                             }
                         }
                     }
