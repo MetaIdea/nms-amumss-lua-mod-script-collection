ad51234a289aed17d64f75913848575deb9335d9
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-11-20 23:42:20 -0700

    rgog

---
+++ Redmas/mods/src/rgog-lua/LUA/MAIN.lua
@@ -0,0 +1,14 @@
+
+
+-- MAIN ------------------------------------------------------------------------------------
+
+NMS_MOD_DEFINITION_CONTAINER =
+{
+  ["MOD_FILENAME"] 			= "___REDMAS_"..MODNAME..".pak",
+  ["MOD_DESCRIPTION"]		= "Redmas Game Overhaul Builder",
+  ["MOD_AUTHOR"]			= "Redmas",
+  ["NMS_VERSION"]			= "4.02+",
+  ["ADD_FILES"] 			= CUSTOM_CONTENT,
+  ["MODIFICATIONS"]   = FEATURES_CHANGE_TABLE
+}
+----
