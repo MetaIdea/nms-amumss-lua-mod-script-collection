202a6ddca2afc9df7d4a5eeb6d381f05ba3c4d93
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-11-23 18:40:56 -0700

    4.07 DUD's Updates

--- Babscoole/JasonDudes Mods/_MOD_DUD_AiShipPlusMoreDamage_v4071.lua
+++ Babscoole/JasonDudes Mods/_MOD_DUD_AiShipPlusMoreDamage_v4071.lua
@@ -1,9 +1,9 @@
 NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"]  = "_MOD_DUD_AiShipPlusMoreDamage_v4061.pak",
+["MOD_FILENAME"]  = "_MOD_DUD_AiShipPlusMoreDamage_v4071.pak",
 ["MOD_AUTHOR"]    = "jasondude7116",
 ["LUA_AUTHOR"]    = "Babscoole",
-["NMS_VERSION"]   = "4.06",
+["NMS_VERSION"]   = "4.07",
 ["MODIFICATIONS"] =
 	{
 		{
