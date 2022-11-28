d22d0ebe9ddf9d9a16ce3130a71e80b9928c6f5c
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-11-28 07:29:07 -0700

    Lo2K and Silent updates

--- Lo2k/Long Range Freighters 4.7.lua
+++ Lo2k/Long Range Freighters 4.7.lua
@@ -1,9 +1,9 @@
 NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"] 			= "Long Range Freighters 4.6.pak", 
+["MOD_FILENAME"] 			= "Long Range Freighters 4.7.pak", 
 ["MOD_AUTHOR"]				= "Lo2k",
 ["LUA_AUTHOR"]				= "Lo2k",
-["NMS_VERSION"]				= "4.04",
+["NMS_VERSION"]				= "4.07",
 ["MOD_DESCRIPTION"]			= "This mod multiply by 10 all freighter warp drive ranges",
 ["MODIFICATIONS"] 			= 
 	{
