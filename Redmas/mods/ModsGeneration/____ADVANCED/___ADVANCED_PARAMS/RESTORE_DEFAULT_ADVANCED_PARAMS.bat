738c6f99faf5c23de33f409c66d53f8bde07879a
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-11-29 08:39:30 -0700

    Update RESTORE_DEFAULT_ADVANCED_PARAMS.bat

--- Redmas/mods/ModsGeneration/____ADVANCED/___ADVANCED_PARAMS/RESTORE_DEFAULT_ADVANCED_PARAMS.bat
+++ Redmas/mods/ModsGeneration/____ADVANCED/___ADVANCED_PARAMS/RESTORE_DEFAULT_ADVANCED_PARAMS.bat
@@ -1,5 +1,5 @@
 @echo off
-copy ..\..\..\src\rgog-lua\LUA\REDMASPARAMS\ADVANCED_PARAMS.lua ..\..\localFiles\UserParams\ADVANCED_PARAMS.lua
+copy ..\..\..\src\rgog-lua\LUA\REDMASPARAMS\ADVANCED_PARAMS.lua ..\..\..\localFiles\UserParams\ADVANCED_PARAMS.lua
 cd ..\..\..\src\rgog-engine\tools\LITE
 start  lite.exe ..\..\..\..\localFiles\UserParams\ADVANCED_PARAMS.lua
 
