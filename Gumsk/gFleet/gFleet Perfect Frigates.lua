cf250de36ca30a0b0c3c91a52e5e1c464990e60b
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-12-19 08:43:04 -0700

    Update gFleet Perfect Frigates.lua

--- Gumsk/gFleet/gFleet Perfect Frigates.lua
+++ Gumsk/gFleet/gFleet Perfect Frigates.lua
@@ -2,8 +2,8 @@ Author = "unsick"
 ModName = "gFleet Perfect Frigates"
 ModNameSub = ""
 BaseDescription = ""
-GameVersion = "399"
-ModVersion = "b"
+GameVersion = "408"
+ModVersion = "a"
 FileSource1 = "GCFLEETGLOBALS.GLOBAL.MBIN"
 FileSource2 = "METADATA\REALITY\TABLES\FRIGATETRAITTABLE.MBIN"
 
@@ -23,23 +23,63 @@ NMS_MOD_DEFINITION_CONTAINER = {
 						{
 							["VALUE_CHANGE_TABLE"] = {
 								{"PercentChangeOfFrigateBeingPurchasable",100},			--60
-								{"MinFrigateStatValue",50},								--(-10)
 								{"PercentChanceOfFrigateAdditionalSpawnedTrait",100},	--55
+							},
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Combat","GcFrigateStatRange.xml"},
+							["REPLACE_TYPE"] = "ALL",
+							["VALUE_CHANGE_TABLE"] = {
+								{"Minimum",50},
+								{"Maximum",50},
+							},
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Exploration","GcFrigateStatRange.xml"},
+							["REPLACE_TYPE"] = "ALL",
+							["VALUE_CHANGE_TABLE"] = {
+								{"Minimum",50},
+								{"Maximum",50},
+							},
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Mining","GcFrigateStatRange.xml"},
+							["REPLACE_TYPE"] = "ALL",
+							["VALUE_CHANGE_TABLE"] = {
+								{"Minimum",50},
+								{"Maximum",50},
+							},
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Diplomatic","GcFrigateStatRange.xml"},
+							["REPLACE_TYPE"] = "ALL",
+							["VALUE_CHANGE_TABLE"] = {
+								{"Minimum",50},
+								{"Maximum",50},
 							},
 						},
 						{
 							["SPECIAL_KEY_WORDS"] = {"FuelBurnRate","GcFrigateStatRange.xml"},
 							["REPLACE_TYPE"] = "ALL",
-							["MATH_OPERATION"] = "*FB:Minimum", 
 							["VALUE_CHANGE_TABLE"] = {
-								{"Maximum",1},
+								{"Minimum",0},
+								{"Maximum",0},
 							},
 						},
 						{
+							["SPECIAL_KEY_WORDS"] = {"FuelCapacity","GcFrigateStatRange.xml"},
 							["REPLACE_TYPE"] = "ALL",
-							["MATH_OPERATION"] = "*F:Maximum", 
 							["VALUE_CHANGE_TABLE"] = {
-								{"Minimum",1},
+								{"Minimum",50},
+								{"Maximum",50},
+							},
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {"Speed","GcFrigateStatRange.xml"},
+							["REPLACE_TYPE"] = "ALL",
+							["VALUE_CHANGE_TABLE"] = {
+								{"Minimum",50},
+								{"Maximum",50},
 							},
 						},
 					},
