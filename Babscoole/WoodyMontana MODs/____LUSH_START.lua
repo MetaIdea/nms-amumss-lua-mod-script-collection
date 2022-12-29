ce4f52b5c6bdb5ca1683eb3cd96f687433fcd382
Babscoole <82686995+Babscoole@users.noreply.github.com>
2022-12-28 19:15:28 -0700

    WoodyMontana updates

---
+++ Babscoole/WoodyMontana MODs/____LUSH_START.lua
@@ -0,0 +1,40 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"]  = "____LUSH_START.pak",
+["MOD_AUTHOR"]    = "WoodyMontana",
+["LUA_AUTHOR"]    = "Babscoole",
+["NMS_VERSION"]   = "4.08",
+["MODIFICATIONS"] = 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] = 
+			{ 
+				{  
+					["MBIN_FILE_SOURCE"]  = "METADATA\SIMULATION\SOLARSYSTEM\BIOMES\BIOMEFILENAMES.MBIN",
+					["EXML_CHANGE_TABLE"] = 
+					{				
+						{
+							["PRECEDING_FIRST"] = "TRUE",
+							["PRECEDING_KEY_WORDS"] = {"ValidStartPlanetBiome"},
+							["FOREACH_SKW_GROUP"] = 
+							{
+								{"Biome", "Scorched"},
+								{"Biome", "Radioactive"},
+								{"Biome", "Frozen"},			
+							},
+							["REMOVE"] = "SECTION",
+						},
+						{
+							["PRECEDING_FIRST"] = "TRUE",
+							["PRECEDING_KEY_WORDS"] = {"ValidStartPlanetBiome"},
+							["VALUE_CHANGE_TABLE"] = 
+							{
+								{"Biome", "Lush"},
+							} 
+						},
+					}
+				},				
+			}
+		},
+	}	
+}
