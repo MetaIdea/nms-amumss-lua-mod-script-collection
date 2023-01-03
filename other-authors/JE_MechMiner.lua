3a2d2fb8a1afce84d5976c48ef125127a3409416
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-01-03 16:40:49 -0700

    JE's adds

---
+++ other-authors/JE_MechMiner.lua
@@ -0,0 +1,40 @@
+Author = "Jaggid Edje"
+ModName = "MechMiner"
+ModNameSub = "JE"
+BaseDescription = "Adjusts the fuel maximum and charge of the Minotaur Bore"
+GameVersion = "4.07"
+ModVersion = "v1.00"
+FileSource1 = "METADATA/REALITY/TABLES/NMS_REALITY_GCTECHNOLOGYTABLE.MBIN"
+
+MB_Charge = 1000 -- default is 200
+MB_Multipler = 5  -- Default is 1
+
+NMS_MOD_DEFINITION_CONTAINER =
+{
+    ["MOD_FILENAME"]    = ModNameSub.."_"..ModName..".pak",
+    ["MOD_DESCRIPTION"]    = BaseDescription,
+    ["MOD_AUTHOR"]        = Author,
+    ["NMS_VERSION"]        = GameVersion,
+    ["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] = {
+                {
+                    ["MBIN_FILE_SOURCE"] = FileSource1,
+                    ["EXML_CHANGE_TABLE"] = {					
+						{
+							["SPECIAL_KEY_WORDS"]	= {"ID", "MECH_MINER"},
+							["REPLACE_TYPE"] 		= "ALL",
+							["VALUE_CHANGE_TABLE"] 	=
+							{
+								{"ChargeAmount",	MB_Charge},
+								{"ChargeMultiplier",	MB_Multipler}
+							}
+						}
+						
+                    }
+                }
+			}
+        }
+    }
+}
