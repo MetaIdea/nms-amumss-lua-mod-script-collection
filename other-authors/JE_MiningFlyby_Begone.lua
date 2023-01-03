6abbbf09bd8f93fd644397e7a953e23d922ad4f0
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-01-03 16:40:49 -0700

    JE's adds

---
+++ other-authors/JE_MiningFlyby_Begone.lua
@@ -0,0 +1,40 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "JE_MiningFlyby_Begone.pak", 
+["MOD_AUTHOR"]				= "Jaggid Edje",
+["LUA_AUTHOR"]				= "Jaggid Edje",
+["NMS_VERSION"]				= "4.07",
+["MOD_DESCRIPTION"]			= "Adjusts the frequency and distance of various flyby types within the game",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{ 
+					["MBIN_FILE_SOURCE"] 	= "METADATA\SIMULATION\SCENE\EXPERIENCESPAWNTABLE.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["PRECEDING_KEY_WORDS"] = {"MiningFlybySpawns", "Count" },
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"x",	"0"}, 		-- Original "1"
+								{"y",	"0"} 		-- Original "1"
+							}
+						},
+						{
+							["PRECEDING_KEY_WORDS"]  = {"MiningFlybySpawns"},
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"MinRange",	"1000"},  --original : 500
+							}
+						}
+					}
+				}
+			}
+		}
+	}	
+}
+
+
+  
