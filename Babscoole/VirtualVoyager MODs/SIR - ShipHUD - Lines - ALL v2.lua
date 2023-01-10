e7e0154512c3b511b3d26e3dd0d6b6f521bf5660
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-01-10 09:32:44 -0700

    Create SIR - ShipHUD - Lines - ALL v2.lua

---
+++ Babscoole/VirtualVoyager MODs/SIR - ShipHUD - Lines - ALL v2.lua
@@ -0,0 +1,282 @@
+-- Data section
+
+-- default values
+NOLINES = false
+COLORCHOICE = "BLUE"
+
+
+ColorTables = {
+BLUE =
+{
+	{"SpaceFriendlyShipLineColour", {0,1,0,0.8}},
+	{"SpaceEnemyShipLineColour", {1,0,0,0.8}},
+	{"SpaceMapAttackColour", {1,0,0,0.8}},
+	{"SpaceMapOtherPlayerColour", {0,0,1,0.8}},
+	{"SpaceMapThreatColour", {1,0,0,0.8}},
+	{"SpaceMapNeutralColour", {0,0,1,0.8}},
+	{"SpaceMapSquadronColour", {1,1,0,0.8}},
+	{"SpaceMapDeathPointColour", {0,0,1,0.8}},
+	{"SpaceMapMarkerData", {0,0,1,0.8}},
+	{"SpaceMapPlanetData", {0,0,1,0.8}},
+	{"SpaceMapStationData", {0,0,1,0.8}},
+	{"SpaceMapNexusData", {0,0,1,0.8}},
+	{"SpaceMapAtlasData", {0,0,1,0.8}},
+	{"SpaceMapBlackHoleData", {0,0,1,0.8}},
+	{"SpaceMapShipData", {0,0,1,0.8}},
+	{"SpaceMapFreighterData", {0,0,1,0.8}},
+	{"SpaceMapPulseEncounterData", {0,0,1,0.8}},
+},
+
+CYAN =
+{
+	{"SpaceFriendlyShipLineColour", {0,1,0,0.8}},
+	{"SpaceEnemyShipLineColour", {1,0,0,0.8}},
+	{"SpaceMapAttackColour", {1,0,0,0.8}},
+	{"SpaceMapOtherPlayerColour", {0,1,1,0.8}},
+	{"SpaceMapThreatColour", {1,0,0,0.8}},
+	{"SpaceMapNeutralColour", {0,1,1,0.8}},
+	{"SpaceMapSquadronColour", {1,1,0,0.8}},
+	{"SpaceMapDeathPointColour", {0,1,1,0.8}},
+	{"SpaceMapMarkerData", {0,1,1,0.8}},
+	{"SpaceMapPlanetData", {0,1,1,0.8}},
+	{"SpaceMapStationData", {0,1,1,0.8}},
+	{"SpaceMapNexusData", {0,1,1,0.8}},
+	{"SpaceMapAtlasData", {0,1,1,0.8}},
+	{"SpaceMapBlackHoleData", {0,1,1,0.8}},
+	{"SpaceMapShipData", {0,1,1,0.8}},
+	{"SpaceMapFreighterData", {0,1,1,0.8}},
+	{"SpaceMapPulseEncounterData", {0,1,1,0.8}},
+},
+
+GREEN =
+{
+	{"SpaceFriendlyShipLineColour", {0,1,1,0.8}},
+	{"SpaceEnemyShipLineColour", {1,0,0,0.8}},
+	{"SpaceMapAttackColour", {1,0,0,0.8}},
+	{"SpaceMapOtherPlayerColour", {0,1,0,0.8}},
+	{"SpaceMapThreatColour", {1,0,0,0.8}},
+	{"SpaceMapNeutralColour", {0,1,0,0.8}},
+	{"SpaceMapSquadronColour", {1,1,0,0.8}},
+	{"SpaceMapDeathPointColour", {0,1,0,0.8}},
+	{"SpaceMapMarkerData", {0,1,0,0.8}},
+	{"SpaceMapPlanetData", {0,1,0,0.8}},
+	{"SpaceMapStationData", {0,1,0,0.8}},
+	{"SpaceMapNexusData", {0,1,0,0.8}},
+	{"SpaceMapAtlasData", {0,1,0,0.8}},
+	{"SpaceMapBlackHoleData", {0,1,0,0.8}},
+	{"SpaceMapShipData", {0,1,0,0.8}},
+	{"SpaceMapFreighterData", {0,1,0,0.8}},
+	{"SpaceMapPulseEncounterData", {0,1,0,0.8}},
+},
+
+PINK =
+{
+	{"SpaceFriendlyShipLineColour", {0,1,0,0.8}},
+	{"SpaceEnemyShipLineColour", {1,0,0,0.8}},
+	{"SpaceMapAttackColour", {1,0,0,0.8}},
+	{"SpaceMapOtherPlayerColour", {1,0,1,0.8}},
+	{"SpaceMapThreatColour", {1,0,0,0.8}},
+	{"SpaceMapNeutralColour", {1,0,1,0.8}},
+	{"SpaceMapSquadronColour", {1,1,0,0.8}},
+	{"SpaceMapDeathPointColour", {1,0,1,0.8}},
+	{"SpaceMapMarkerData", {1,0,1,0.8}},
+	{"SpaceMapPlanetData", {1,0,1,0.8}},
+	{"SpaceMapStationData", {1,0,1,0.8}},
+	{"SpaceMapNexusData", {1,0,1,0.8}},
+	{"SpaceMapAtlasData", {1,0,1,0.8}},
+	{"SpaceMapBlackHoleData", {1,0,1,0.8}},
+	{"SpaceMapShipData", {1,0,1,0.8}},
+	{"SpaceMapFreighterData", {1,0,1,0.8}},
+	{"SpaceMapPulseEncounterData", {1,0,1,0.8}},
+},
+
+PURPLE =
+{
+	{"SpaceFriendlyShipLineColour", {0,1,0,0.8}},
+	{"SpaceEnemyShipLineColour", {1,0,0,0.8}},
+	{"SpaceMapAttackColour", {1,0,0,0.8}},
+	{"SpaceMapOtherPlayerColour", {0.5,0,0.5,0.8}},
+	{"SpaceMapThreatColour", {1,0,0,0.8}},
+	{"SpaceMapNeutralColour", {0.5,0,0.5,0.8}},
+	{"SpaceMapSquadronColour", {1,1,0,0.8}},
+	{"SpaceMapDeathPointColour", {0.5,0,0.5,0.8}},
+	{"SpaceMapMarkerData", {0.5,0,0.5,0.8}},
+	{"SpaceMapPlanetData", {0.5,0,0.5,0.8}},
+	{"SpaceMapStationData", {0.5,0,0.5,0.8}},
+	{"SpaceMapNexusData", {0.5,0,0.5,0.8}},
+	{"SpaceMapAtlasData", {0.5,0,0.5,0.8}},
+	{"SpaceMapBlackHoleData", {0.5,0,0.5,0.8}},
+	{"SpaceMapShipData", {0.5,0,0.5,0.8}},
+	{"SpaceMapFreighterData", {0.5,0,0.5,0.8}},
+	{"SpaceMapPulseEncounterData", {0.5,0,0.5,0.8}},
+},
+
+RED =
+{
+	{"SpaceFriendlyShipLineColour", {0,1,0,0.8}},
+	{"SpaceEnemyShipLineColour", {1,1,1,0.8}},
+	{"SpaceMapAttackColour", {1,1,1,0.8}},
+	{"SpaceMapOtherPlayerColour", {1,0,0,0.8}},
+	{"SpaceMapThreatColour", {1,1,1,0.8}},
+	{"SpaceMapNeutralColour", {1,0,0,0.8}},
+	{"SpaceMapSquadronColour", {1,1,0,0.8}},
+	{"SpaceMapDeathPointColour", {1,0,0,0.8}},
+	{"SpaceMapMarkerData", {1,0,0,0.8}},
+	{"SpaceMapPlanetData", {1,0,0,0.8}},
+	{"SpaceMapStationData", {1,0,0,0.8}},
+	{"SpaceMapNexusData", {1,0,0,0.8}},
+	{"SpaceMapAtlasData", {1,0,0,0.8}},
+	{"SpaceMapBlackHoleData", {1,0,0,0.8}},
+	{"SpaceMapShipData", {1,0,0,0.8}},
+	{"SpaceMapFreighterData", {1,0,0,0.8}},
+	{"SpaceMapPulseEncounterData", {1,0,0,0.8}},
+},
+
+WHITE =
+{
+	{"SpaceFriendlyShipLineColour", {0,1,0,0.8}},
+	{"SpaceEnemyShipLineColour", {1,0,0,0.8}},
+	{"SpaceMapAttackColour", {1,0,0,0.8}},
+	{"SpaceMapOtherPlayerColour", {1,1,1,0.8}},
+	{"SpaceMapThreatColour", {1,0,0,0.8}},
+	{"SpaceMapNeutralColour", {1,1,1,0.8}},
+	{"SpaceMapSquadronColour", {1,1,0,0.8}},
+	{"SpaceMapDeathPointColour", {1,1,1,0.8}},
+	{"SpaceMapMarkerData", {1,1,1,0.8}},
+	{"SpaceMapPlanetData", {1,1,1,0.8}},
+	{"SpaceMapStationData", {1,1,1,0.8}},
+	{"SpaceMapNexusData", {1,1,1,0.8}},
+	{"SpaceMapAtlasData", {1,1,1,0.8}},
+	{"SpaceMapBlackHoleData", {1,1,1,0.8}},
+	{"SpaceMapShipData", {1,1,1,0.8}},
+	{"SpaceMapFreighterData", {1,1,1,0.8}},
+	{"SpaceMapPulseEncounterData", {1,1,1,0.8}},
+},
+
+YELLOW =
+{
+	{"SpaceFriendlyShipLineColour", {0,1,0,0.8}},
+	{"SpaceEnemyShipLineColour",    {1,0,0,0.8}},
+	{"SpaceMapAttackColour",        {1,0,0,0.8}},
+	{"SpaceMapOtherPlayerColour",   {1,1,0,0.8}},
+	{"SpaceMapThreatColour",        {1,0,0,0.8}},
+	{"SpaceMapNeutralColour",       {1,1,0,0.8}},
+	{"SpaceMapSquadronColour",      {0,1,1,0.8}},
+	{"SpaceMapDeathPointColour",    {1,1,0,0.8}},
+	{"SpaceMapMarkerData",          {1,1,0,0.8}},
+	{"SpaceMapPlanetData",          {1,1,0,0.8}},
+	{"SpaceMapStationData",         {1,1,0,0.8}},
+	{"SpaceMapNexusData",           {1,1,0,0.8}},
+	{"SpaceMapAtlasData",           {1,1,0,0.8}},
+	{"SpaceMapBlackHoleData",       {1,1,0,0.8}},
+	{"SpaceMapShipData",            {1,1,0,0.8}},
+	{"SpaceMapFreighterData",       {1,1,0,0.8}},
+	{"SpaceMapPulseEncounterData",  {1,1,0,0.8}},
+},
+}
+
+InputCOLORCHOICE = {COLORCHOICE,
+[[
+    Choose a color option?  Options are BLUE, CYAN, GREEN, PINK, PURPLE, RED, WHITE, or YELLOW.
+    Default = BLUE | Current = >> ]] .. (COLORCHOICE) .. [[ <<
+]]}
+
+InputNOLINESCHOICE = {NOLINES,
+[[
+    Do you wish to use the No Lines option?
+    Default = false | Current = >> ]] .. (NOLINES and "Y" or "N") .. [[ <<
+]]}
+-- END: Data section
+
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"]    = "SIR - ShipHUD - Lines - ALL.pak",
+["MOD_DESCRIPTION"] = "",
+["MOD_AUTHOR"]      = "VirtualVoyager",
+["LUA_AUTHOR"]      = "Babscoole",
+["NMS_VERSION"]     = "4.08",
+["MODIFICATIONS"]   = 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] = 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"]  = "GCUIGLOBALS.GLOBAL.MBIN",
+					["EXML_CHANGE_TABLE"] = 
+					{
+						{
+							["VALUE_CHANGE_TABLE"] = 	
+							{
+								{"SpaceMapShowPlanetLines", "False"},
+							}
+						},
+					}
+				}
+			}
+		}
+	}	
+}
+
+-- START of processing
+NOLINES = GUIF(InputNOLINESCHOICE)
+-- print("NOLINES = "..tostring(NOLINES))
+
+while not NOLINES do
+  -- Select Color Variation from BLUE, CYAN, GREEN, PINK, PURPLE, RED, WHITE or YELLOW -- choice is already made for NOLINES
+  -- COLORCHOICE = "BLUE"
+  
+  COLORCHOICE = GUIF(InputCOLORCHOICE):upper()
+-- print("COLORCHOICE = "..tostring(COLORCHOICE))
+  
+  if ColorTables[COLORCHOICE] == nil then  -- when COLORCHOICE is NOT in the table
+    print("         >>> ["..COLORCHOICE.."] is NOT a valid color choice, please retry! <<<")
+  else
+    -- user selected one of the good colors
+    break
+  end
+end
+
+local Change_Table_Array = NMS_MOD_DEFINITION_CONTAINER["MODIFICATIONS"][1]["MBIN_CHANGE_TABLE"][1]["EXML_CHANGE_TABLE"]
+
+if NOLINES then
+  local temp_table =
+  {
+      ["VALUE_CHANGE_TABLE"]  =
+      {
+          {"SpaceMapLineWidth",     "0"},
+          {"SpaceMapLineBaseScale", "0"},
+          {"SpaceMapLineBaseFade",  "0"},
+      },
+  }
+  Change_Table_Array[#Change_Table_Array + 1] = temp_table
+        
+else
+  local ColorTable = ColorTables[COLORCHOICE]
+
+  for i = 1, #ColorTable do
+    local PK = ColorTable[i][1]
+-- print("PK = ["..tostring(PK).."]")
+
+    local TableData = ColorTable[i][2]
+
+    local R = TableData[1]
+    local B = TableData[2]
+    local G = TableData[3]
+    local A = TableData[4]
+  
+    local temp_table =
+    {
+        ["PRECEDING_KEY_WORDS"] = {PK},
+        ["INTEGER_TO_FLOAT"] = "FORCE",
+        ["VALUE_CHANGE_TABLE"]  =
+        {
+            {"R",R},
+            {"G",G},
+            {"B",B},
+            {"A",A},
+        },
+    }
+    Change_Table_Array[#Change_Table_Array + 1] = temp_table
+  end
+  
+end
