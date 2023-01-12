88d3f9c30ee604119c551305c4cafd244ee27de1
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-01-12 15:36:38 -0700

    Update DaPoPo add JE FrigateFuel

---
+++ other-authors/JE_FrigateFuel.lua
@@ -0,0 +1,63 @@
+Author = "Jaggid Edje"
+ModName = "FrigateFuel"
+ModNameSub = "JE"
+BaseDescription = "Adjusts frigate fuel useage maximums to be the same as minimum for all frigate types"
+GameVersion = "4.08"
+ModVersion = "v4.08"
+FileSource1 = "GCFLEETGLOBALS.GLOBAL.MBIN"
+
+
+
+
+NMS_MOD_DEFINITION_CONTAINER =
+{
+    ["MOD_FILENAME"]    = ModNameSub.."_"..ModName..".pak",
+    ["MOD_DESCRIPTION"]    = BaseDescription,
+    ["MOD_AUTHOR"]        = Author,
+    ["NMS_VERSION"]        = GameVersion,
+    ["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] = {
+                {
+                    ["MBIN_FILE_SOURCE"] = FileSource1,
+                    ["EXML_CHANGE_TABLE"] = {
+                        
+						{
+                            ["PRECEDING_KEY_WORDS"] = {"FuelBurnRate",}, 
+                            ["REPLACE_TYPE"] = "ALL",
+							["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"Maximum", 8} -- 12
+                            }
+                        },
+						{
+                            ["PRECEDING_KEY_WORDS"] = {"FrigateInitialStats","FrigateClass","Support","Stats","FuelBurnRate",}, 
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"Maximum", 2} -- 5
+                            }
+                        },
+						{
+                            ["PRECEDING_KEY_WORDS"] = {"FrigateInitialStats","FrigateClass","DeepSpace","Stats","FuelBurnRate",}, 
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"Minimum", 6},
+								{"Maximum", 6} -- 8
+                            }
+                        },
+						{
+                            ["PRECEDING_KEY_WORDS"] = {"FrigateInitialStats","FrigateClass","DeepSpaceCommon","Stats","FuelBurnRate",}, 
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"Minimum", 6}, -- 8
+								{"Maximum", 6} -- 12
+                            }
+                        }
+						
+                    }
+                }
+            }
+        }
+    }
+}
