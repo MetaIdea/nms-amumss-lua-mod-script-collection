4f66c37fa18c66e8ebc6141f8a4edf73b855f769
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-01-12 15:36:38 -0700

    Update DaPoPo add JE FrigateFuel

--- WinderTP/Winder_DaPoPoBeFastOnYoAss.lua
+++ WinderTP/Winder_DaPoPoBeFastOnYoAss.lua
@@ -42,6 +42,24 @@ MULTIPLIER_TABLE =
 	},
 }
 
+SHIP_MULTIPLIER =
+{
+	["Standard"] = 
+	{
+		["AIShipRole"] = "Standard",
+		["MinAmount"] = 2.5,
+		["MaxAmount"] = 6.5,			
+	},
+}
+
+FREIGHT_SHIP_MULTIPLIER =
+{
+	["Freighter"] = 
+	{
+		["AIShipRole"] = "Freighter",		
+	},
+}
+
 function GetSentinelTypeMultiply(TYPE, MIN, MAX)
 return
 	{
@@ -60,13 +78,169 @@ return
 	}
 end
 
+function GetSentinelShipTypeMultiply(TYPE, MIN, MAX)
+return
+	{
+		["PRECEDING_KEY_WORDS"] = {"PoliceSpawns"},
+		["SPECIAL_KEY_WORDS"] = {"AIShipRole", TYPE},
+		["PRECEDING_FIRST"] = "TRUE",
+		["INTEGER_TO_FLOAT"] = "PRESERVE",
+		["SECTION_UP"] = 1,
+		["MATH_OPERATION"] = "*",
+		["REPLACE_TYPE"] = "ALL",
+		["WISUBSEC_OPTION"] = "ALL",
+		["WHERE_IN_SUBSECTION"] = 
+		{
+			{"Count", "Vector2f.xml"},
+		},
+		["VALUE_CHANGE_TABLE"] 	= 
+		{
+			{"x",	MIN},
+			{"y",	MAX},
+		}
+	}
+end
+
+function GetSentinelShipSpreadMultiply(TYPE, MIN)
+return
+	{
+		["PRECEDING_KEY_WORDS"] = {"PoliceSpawns"},
+		["SPECIAL_KEY_WORDS"] = {"AIShipRole", TYPE},
+		["PRECEDING_FIRST"] = "TRUE",
+		["INTEGER_TO_FLOAT"] = "PRESERVE",
+		["SECTION_UP"] = 1,
+		["MATH_OPERATION"] = "*",
+		["REPLACE_TYPE"] = "ALL",
+		["WISUBSEC_OPTION"] = "ALL",
+		["WHERE_IN_SUBSECTION"] = 
+		{
+			{"Spread", "Vector2f.xml"},
+		},
+		["VALUE_CHANGE_TABLE"] 	= 
+		{
+			{"x",	MIN},
+			{"y",	MIN},
+		}
+	}
+end
+
+function GetSentinelFreighterTypeMultiply(TYPE)
+return
+	{
+		["PRECEDING_KEY_WORDS"] = {"PoliceSpawns"},
+		["SPECIAL_KEY_WORDS"] = {"AIShipRole", TYPE},
+		["PRECEDING_FIRST"] = "TRUE",
+		-- ["INTEGER_TO_FLOAT"] = "PRESERVE",
+		["SECTION_UP"] = 1,
+		-- ["MATH_OPERATION"] = "*",
+		["REPLACE_TYPE"] = "RAW",
+		["VALUE_CHANGE_TABLE"] 	= 
+		{
+			{ 
+			[[<Property name="ChildSpawns" />]], 
+			[[
+      <Property name="ChildSpawns">
+        <Property value="GcAIShipSpawnData.xml">
+		  <Property name="Message" value="" />
+		  <Property name="CombatMessage" value="" />
+		  <Property name="OSDMessage" value="" />
+		  <Property name="Shortcut" value="TkInputEnum.xml">
+			<Property name="InputButton" value="None" />
+		  </Property>
+		  <Property name="WarpIn" value="True" />
+		  <Property name="Role" value="GcAISpaceshipRoles.xml">
+			<Property name="AIShipRole" value="Standard" />
+		  </Property>
+		  <Property name="MinRange" value="800" />
+		  <Property name="Scale" value="Vector2f.xml">
+			<Property name="x" value="1" />
+			<Property name="y" value="1" />
+		  </Property>
+		  <Property name="Reward" value="" />
+		  <Property name="RewardMessage" value="" />
+		  <Property name="MarkerData" value="GcAIShipSpawnMarkerData.xml">
+			<Property name="MarkerIcon" value="TkTextureResource.xml">
+			  <Property name="Filename" value="" />
+			  <Property name="ResHandle" value="GcResource.xml">
+				<Property name="ResourceID" value="0" />
+			  </Property>
+			</Property>
+			<Property name="MarkerLabel" value="" />
+			<Property name="HideDuringCombat" value="False" />
+			<Property name="MinVisibleRange" value="0" />
+			<Property name="MaxVisibleRange" value="10000" />
+			<Property name="MinAngleVisible" value="0" />
+			<Property name="ShipsToMark" value="None" />
+		  </Property>
+		  <Property name="AttackFreighter" value="False" />
+		  <Property name="Spread" value="Vector2f.xml">
+			<Property name="x" value="375" />
+			<Property name="y" value="375" />
+		  </Property>
+		  <Property name="Count" value="Vector2f.xml">
+			<Property name="x" value="7" />
+			<Property name="y" value="7" />
+		  </Property>
+		  <Property name="StartTime" value="Vector2f.xml">
+			<Property name="x" value="1" />
+			<Property name="y" value="3" />
+		  </Property>
+		  <Property name="SpawnShape" value="Cone" />
+		  <Property name="Performances" value="GcShipAIPerformanceArray.xml">
+			<Property name="Array" />
+		  </Property>
+		  <Property name="ChildSpawns" />
+		</Property>
+	</Property>]]
+			}
+		}
+	}
+end
+
+function GetStationShipMultiply(MULT)
+return
+{
+	["MBIN_FILE_SOURCE"] 	= "GCAISPACESHIPGLOBALS.GLOBAL.MBIN",
+	["EXML_CHANGE_TABLE"]	= 
+	{
+		{
+			["MATH_OPERATION"] = "*",
+			["REPLACE_TYPE"] = "ONCE",
+			["VALUE_CHANGE_TABLE"] 	= 
+			{
+				{"PoliceStationNumToLaunch",	MULT},
+			},
+		}
+	}
+}
+end
+
 CHANGE_TABLE = {}
 MINEST_MIN = 1
+
+AISPACESHIP_CHANGE = { true, [[Multiply Space Station Sentinel ship spawn?
+It will modify GCAISPACESHIPGLOBALS, remember to check for mod conflicts!]] }
+
+INJECT_DECISION = GUIF(AISPACESHIP_CHANGE)
+MBIN_CHANGE = {}
+if INJECT_DECISION then
+	STATIONSHIP_MULT = SHIP_MULTIPLIER["Standard"]["MinAmount"]
+	table.insert(MBIN_CHANGE, GetStationShipMultiply(STATIONSHIP_MULT))
+end
 
 for _,j in pairs(MULTIPLIER_TABLE) do
 	table.insert(CHANGE_TABLE, GetSentinelTypeMultiply(j["Type"],j["MinAmount"],j["MaxAmount"]))
 	if j["MinAmount"] < MINEST_MIN then MINEST_MIN = j["MinAmount"]
 	end
+end
+
+for _,j in pairs(FREIGHT_SHIP_MULTIPLIER) do
+	table.insert(CHANGE_TABLE, GetSentinelFreighterTypeMultiply(j["AIShipRole"]))
+end
+
+for _,j in pairs(SHIP_MULTIPLIER) do
+	table.insert(CHANGE_TABLE, GetSentinelShipTypeMultiply(j["AIShipRole"],j["MinAmount"],j["MaxAmount"]))
+	table.insert(CHANGE_TABLE, GetSentinelShipSpreadMultiply(j["AIShipRole"],j["MinAmount"]))
 end
 
 if MINEST_MIN > 1 then
@@ -82,24 +256,24 @@ if MINEST_MIN > 1 then
 			}	
 	})
 end
+
+table.insert(MBIN_CHANGE, 
+{
+	["MBIN_FILE_SOURCE"] 	= "METADATA\SIMULATION\SCENE\EXPERIENCESPAWNTABLE.MBIN",
+	["EXML_CHANGE_TABLE"]	= CHANGE_TABLE
+})
 	
 NMS_MOD_DEFINITION_CONTAINER = 
 {
 ["MOD_FILENAME"] 		= "DaPoPoBeFastOnYoAss.pak",
 ["MOD_BATCHNAME"]		= "~~~DaPoPoBeFastOnYoAss_COMPATIBILITY.pak",
 ["MOD_DESCRIPTION"]		= "Spawns MORE sentinels",
-["MOD_AUTHOR"]			= "WinderTP",
+["MOD_AUTHOR"]			= "WinderTP, lyravega",
 ["NMS_VERSION"]			= "3.90",
 ["MODIFICATIONS"] 		= 
 	{
 		{
-			["MBIN_CHANGE_TABLE"] = 
-			{ 
-				{
-					["MBIN_FILE_SOURCE"] 	= "METADATA\SIMULATION\SCENE\EXPERIENCESPAWNTABLE.MBIN",
-					["EXML_CHANGE_TABLE"]	= CHANGE_TABLE
-				}
-			}
+			["MBIN_CHANGE_TABLE"] = MBIN_CHANGE
 		}
 	},
 }
