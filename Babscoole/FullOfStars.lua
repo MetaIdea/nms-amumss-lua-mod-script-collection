903e36a54fa208889ddcdcefba7eab2d1912bc10
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-01-14 13:50:30 -0700

    Create FullOfStars.lua

---
+++ Babscoole/FullOfStars.lua
@@ -0,0 +1,28 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"]  = "FullOfStars.pak",
+["MOD_AUTHOR"]    = "darkmessenger84",
+["LUA_AUTHOR"]    = "Babscoole",
+["NMS_VERSION"]   = "4.08",
+["MODIFICATIONS"] = 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] = 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"]  = "GCGALAXYGLOBALS.GLOBAL.MBIN",
+					["EXML_CHANGE_TABLE"] = 
+					{
+						{
+							["INTEGER_TO_FLOAT"] = "FORCE",							
+							["VALUE_CHANGE_TABLE"] = 
+							{
+								{"StarGenerationThreshold",		"0"}, -- Original "0.68"
+							}
+						},					
+					}
+				},
+			}
+		},
+	}	
+}
