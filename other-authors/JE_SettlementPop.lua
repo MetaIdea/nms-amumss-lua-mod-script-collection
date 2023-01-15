6406d213037bc6f98ab132ca936d9980100eeb27
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-01-14 21:37:19 -0700

    Create JE_SettlementPop.lua

---
+++ other-authors/JE_SettlementPop.lua
@@ -0,0 +1,63 @@
+Author = "Jaggid Edje"
+ModName = "SettlementPop"
+ModNameSub = "JE"
+BaseDescription = "Increases population gain from Settlement events"
+GameVersion = "4.08"
+ModVersion = "v4.08"
+FileSource1 = "GCSETTLEMENTGLOBALS.MBIN"
+
+
+
+
+NMS_MOD_DEFINITION_CONTAINER =
+{
+    ["MOD_FILENAME"]    = ModNameSub.."_"..ModName..".pak",
+    ["MOD_DESCRIPTION"]    = BaseDescription,
+    ["MOD_AUTHOR"]        = Author,
+    ["NMS_VERSION"]        = GameVersion,
+    ["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] = {
+                {
+                    ["MBIN_FILE_SOURCE"] = FileSource1,
+                    ["EXML_CHANGE_TABLE"] = {
+                        {
+                            ["PRECEDING_KEY_WORDS"] = {"PerkStatStrengthValues","Population","PerkStatStrengthValues","PositiveWide"}, 
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"AmountMin", 5}, -- 1
+								{"AmountMax", 12} -- 5
+                            }
+                        },
+						{
+                            ["PRECEDING_KEY_WORDS"] = {"PerkStatStrengthValues","Population","PerkStatStrengthValues","PositiveLarge"}, 
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"AmountMin", 10}, -- 3
+								{"AmountMax", 12}  -- 5
+                            }
+                        },
+						{
+                            ["PRECEDING_KEY_WORDS"] = {"PerkStatStrengthValues","Population","PerkStatStrengthValues","PositiveMedium"}, 
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"AmountMin", 8}, -- 2
+								{"AmountMax", 9} -- 3
+                            }
+                        },
+						{
+                            ["PRECEDING_KEY_WORDS"] = {"PerkStatStrengthValues","Population","PerkStatStrengthValues","PositiveSmall"}, 
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"AmountMin", 5}, -- 1
+								{"AmountMax", 5}  -- 1
+                            }
+                        }
+						
+                    }
+                }
+            }
+        }
+    }
+}
