bdbc34f47728dfcb66d0e59114e18f95f9bbf5ff
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-02-02 11:42:28 -0700

    Create gShip Phase Beam Mining.lua

---
+++ Gumsk/gShip/gShip Phase Beam Mining.lua
@@ -0,0 +1,35 @@
+Author			= "Gumsk"
+ModName 		= "gShip Phase Beam Mining"
+ModNameSub		= ""
+BaseDescription = "Makes the phase beam able to mine"
+GameVersion 	= "408"
+ModVersion		= "a"
+
+--[[Files Modified
+METADATA/PROJECTILES/PROJECTILETABLE.MBIN
+--]]
+
+NMS_MOD_DEFINITION_CONTAINER = {
+	["MOD_FILENAME"]			= ModName.." "..ModNameSub.." "..GameVersion..ModVersion..".pak",
+	["MOD_DESCRIPTION"]			= BaseDescription,
+	["MOD_AUTHOR"]				= Author,
+	["NMS_VERSION"]				= GameVersion,
+	["GLOBAL_INTEGER_TO_FLOAT"]	= "FORCE",
+	["MODIFICATIONS"]			= {
+		{
+			["MBIN_CHANGE_TABLE"] = {
+				{
+					["MBIN_FILE_SOURCE"] = "METADATA/PROJECTILES/PROJECTILETABLE.MBIN",
+					["EXML_CHANGE_TABLE"] = {
+						{
+							["SPECIAL_KEY_WORDS"] = {"Id","PLAYER_SHIP"},
+							["VALUE_CHANGE_TABLE"] = {
+								{"CanMine", "True"},		--"False"
+							},
+						},
+					},
+				},
+			}
+		}
+	}
+}
