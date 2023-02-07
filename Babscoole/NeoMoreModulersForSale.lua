bf3cb3edd0bb2f8d35af17d6ee9a33f911ec9df5
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-02-06 18:03:08 -0700

    Create NeoMoreModulersForSale.lua

---
+++ Babscoole/NeoMoreModulersForSale.lua
@@ -0,0 +1,45 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]    = "NeoMoreModulersForSale.pak",
+["MOD_AUTHOR"]      = "Neo6891",
+["LUA_AUTHOR"]      = "Babscoole",
+["NMS_VERSION"]     = "4.08",
+["MOD_DESCRIPTION"] = "Merchants will have more modules for sale",
+["MODIFICATIONS"]   =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"]  = "METADATA\REALITY\DEFAULTREALITY.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["FSKWG"] =
+                            {
+                                {"ShipTechSpecialist", "GcTradeData.xml",},
+                                {"SuitTechSpecialist", "GcTradeData.xml",},
+                                {"WeapTechSpecialist", "GcTradeData.xml",},
+                                {"NexusTechSpecialist", "GcTradeData.xml",},
+                            },
+                            ["SPECIAL_KEY_WORDS"] = {"ShipTechSpecialist","GcTradeData.xml",},
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"MinItemsForSale", "40"},
+                                {"MaxItemsForSale", "60"},
+                            }
+                        },
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {"VehicleTechSpecialist","GcTradeData.xml",},
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"MinItemsForSale", "40"},
+                                {"MaxItemsForSale", "40"},
+                            }
+                        },
+                    }
+                },
+            }
+        }
+    }
+}
