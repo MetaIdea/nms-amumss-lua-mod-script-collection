81a1f0381481be6c3fe444a7b0ebc43e6f2041ac
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-02-09 23:57:00 -0700

    Add gCam Filters Enabled FrozenStorm and TrueColor Standard

---
+++ Gumsk/gCam/Filters/gCam Filters Enabled FrozenStorm.lua
@@ -0,0 +1,34 @@
+Author = "Gumsk"
+ModName = "gCam Filters Enabled"
+ModNameSub = "FrozenStorm"
+BaseDescription = ""
+GameVersion = "408"
+ModVersion = "a"
+
+--[[ Files Modified:
+METADATA\\EFFECTS\\SCREENFILTERS.MBIN
+]]--
+
+NMS_MOD_DEFINITION_CONTAINER = {
+	["MOD_FILENAME"]	= ModName.." "..ModNameSub.." "..GameVersion..ModVersion..".pak",
+	["MOD_DESCRIPTION"]	= BaseDescription,
+	["MOD_AUTHOR"]		= Author,
+	["NMS_VERSION"]		= GameVersion,
+	["MODIFICATIONS"]	= {
+		{
+			["MBIN_CHANGE_TABLE"] = {
+				{
+					["MBIN_FILE_SOURCE"] = "METADATA\\EFFECTS\\SCREENFILTERS.MBIN",
+					["EXML_CHANGE_TABLE"] = {
+						{
+							["SPECIAL_KEY_WORDS"] = {"FrozenStorm","GcScreenFilterData.xml"},
+							["VALUE_CHANGE_TABLE"] = {
+								{"SelectableInPhotoMode","True"},
+							},
+						},
+					},
+				},
+			}
+		}
+	}
+}
