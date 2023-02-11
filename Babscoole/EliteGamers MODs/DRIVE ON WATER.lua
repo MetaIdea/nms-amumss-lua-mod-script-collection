15a565dc9f92107ffd317d4b4a6e374cee958420
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-02-10 18:38:58 -0700

    Create DRIVE ON WATER.lua

---
+++ Babscoole/EliteGamers MODs/DRIVE ON WATER.lua
@@ -0,0 +1,43 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]  = "DRIVE ON WATER.pak",
+["MOD_AUTHOR"]    = "EliteGamers",
+["LUA_AUTHOR"]    = "Babscoole",
+["NMS_VERSION"]   = "4.08",
+["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"] = "GCVEHICLEGLOBALS.GLOBAL.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["FOREACH_SKW_GROUP"] =
+                            {
+                                {"Name", "MED_BUGGY"},
+                                {"Name", "TRUCK"},
+                                {"Name", "WHEELEDBIKE"},
+                            },
+                            ["INTEGER_TO_FLOAT"] = "FORCE",
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"VehicleGravity",    "17.5"},
+                                {"DriveOnTopOfWater", "True"},
+                            }
+                        },
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {"Name", "MECH"},
+                            ["INTEGER_TO_FLOAT"] = "FORCE",
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"VehicleGravity", "17.5"},
+                            }
+                        },
+                    }
+                },
+            }
+        },
+    }
+}
