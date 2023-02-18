011f15301b9426c10174ddb76569d074441c9e96
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-02-17 21:59:04 -0700

    gCreatures Giants updates

--- Gumsk/GCreatures/Giants/GCreatures Giants 5x.lua
+++ Gumsk/GCreatures/Giants/GCreatures Giants 5x.lua
@@ -4,9 +4,13 @@ Author = "Gumsk"
 ModName = "gCreatures Giants"
 ModNameSub = Multiplier.."x"
 BaseDescription = "Increases creature sizes"
-GameVersion = "401"
+GameVersion = "408"
 ModVersion = "a"
-FileSource1 = "METADATA\SIMULATION\ECOSYSTEM\CREATUREDATATABLE.MBIN"
+
+--[[
+METADATA\\SIMULATION\\ECOSYSTEM\\CREATUREDATATABLE.MBIN
+GCCREATUREGLOBALS.MBIN
+]]--
 
 NMS_MOD_DEFINITION_CONTAINER = {
 	["MOD_FILENAME"]	= ModName.." "..ModNameSub.." "..GameVersion..ModVersion..".pak",
@@ -17,7 +21,7 @@ NMS_MOD_DEFINITION_CONTAINER = {
 		{
 			["MBIN_CHANGE_TABLE"] = {
 				{
-					["MBIN_FILE_SOURCE"] = FileSource1,
+					["MBIN_FILE_SOURCE"] = "METADATA\\SIMULATION\\ECOSYSTEM\\CREATUREDATATABLE.MBIN",
 					["EXML_CHANGE_TABLE"] = {
 						{
 							["MATH_OPERATION"] = "*",
@@ -25,6 +29,18 @@ NMS_MOD_DEFINITION_CONTAINER = {
 							["REPLACE_TYPE"] = "ALL",
 							["VALUE_CHANGE_TABLE"] = {
 								{"MaxScale", Multiplier},
+							},
+						},
+					},
+				},
+				{
+					["MBIN_FILE_SOURCE"] = "GCCREATUREGLOBALS.MBIN",
+					["EXML_CHANGE_TABLE"] = {
+						{
+							["MATH_OPERATION"] = "*",
+							["INTEGER_TO_FLOAT"] = "FORCE",
+							["VALUE_CHANGE_TABLE"] = {
+								{"MaxCreatureSize", Multiplier},
 							},
 						},
 					},
