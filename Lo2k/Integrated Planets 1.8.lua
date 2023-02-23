7549007410fa48e24d38de3cb35279febb7bc3f8
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-02-23 09:09:15 -0700

    Lo2k Fractal updates

--- Lo2k/Integrated Planets 1.8.lua
+++ Lo2k/Integrated Planets 1.8.lua
@@ -1,9 +1,9 @@
 NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"] 			= "Integrated Planets 1.7.pak", 
+["MOD_FILENAME"] 			= "Integrated Planets 1.8.pak", 
 ["MOD_AUTHOR"]				= "Lo2k",
 ["LUA_AUTHOR"]				= "Lo2k",
-["NMS_VERSION"]				= "4.08",
+["NMS_VERSION"]				= "4.10",
 ["MOD_DESCRIPTION"]			= "This mod rebalances space fog over planets, distant ships and asteroids",
 ["MODIFICATIONS"] 			= 
 	{
