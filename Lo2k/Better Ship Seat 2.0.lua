2e0611a5d6571b437ea92799dc7b81d5766d35f6
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-02-24 15:42:42 -0700

    Lo2k Fractals updates

--- Lo2k/Better Ship Seat 2.0.lua
+++ Lo2k/Better Ship Seat 2.0.lua
@@ -1,9 +1,9 @@
 NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"] 			= "Better Ship Seat 1.9.pak", 
+["MOD_FILENAME"] 			= "Better Ship Seat 2.0.pak", 
 ["MOD_AUTHOR"]				= "Lo2k",
 ["LUA_AUTHOR"]				= "Lo2k",
-["NMS_VERSION"]				= "3.94",
+["NMS_VERSION"]				= "4.10",
 ["MOD_DESCRIPTION"]			= "This mod changes first person ship camera to be able to view all ship monitors",
 ["MODIFICATIONS"] 			= 
 	{
