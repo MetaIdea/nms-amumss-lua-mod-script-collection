949762096c2d515622e83f1fee7f8f35a5376597
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-02-28 09:04:03 -0700

    Fractals updates

--- Lo2k/Old Style Slots 1.5.lua
+++ Lo2k/Old Style Slots 1.5.lua
@@ -1,9 +1,9 @@
 NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"] 			= "Old Style Slots 1.4.pak", 
+["MOD_FILENAME"] 			= "Old Style Slots 1.5.pak", 
 ["MOD_AUTHOR"]				= "Lo2k",
 ["LUA_AUTHOR"]				= "Lo2k",
-["NMS_VERSION"]				= "4.07",
+["NMS_VERSION"]				= "4.10",
 ["MOD_DESCRIPTION"]			= "This mods restores the old style slots",
 ["MODIFICATIONS"] 			= 
 	{
