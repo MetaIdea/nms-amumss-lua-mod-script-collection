43567a48ebe803c534476c38f05eba9662bab708
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-03-16 08:28:40 -0600

    Syzzle gets a folder!  +SACS update

---
+++ Syzzle/No Portal Charging.lua
@@ -0,0 +1,32 @@
+Author = "Syzzle"
+ModName = "No Portal Charging"
+GameVersion = "4.14"
+Description = "Skips the whole process of charging the portal in order to use it."
+
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= ModName.." "..GameVersion..".pak",
+["MOD_AUTHOR"]				= Author,
+["NMS_VERSION"]				= GameVersion,
+["MOD_DESCRIPTION"] 		= Description,
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "MODELS/PLANETS/BIOMES/COMMON/BUILDINGS/PORTAL/PORTAL/ENTITIES/BUTTON.ENTITY.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["VALUE_CHANGE_TABLE"]  =
+							{
+								{"AutoCompleteOnStart","True"}, -- Original "False"
+							}
+						},	
+					}
+				},	
+			}
+		}
+	}	
+}
