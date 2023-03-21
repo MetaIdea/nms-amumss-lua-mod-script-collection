d0291ef9fc65bb62226a2ad355844990d26deb29
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-03-20 22:42:08 -0600

    Fix for me accidentally uploading a shortcut.  :)

---
+++ Babscoole/__Jetpack_Super_Turbo_fraktal.lua
@@ -0,0 +1,60 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]    = "__Jetpack_Super_Turbo_fraktal.pak",
+["MOD_AUTHOR"]      = "Tuz1971",
+["LUA_AUTHOR"]      = "Babscoole",
+["NMS_VERSION"]     = "4.15",
+["MODIFICATIONS"]   =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"]  = "GCPLAYERGLOBALS.GLOBAL.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["INTEGER_TO_FLOAT"] = "FORCE",
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"GroundWalkSpeed",                     "5"},
+                                {"GroundRunSpeed",                      "10"},
+                                {"JetpackUnderwaterFillRate",           "3"},
+                                {"JetpackDrainHorizontalFactor",        "0.5"},
+                                {"JetpackForce",                        "61"},
+                                {"JetpackBrake",                        "3.5"},
+                                {"JetpackMaxSpeed",                     "20"},
+                                {"JetpackMaxUpSpeed",                   "55"},
+                                {"JetpackUpForce",                      "55"},
+                                {"JetpackIgnitionForce",                "100"},
+                                {"JetpackIgnitionTime",                 "0.1"},
+                                {"JetpackFillRate",                     "10"},
+                                {"JetpackFillRateMidair",               "10"},
+                                {"JetpackUpForceDeadPlanetExtra",       "15"},
+                                {"JetpackForceDeadPlanetExtra",         "50"},
+                                {"JetpackIgnitionForceDeadPlanetExtra", "80"},
+                                {"MaximumHorizontalOffsetVR",           "0.95"},
+                            }
+                        },
+                        {
+                            ["PRECEDING_KEY_WORDS"] = {"JetpackTankTimes",},
+							["LINE_OFFSET"] = "+1",
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"IGNORE", "60"},
+                            }
+                        },
+                        {
+                            ["PRECEDING_KEY_WORDS"] = {"JetpackTankTimes",},
+							["LINE_OFFSET"] = "+2",
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"IGNORE", "40"},
+                            }
+                        },
+                    }
+                }
+            }
+        }
+    }
+}
