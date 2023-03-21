28cdc2b6fbe990520172d4ea42ff93656a232393
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-03-20 22:36:58 -0600

    4.15 updates

---
+++ Babscoole/StationsTypeB.lua
@@ -0,0 +1,28 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]  = "StationsTypeB.pak",
+["MOD_AUTHOR"]    = "AyyMang",
+["LUA_AUTHOR"]    = "Babscoole",
+["NMS_VERSION"]   = "4.15",
+["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"] = "GCSIMULATIONGLOBALS.GLOBAL.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["NOTICE_OFF"] = "TRUE",
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"SpaceStationFile", "MODELS/SPACE/SPACESTATION/SPACESTATIONTYPEB.SCENE.MBIN"},
+                            }
+                        },
+                    }
+                },
+            }
+        },
+    }
+}
