d22f389b2004d388bc505eb9fee54198bf35e797
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-03-28 12:31:55 -0600

    Add OGGrass and updates

---
+++ Babscoole/Bodie 420 MODs/_Bodie420.GCCameraGlobals.12.0.lua
@@ -0,0 +1,37 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]    = "_Bodie420.GCCameraGlobals.12.0.pak",
+["MOD_AUTHOR"]      = "Bodie420",
+["LUA_AUTHOR"]      = "Babscoole",
+["NMS_VERSION"]     = "2.0",
+["MODIFICATIONS"]   =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"]  = "GCCAMERAGLOBALS.GLOBAL.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["PRECEDING_KEY_WORDS"] = {"Cameras","GcCameraFollowSettings.xml"},
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"MinSpeed",   "0"},
+                                {"SpeedRange", "200"},
+                            }
+                        },
+                        {
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"PhotoModeMaxDistance",      "999999"},
+                                {"PhotoModeMaxDistanceSpace", "999999"},
+                                {"BuildingModeMaxDistance",   "999999"},
+                            }
+                        },
+                    }
+                }
+            }
+        }
+    }
+}
