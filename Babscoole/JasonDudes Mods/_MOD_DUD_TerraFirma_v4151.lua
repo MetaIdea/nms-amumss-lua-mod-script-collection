66c2c423266b7392401dbe1186b670c7f264b177
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-03-28 15:40:55 -0600

    Duds TerraFirma Update

--- Babscoole/JasonDudes Mods/_MOD_DUD_TerraFirma_v4151.lua
+++ Babscoole/JasonDudes Mods/_MOD_DUD_TerraFirma_v4151.lua
@@ -1,9 +1,9 @@
 NMS_MOD_DEFINITION_CONTAINER =
 {
-["MOD_FILENAME"]  = "_MOD_DUD_TerraFirma_v4061.pak",
+["MOD_FILENAME"]  = "_MOD_DUD_TerraFirma_v4151.pak",
 ["MOD_AUTHOR"]    = "jasondude7116",
 ["LUA_AUTHOR"]    = "Babscoole",
-["NMS_VERSION"]   = "4.08",
+["NMS_VERSION"]   = "4.15",
 ["GLOBAL_INTEGER_TO_FLOAT"] = "FORCE",
 ["MODIFICATIONS"] =
     {
