b4f5062e242904a48ee94f51c96ddf8d7afee808
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-03-28 22:12:35 -0600

    Update DUD's Sky and Sky Colors

---
+++ Babscoole/JasonDudes Mods/MODs with additional files/_MOD_DUD_SnowFX_v4151.lua
@@ -0,0 +1,31 @@
+--Must get the TEXTURES folder from the orignal .pak and place into ModExtraFilesToInclude
+
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]  = "_MOD_DUD_SnowFX_v4151.pak",
+["MOD_AUTHOR"]    = "jasondude7116",
+["LUA_AUTHOR"]    = "Babscoole",
+["NMS_VERSION"]   = "4.15",
+["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"]  = "MODELS\EFFECTS\HEAVYAIR\SNOW\SNOW1.HEAVYAIR.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"MinParticleLifetime", "5"},
+                                {"MaxParticleLifetime", "10"},
+                                {"FadeTime",            "0.5"},
+                            }
+                        },
+                    }
+                },
+            }
+        },
+    },
+}
