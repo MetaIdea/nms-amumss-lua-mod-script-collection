abf8dfeba18d61f85240130183ddc9d9ba90bc5d
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-04-09 19:13:46 -0600

    planetary flight fixes update

--- other-authors/Planetary_Flight_Fixes_Lite_3_00_script.lua
+++ other-authors/Planetary_Flight_Fixes_Lite_3_00_script.lua
@@ -10,9 +10,9 @@ BOOSTMULT = 1.5		-- Multiplier for strength of the boost acceleration; Set this 
 
 NMS_MOD_DEFINITION_CONTAINER =
 {
-  ["MOD_FILENAME"]             = "_Planetary_Flight_Fixes_Lite_2_9.pak", 
+  ["MOD_FILENAME"]             = "_Planetary_Flight_Fixes_Lite_3_00.pak", 
   ["MOD_AUTHOR"]               = "chronicallybored",
-  ["NMS_VERSION"]              = "4.08",
+  ["NMS_VERSION"]              = "4.20",
   ["MOD_DESCRIPTION"]          = "Fixes various aspects of the flight system while inside a planets atmosphere",
   ["MODIFICATIONS"]            = 
 	{
