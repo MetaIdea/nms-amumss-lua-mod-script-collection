8e31f1407944b127c277cd16f60866a974b473c7
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-04-13 08:17:02 -0600

    Update Sentinel cockpit no red glow.lua

--- lMonk/+Publish/Sentinel cockpit no red glow.lua
+++ lMonk/+Publish/Sentinel cockpit no red glow.lua
@@ -1,8 +1,9 @@
----------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------
 mod_desc = [[
-  Removes the red glow from parts of the sentinel ship cockpit instruments
-  and gets rid of those floating red lights on the canopy
-]]-------------------------------------------------------------------------
+  Removes the red glow from the sentinel ship cockpit the canopy.
+  The 2 front monitors, scrolling pulse lines along the side wires, base of the system map
+  and side walls along the back are affected.
+]]-----------------------------------------------------------------------------------------
 
 NMS_MOD_DEFINITION_CONTAINER = {
 	MOD_FILENAME 			= '_MOD.lMonk.Sentinel cockpit no red glow.pak',
@@ -12,11 +13,19 @@ NMS_MOD_DEFINITION_CONTAINER = {
 	MODIFICATIONS 			= {{
 	MBIN_CHANGE_TABLE		= {
 	{
-		MBIN_FILE_SOURCE	= 'MODELS/COMMON/SPACECRAFT/SENTINELSHIP/SENTINELCOCKPIT/LIGHTS_MAT6.MATERIAL.MBIN',
+		MBIN_FILE_SOURCE	= 'MODELS/COMMON/SPACECRAFT/SENTINELSHIP/SENTINELCOCKPIT.SCENE.MBIN',
 		EXML_CHANGE_TABLE	= {
 			{
-				PRECEDING_KEY_WORDS	= 'Samplers',
-				REMOVE				= 'Section'
+				FOREACH_SKW_GROUP = {
+					{'Name', 'lightrim'},			-- high sidewall scroll glow
+					{'Name', 'WireLightsL'},		-- L sidewires scroll glow
+					{'Name', 'WireLightsR'},		-- R sidewires scroll glow
+					{'Name', 'polySurface8398'},	-- canopy lights
+					{'Name', 'Lightbase'},		 	-- map base center
+					{'Name', 'MonitorL1'},			-- L monitor
+					{'Name', 'MonitorL2'},			-- R monitor
+				},
+				REMOVE = 'Section'
 			}
 		}
 	}
