9f4a6d5eae55c5637887d32d41482ced3471fb28
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-04-18 20:40:29 -0600

    Update gLight Better Ship Lighting Freighter.lua

--- Gumsk/gLight/Better Ship Lighting/gLight Better Ship Lighting Freighter.lua
+++ Gumsk/gLight/Better Ship Lighting/gLight Better Ship Lighting Freighter.lua
@@ -2,7 +2,7 @@ ModName = "gLight Better Ship Lighting"
 ModNameSub = "Freighter"
 BaseDescription = ""
 GameVersion = "423"
-ModVersion = "a"
+ModVersion = "b"
 
 --[[ Files Modified:
 MODELS\COMMON\SPACECRAFT\INDUSTRIAL\ACCESSORIES\LANDINGPAD_HANGAR.SCENE.MBIN
@@ -63,6 +63,38 @@ NMS_MOD_DEFINITION_CONTAINER = {
 							},
 							["VALUE_CHANGE_TABLE"] = {
 								{"Value",35000},
+							}
+						},
+					}
+				},
+				{
+					["MBIN_FILE_SOURCE"] = "MODELS\COMMON\SPACECRAFT\INDUSTRIAL\ACCESSORIES\HANGARPARTS\HANGARLAYOUT.SCENE.MBIN",
+					["EXML_CHANGE_TABLE"] = {
+						{
+							["SPECIAL_KEY_WORDS"] = {
+								"Name","COL_R"
+							},
+							["REPLACE_TYPE"] = "ALL",
+							["VALUE_CHANGE_TABLE"] = {
+								{"Value",1.0},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {
+								"Name","COL_G"
+							},
+							["REPLACE_TYPE"] = "ALL",
+							["VALUE_CHANGE_TABLE"] = {
+								{"Value",1.0},
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = {
+								"Name","COL_B"
+							},
+							["REPLACE_TYPE"] = "ALL",
+							["VALUE_CHANGE_TABLE"] = {
+								{"Value",1.0},
 							}
 						},
 					}
