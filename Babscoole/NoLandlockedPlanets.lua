c539b162f74791fc139ea3f407e314fbf8db1ee8
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-04-19 11:44:30 -0600

    Create NoLandlockedPlanets.lua

---
+++ Babscoole/NoLandlockedPlanets.lua
@@ -0,0 +1,27 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]  = "NoLandlockedPlanets.pak",
+["MOD_AUTHOR"]    = "darkmessenger84",
+["LUA_AUTHOR"]    = "Babscoole",
+["NMS_VERSION"]   = "4.23",
+["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"] = "GCDEBUGOPTIONS.GLOBAL.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"ForcePlanetsToHaveWater", "True"},
+                            }
+                        },
+                    }
+                },
+            }
+        },
+    }
+}
