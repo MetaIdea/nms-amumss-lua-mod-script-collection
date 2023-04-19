568fdca44e13fdb1af86f9807262fc4f4ef0900a
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-04-19 09:24:02 -0600

    Update Ship and multitool upgrade terminals.lua

--- lMonk/+Publish/Ship and multitool upgrade terminals.lua
+++ lMonk/+Publish/Ship and multitool upgrade terminals.lua
@@ -4,51 +4,12 @@ mod_desc = [[
   The multitool upgrade menu from the weapons specialist terminal.
   The ship salavage and upgrade menu from the old monitor station.
 ]]------------------------------------------------------------------
-mod_version = 1.61
-
-local function AddInteractionComponent(action_type)
-	return [[
-		<Property value="GcInteractionComponentData.xml">
-			<Property name="InteractionAction" value="PressButton"/>
-			<Property name="InteractionType" value="GcInteractionType.xml">
-				<Property name="InteractionType" value="]]..action_type..[["/>
-			</Property>
-			<Property name="AttractDistanceSq" value="9"/>
-			<Property name="BlendToCameraTime" value="0.5"/>
-			<Property name="BlendFromCameraTime" value="-1"/>
-			<Property name="TriggerAction" value="INACTIVE"/>
-			<Property name="InteractAngle" value="360"/>
-			<Property name="InteractDistance" value="3"/>
-			<Property name="SecondaryCameraTransitionTime" value="1"/>
-			<Property name="DepthOfField" value="GcInteractionDof.xml">
-				<Property name="IsEnabled" value="True"/>
-				<Property name="UseGlobals" value="True"/>
-				<Property name="NearPlaneMin" value="2"/>
-				<Property name="NearPlaneAdjust" value="1"/>
-				<Property name="FarPlane" value="3"/>
-				<Property name="FarFadeDistance" value="2"/>
-			</Property>
-			<Property name="OverrideInteriorExteriorMarker" value="No"/>
-		</Property>
-		<Property value="TkPhysicsComponentData.xml">
-			<Property name="Data" value="TkPhysicsData.xml">
-				<Property name="Mass" value="0"/>
-				<Property name="Friction" value="0.5"/>
-				<Property name="RollingFriction" value="0.2"/>
-				<Property name="AngularDamping" value="0.2"/>
-				<Property name="LinearDamping" value="0.1"/>
-				<Property name="Gravity" value="20"/>
-			</Property>
-			<Property name="TriggerVolumeType" value="TkVolumeTriggerType.xml">
-				<Property name="VolumeTriggerType" value="Open"/>
-			</Property>
-		</Property>]]
-end
+mod_version = '1.63'
 
 NMS_MOD_DEFINITION_CONTAINER = {
 	MOD_FILENAME 		= '_MOD.lMonk.ship and multitool upgrade terminals.'..mod_version..'.pak',
 	MOD_AUTHOR			= 'lMonk',
-	NMS_VERSION			= '4.12',
+	NMS_VERSION			= '4.23',
 	MOD_DESCRIPTION		= mod_desc,
 	MODIFICATIONS 		= {{
 	MBIN_CHANGE_TABLE	= {
@@ -57,7 +18,21 @@ NMS_MOD_DEFINITION_CONTAINER = {
 		EXML_CHANGE_TABLE	= {
 			{
 				PRECEDING_KEY_WORDS	= 'Components',
-				ADD					= AddInteractionComponent('WeaponUpgrade')
+				ADD					= [[
+					<Property value="GcInteractionComponentData.xml">
+						<Property name="InteractionAction" value="PressButton"/>
+						<Property name="InteractionType" value="GcInteractionType.xml">
+							<Property name="InteractionType" value="WeaponUpgrade"/>
+						</Property>
+						<Property name="AttractDistanceSq" value="9"/>
+						<Property name="BlendToCameraTime" value="0.5"/>
+						<Property name="BlendFromCameraTime" value="-1"/>
+						<Property name="TriggerAction" value="INACTIVE"/>
+						<Property name="InteractAngle" value="360"/>
+						<Property name="InteractDistance" value="3"/>
+						<Property name="SecondaryCameraTransitionTime" value="1"/>
+					</Property>
+					<Property value="TkPhysicsComponentData.xml" />]]
 			}
 		}
 	},
