8a62947c261f079f9260ac87ddb6822a2ec0d543
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-04-19 10:45:24 -0600

    Add InterceptorNoUnfold

---
+++ Babscoole/Mjjstral MODs/InterceptorNoUnfold.lua
@@ -0,0 +1,42 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]  	= "InterceptorNoUnfold.pak",
+["MOD_AUTHOR"]    	= "Mjstral and maintenance by Babscoole",
+["MOD_EXTRA_INFO"]  = "Design your own fully customized sentinel ship (seed) on www.nms.center",
+["NMS_VERSION"]   	= "4.23+",
+["MODIFICATIONS"] 	=
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"]  = {
+						"MODELS\COMMON\SPACECRAFT\SENTINELSHIP\SENTINELSHIP_PROC\ENTITIES\ROOTJNT.ENTITY.MBIN",
+						"MODELS\COMMON\SPACECRAFT\SENTINELSHIP\PARTS\WINGSB\ENTITIES\ROOTJNT.ENTITY.MBIN",
+						"MODELS\COMMON\SPACECRAFT\SENTINELSHIP\PARTS\ENGINEFLAMEL\ENTITIES\DATA.ENTITY.MBIN",
+						"MODELS\COMMON\SPACECRAFT\SENTINELSHIP\PARTS\ENGINEFLAMEBODY1\ENTITIES\DATA.ENTITY.MBIN",
+					},
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["SPECIAL_KEY_WORDS"]  = {"Anim","TAKEOFF"},
+                            ["VALUE_CHANGE_TABLE"] = {{"Filename", ""},}
+                        },
+                    }
+                },
+                {
+                    ["MBIN_FILE_SOURCE"]  = {
+						"MODELS\COMMON\SPACECRAFT\SENTINELSHIP\PARTS\ENGINEFLAMEL\ENTITIES\DATA.ENTITY.MBIN",
+					},
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["SPECIAL_KEY_WORDS"]  = {"Anim","LOWALTITUDE"},
+                            ["VALUE_CHANGE_TABLE"] = {{"Filename", ""},}
+                        },
+                    }
+                },
+            }
+        }
+    }
+}
