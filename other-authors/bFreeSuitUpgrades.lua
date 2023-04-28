4ab0d99e137b45d87875b01c3df37598031f33c1
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-04-28 00:42:51 -0600

    Update bFreeSuitUpgrades.lua

--- other-authors/bFreeSuitUpgrades.lua
+++ other-authors/bFreeSuitUpgrades.lua
@@ -1,3 +1,11 @@
+SuitMainPrices = [[  <Property name="SuitUpgradePrices">
+	  <Property value="0"  />
+  </Property>]]
+
+SuitTechPrices = [[  <Property name="SuitTechOnlyUpgradePrices">
+	  <Property value="0"  />
+  </Property>]]
+
 NMS_MOD_DEFINITION_CONTAINER = {
   ["MOD_FILENAME"] = "Free Suit Upgrade.pak",
   ["MOD_DESCRIPTION"] = "Makes suit upgrades free and repeatable in the space station and at drop pods",
@@ -10,13 +18,13 @@ NMS_MOD_DEFINITION_CONTAINER = {
           ["MBIN_FILE_SOURCE"] = "MODELS/PLANETS/BIOMES/COMMON/BUILDINGS/PARTS/COMMONPARTS/CRYOCHAMBER/ENTITIES/CRYOCHAMBERINTERACTION.ENTITY.MBIN",
           ["EXML_CHANGE_TABLE"] = {
             {
-              ["SPECIAL_KEY_WORDS"] = {"StateID", "INTERACTOPTION"},
+              ["SPECIAL_KEY_WORDS"] = { "StateID", "INTERACTOPTION" },
               ["REMOVE"] = "SECTION"
             },
             {
               ["VALUE_CHANGE_TABLE"] = {
-                {"RepeatInteraction", "True"},       -- Original "False"
-                {"ReseedAfterRewardSuccess", "True"} -- Original "False"
+                { "RepeatInteraction",        "True" }, -- Original "False"
+                { "ReseedAfterRewardSuccess", "True" } -- Original "False"
               }
             }
           }
@@ -25,18 +33,42 @@ NMS_MOD_DEFINITION_CONTAINER = {
           ["MBIN_FILE_SOURCE"] = "MODELS/PLANETS/BIOMES/COMMON/BUILDINGS/PARTS/COMMONPARTS/CRYOCHAMBERPOD/ENTITIES/CRYOCHAMBERPOD.ENTITY.MBIN",
           ["EXML_CHANGE_TABLE"] = {
             {
-              ["PRECEDING_KEY_WORDS"] = {"GcMaintenanceComponentData.xml"},
+              ["PRECEDING_KEY_WORDS"] = { "GcMaintenanceComponentData.xml" },
               ["REMOVE"] = "SECTION"
             },
             {
-              ["SPECIAL_KEY_WORDS"] = {"StateID", "INTERACTOPTION"},
+              ["SPECIAL_KEY_WORDS"] = { "StateID", "INTERACTOPTION" },
               ["REMOVE"] = "SECTION"
             },
             {
               ["VALUE_CHANGE_TABLE"] = {
-                {"RepeatInteraction", "True"},       -- Original "False"
-                {"ReseedAfterRewardSuccess", "True"} -- Original "False"
+                { "RepeatInteraction",        "True" }, -- Original "False"
+                { "ReseedAfterRewardSuccess", "True" } -- Original "False"
               }
+            }
+          }
+        },
+        {
+          ["MBIN_FILE_SOURCE"] = "METADATA/REALITY/DEFAULTREALITY.MBIN",
+          ["EXML_CHANGE_TABLE"] = {
+            {
+              ["PRECEDING_KEY_WORDS"] = { "SuitUpgradePrices" },
+              ["REMOVE"] = "SECTION",
+            },
+            {
+              ["PRECEDING_KEY_WORDS"] = { "SuitTechOnlyUpgradePrices" },
+              ["REMOVE"] = "SECTION"
+            },
+            {
+              
+                ["PRECEDING_KEY_WORDS"] = { "ShipCargoOnlyStartingLayout" },
+                ["ADD"] = SuitMainPrices,
+                ["ADD_OPTION"] = "ADDAfterSECTION",
+            },
+            {
+              ["PRECEDING_KEY_WORDS"] = { "SuitUpgradePrices" },
+              ["ADD"] = SuitTechPrices,
+              ["ADD_OPTION"] = "ADDAfterSECTION",
             }
           }
         }
