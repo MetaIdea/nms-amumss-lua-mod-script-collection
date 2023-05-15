be730ec18efc9890f3b98ba26891d4fa7ce42cda
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-05-15 00:22:11 -0600

    Pyronox updates

--- PyronoxOmega/CuriosityFinder.lua
+++ PyronoxOmega/CuriosityFinder.lua
@@ -1,7 +1,7 @@
 Author = "PyronoxOmega"
 ModName = "CuriosityFinder"
-GameVersion = "4.2"
-ModVersion = "1.3"
+GameVersion = "4.23"
+ModVersion = "1.4"
 Description = "easily find curiosities on planet surface"
 
 NMS_MOD_DEFINITION_CONTAINER = 
