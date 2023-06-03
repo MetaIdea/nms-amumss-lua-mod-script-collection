d3b5d53f995020ce150b53bdc5932a2f6ae87a19
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-06-03 08:40:11 -0600

    update FineLOD and Tiny Inventory Changes

--- Lo2k/Tiny Inventory Changes 5.0.lua
+++ Lo2k/Tiny Inventory Changes 5.0.lua
@@ -1,9 +1,9 @@
 NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"] 			= "Tiny Inventory Changes 4.9.pak", 
+["MOD_FILENAME"] 			= "Tiny Inventory Changes 5.0.pak", 
 ["MOD_AUTHOR"]				= "Lo2k",
 ["LUA_AUTHOR"]				= "Lo2k",
-["NMS_VERSION"]				= "4.22",
+["NMS_VERSION"]				= "4.26",
 ["MOD_DESCRIPTION"]			= "This mods changes orientation and lighting of inventory 3D models",
 ["MODIFICATIONS"] 			= 
 	{
