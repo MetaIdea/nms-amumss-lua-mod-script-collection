f48323fd01ca32cd7ce1a1ada644c01c93e095b2
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-06-06 18:31:26 -0600

    Create _NoSpaceRocks.lua

---
+++ Babscoole/_NoSpaceRocks.lua
@@ -0,0 +1,27 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]  = "_NoSpaceRocks.pak",
+["MOD_AUTHOR"]    = "Bookworm",
+["LUA_AUTHOR"]    = "Babscoole",
+["NMS_VERSION"]   = "4.25",
+["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"]  = "GCSOLARGENERATIONGLOBALS.GLOBAL.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["VALUE_CHANGE_TABLE"]     =
+                            {
+                                { "PlanetInvalidAsteroidZone", "1000000" },
+                            }
+                        },
+                    }
+                }
+            }
+        }
+    }
+}
