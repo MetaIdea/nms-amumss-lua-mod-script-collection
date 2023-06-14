903a3810ce39f8e3ed2e289224a77cae85826941
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-06-14 10:54:15 -0600

    Redmas update add shipspeed0

---
+++ other-authors/_ShipSpeed0.lua
@@ -0,0 +1,59 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "_ShipSpeed0.pak",
+["MOD_AUTHOR"]		        = "OTBP",
+["NMS_VERSION"]				= "v4.33",
+["MOD_DESCRIPTION"]			= "Ship Hover Zero Speed, w/Reverse Interceptor",
+["GLOBAL_INTEGER_TO_FLOAT"] = "FORCE", 
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["PAK_FILE_SOURCE"] 	= "NMSARC.globals.pak", 
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "GCSPACESHIPGLOBALS.GLOBAL.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{						
+						{
+						    ["PRECEDING_KEY_WORDS"] = { "Control", "PlanetEngine" },
+						    ["VALUE_CHANGE_TABLE"] 	= 
+							{
+								
+								 {"MinSpeed", 							"0" }, 		--The minimum speed of standard thrusters. Setting to 0 for space will allow full-stops and reversing; for planets it allows hovering. [Default "20"]
+							}
+						},
+						{
+						    ["PRECEDING_KEY_WORDS"] = { "ControlLight", "PlanetEngine" },
+						    ["VALUE_CHANGE_TABLE"] 	= 
+							{
+								
+								{ "MinSpeed", 							"0" }, 		--The minimum speed of standard thrusters. Setting to 0 for space will allow full-stops and reversing; for planets it allows hovering. [Default "20"]
+							}
+						},
+						{
+						    ["PRECEDING_KEY_WORDS"] = { "ControlHeavy", "PlanetEngine" },
+						    ["VALUE_CHANGE_TABLE"] 	= 
+							{
+								
+								{ "MinSpeed", 							"0" }, 		--The minimum speed of standard thrusters. Setting to 0 for space will allow full-stops and reversing; for planets it allows hovering. [Default "20"]
+							}
+					    },
+						{
+						    ["PRECEDING_KEY_WORDS"] = { "ControlHover", "PlanetEngine" },
+						    ["VALUE_CHANGE_TABLE"] 	= 
+							{
+								
+								{ "MinSpeed", 							"0" }, 		--The minimum speed of standard thrusters. Setting to 0 for space will allow full-stops and reversing; for planets it allows hovering. [Default "1"]
+							}
+					    }
+				    }
+				
+			    }
+		    }
+	    }	
+    }
+}	
+--NOTE: ANYTHING NOT in table NMS_MOD_DEFINITION_CONTAINER IS IGNORED AFTER THE SCRIPT IS LOADED
+--IT IS BETTER TO ADD THINGS AT THE TOP IF YOU NEED TO
+--DON'T ADD ANYTHING PASS THIS POINT HERE
