840d1ac97fa3d1027e340d07e7a6f05720700947
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-06-23 21:56:54 -0600

    Create No Intro.lua

---
+++ Babscoole/No Intro.lua
@@ -0,0 +1,32 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"] = "No Intro.pak",
+["MOD_AUTHOR"] = "Flugelwulff",
+["LUA_AUTHOR"] = "Babscoole",
+["NMS_VERSION"] = "4.37",
+["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"] = "METADATA\UI\BOOTLOGOPC.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["PRECEDING_KEY_WORDS"] = {"DisplayTime",},
+                            ["LINE_OFFSET"] = "+1",
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"IGNORE", "0"}, -- Original "3"
+                                {"IGNORE", "0"}, -- Original "2"
+                                {"IGNORE", "0"}, -- Original "1"
+                                {"IGNORE", "0"}  -- Original "1"
+                            }
+                        },
+                    }
+                },
+            }
+        }
+    }
+}
