10874d8a17a65130f6c28f7e24b3712e107486df
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-06-26 13:03:08 -0600

    update JJ's bridge

--- Babscoole/farShipHud.lua
+++ Babscoole/farShipHud.lua
@@ -3,7 +3,7 @@ NMS_MOD_DEFINITION_CONTAINER =
 ["MOD_FILENAME"]  = "farShipHud.pak",
 ["MOD_AUTHOR"]    = "hhliow",
 ["LUA_AUTHOR"]    = "Babscoole",
-["NMS_VERSION"]   = "4.23",
+["NMS_VERSION"]   = "4.36",
 ["MODIFICATIONS"] =
     {
         {
