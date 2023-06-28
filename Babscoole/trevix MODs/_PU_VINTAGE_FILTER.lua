d278e19e25fec298b193b00a7d9a9cee5a4ae392
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-06-27 21:15:33 -0600

    update quickwalks and quickdocks

---
+++ Babscoole/trevix MODs/_PU_VINTAGE_FILTER.lua
@@ -0,0 +1,29 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]    = "_PU_VINTAGE_FILTER.pak",
+["MOD_DESCRIPTION"] = "",
+["MOD_AUTHOR"]      = "trevix",
+["LUA_AUTHOR"]      = "Babscoole",
+["NMS_VERSION"]     = "4.36",
+["MODIFICATIONS"]   =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"]  = "METADATA\EFFECTS\SCREENFILTERS.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["REPLACE_TYPE"] = "ALL",
+                            ["VALUE_CHANGE_TABLE"]  =
+                            {
+                                {"Filename", "TEXTURES/LUT/FILTERS/DEFAULT.DDS"},
+                            }
+                        },
+                    }
+                }
+            }
+        }
+    }
+}
