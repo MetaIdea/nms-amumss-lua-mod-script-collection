108c6fa4b4efea611dc7e45502387be6a2ad5cb3
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-07-02 12:17:48 -0600

    Update Better Rifles.lua

--- Syzzle/Better Rifles.lua
+++ Syzzle/Better Rifles.lua
@@ -1,6 +1,6 @@
 Author = "Syzzle"
 ModName = "Better Rifles"
-GameVersion = "4.34"
+GameVersion = "4.36"
 --ModVersion = "v1.1"
 Description = "Change Rifle base stats to the same as Alien Multi-Tool."
 
