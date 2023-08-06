893d13dd69f52f19eec0bca990759756a10eeb8b
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-08-06 16:16:56 -0600

    Update CleanHUDVR and Seed Generator (fix Exotic ship ICON typo)

--- Babscoole/Mjjstral MODs/SEED-GENERATOR-V6A.lua
+++ Babscoole/Mjjstral MODs/SEED-GENERATOR-V6A.lua
@@ -377,7 +377,7 @@ function CreateQuickActionMenuEntry(BUTTON_TITLE, ANIM_ID)
                 elseif ANIM_ID == "SHUTTLE" then
                     ICON = "TEXTURES\UI\FRONTEND\ICONS\SPECIALSHOP\SPECIAL.TWITCH.SHIP06.DDS"
                 elseif ANIM_ID == "ROYAL" then
-                    ICON = "D:\Unpacked\TEXTURES\UI\FRONTEND\ICONS\SPECIALSHOP\SPECIAL.SHIP_B.DDS"
+                    ICON = "TEXTURES\UI\FRONTEND\ICONS\SPECIALSHOP\SPECIAL.SHIP_B.DDS"
                 elseif ANIM_ID == "ALIEN" then
                     ICON = "TEXTURES\UI\FRONTEND\ICONS\SPECIALSHOP\SPECIAL1.EXPEDITION.MINIBIOFRIG.DDS"
                 elseif ANIM_ID == "SAIL" then
@@ -495,7 +495,7 @@ NMS_MOD_DEFINITION_CONTAINER =
 ["MOD_FILENAME"]    = "zzzSEED-GENERATOR-V6A.pak",
 ["MOD_AUTHOR"]      = "Mjjstral & Babscoole",
 ["MOD_DESCRIPTION"] = "Adds random seed buttons to the quick action emote menu",
-["NMS_VERSION"]     = "4.34",
+["NMS_VERSION"]     = "4.39",
 ["MODIFICATIONS"]   =
     {
         {
@@ -545,7 +545,7 @@ NMS_MOD_DEFINITION_CONTAINER =
     {
         {
             ["FILE_DESTINATION"] = "MODELS\COMMON\PLAYER\PLAYERCHARACTER\ANIMS\EMOTES\NULL.ANIM.EXML",
-            ["FILE_CONTENT"]      =
+            ["FILE_CONTENT"] =
 [[
 <?xml version="1.0" encoding="utf-8"?>
 <Data template="TkAnimMetadata">
