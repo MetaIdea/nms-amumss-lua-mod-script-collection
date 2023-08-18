11060abde434385a6a2270d14a149f8de5dc7f48
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-08-18 10:41:50 -0600

    Create FF_EchoesSplashScreenLogo_438.lua

---
+++ FriendlyFirePL/FF_EchoesSplashScreenLogo_438.lua
@@ -0,0 +1,30 @@
+----------------------------------------------------------------------------------------------------
+-- mod description and credits
+----------------------------------------------------------------------------------------------------
+METADATA_MOD_NAME       = "EchoesSplashScreenLogo"
+METADATA_MOD_AUTHOR     = "FriendlyFirePL"
+METADATA_LUA_AUTHOR     = "FriendlyFirePL"
+METADATA_NMS_VERSION    = "438"
+METADATA_MOD_DESC       = "new splash logo for Echoes update"
+
+
+
+----------------------------------------------------------------------------------------------------
+-- mod definition
+----------------------------------------------------------------------------------------------------
+
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+    ["MOD_FILENAME"] 	    = "FF_"..METADATA_MOD_NAME.."_"..METADATA_NMS_VERSION..".pak",
+    ["MOD_AUTHOR"]		    = METADATA_MOD_AUTHOR,
+    ["LUA_AUTHOR"]		    = METADATA_LUA_AUTHOR,
+    ["NMS_VERSION"]		    = METADATA_NMS_VERSION,
+    ["MOD_DESCRIPTION"]     = METADATA_MOD_DESC,
+    ["MODIFICATIONS"] =     {}
+}
+
+
+
+----------------------------------------------------------------------------------------------------
+-- end of file
+----------------------------------------------------------------------------------------------------
