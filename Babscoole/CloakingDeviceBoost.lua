1307ec78b5223293ff21d9bec43a24cc6d0b9f32
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-08-31 13:48:19 -0600

    Some 4.43 updates

--- Babscoole/CloakingDeviceBoost.lua
+++ Babscoole/CloakingDeviceBoost.lua
@@ -1,27 +1,28 @@
 NMS_MOD_DEFINITION_CONTAINER =
 {
-["MOD_FILENAME"]  = "RestoredBiomeResources.pak",
-["MOD_AUTHOR"]    = "JoeCapricorn",
+["MOD_FILENAME"]  = "CloakDeviceBoost.pak",
+["MOD_AUTHOR"]    = "Mumbles",
 ["LUA_AUTHOR"]    = "Babscoole",
-["NMS_VERSION"]   = "4.41",
+["NMS_VERSION"]   = "4.43",
 ["MODIFICATIONS"] =
     {
         {
             ["MBIN_CHANGE_TABLE"] =
             {
                 {
-                    ["MBIN_FILE_SOURCE"] = "GCGAMEPLAYGLOBALS.GLOBAL.MBIN",
+                    ["MBIN_FILE_SOURCE"] = "GCPLAYERGLOBALS.GLOBAL.MBIN",
                     ["EXML_CHANGE_TABLE"] =
                     {
                         {
                             ["VALUE_CHANGE_TABLE"] =
                             {
-                                {"UseSecondaryBiomeSubstances", "True"},
+                                {"StealthDrainRate",    "5"},
+                                {"StealthRechargeRate", "50"},
                             }
                         },
                     }
-                }
+                },
             }
-        }
+        },
     }
 }
