d66d023872ddcb848f5286f5c235e38ae0d1feec
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-08-31 13:48:19 -0600

    Some 4.43 updates

---
+++ other-authors/ExoSpeedIncrease.lua
@@ -0,0 +1,54 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "ExoSpeedIncrease.pak",
+["MOD_AUTHOR"]				= "Viking",
+["NMS_VERSION"]				= "5.0",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["PAK_FILE_SOURCE"] 	= "NMSARC.59B126E2.pak",
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "GCVEHICLEGLOBALS.GLOBAL.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["PRECEDING_KEY_WORDS"] = {},
+						
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"TopSpeedForward",				"48"}, 	-- Original "16"
+								{"UnderwaterEngineMaxSpeed",	"45"} 	-- Original "15"
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = { "Name", "MECH" },
+							--["PRECEDING_KEY_WORDS"] = { "Name", "MECH" },
+						
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"TopSpeedForward",				"2"}, 	-- Original "2"
+								{"UnderwaterEngineMaxSpeed",	"15"}, 	-- Original "15"
+
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = { "Name", "SUBMARINE" },
+						
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"TopSpeedForward",				"48"}, 	-- Original "18"
+								{"UnderwaterEngineMaxSpeed",	"48"}, 	-- Original "18"
+
+							}
+						}
+					} 
+				}
+			}
+		}
+	}	
+}
+--NOTE: ANYTHING NOT in table NMS_MOD_DEFINITION_CONTAINER IS IGNORED AFTER THE SCRIPT IS LOADED
+--IT IS BETTER TO ADD THINGS AT THE TOP IF YOU NEED TO
+--DON'T ADD ANYTHING PASS THIS POINT HERE
---
+++ other-authors/ExoSpeedIncrease_MECHchanges infinite jetpack.lua
@@ -0,0 +1,59 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"] 			= "ExoSpeedIncrease_MECHchanges_infinite jetpack.pak",
+["MOD_AUTHOR"]				= "Viking",
+["NMS_VERSION"]				= "5.0",
+["MODIFICATIONS"] 			= 
+	{
+		{
+			["PAK_FILE_SOURCE"] 	= "NMSARC.59B126E2.pak",
+			["MBIN_CHANGE_TABLE"] 	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"] 	= "GCVEHICLEGLOBALS.GLOBAL.MBIN",
+					["EXML_CHANGE_TABLE"] 	= 
+					{
+						{
+							["PRECEDING_KEY_WORDS"] = {},
+						
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"MechJetpackMaxSpeed",		"60"}, 	-- Original "20"
+								{"MechJetpackMaxUpSpeed",	"60"}, 	-- Original "20"
+								{"MechJetpackDrainRate",	".4"}, 	-- Original ".5"
+								{"MechJetpackFillRate",		"0.5"}, 	-- Original ".5"
+								{"MechJetpackTurnSpeed",		"6"}, 	-- Original "3"
+								{"TopSpeedForward",				"48"}, 	-- Original "16"
+								{"UnderwaterEngineMaxSpeed",	"45"} 	-- Original "15"
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = { "Name", "MECH" },
+							--["PRECEDING_KEY_WORDS"] = { "Name", "MECH" },
+						
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"TopSpeedForward",				"2"}, 	-- Original "2"
+								{"UnderwaterEngineMaxSpeed",	"15"}, 	-- Original "15"
+
+							}
+						},
+						{
+							["SPECIAL_KEY_WORDS"] = { "Name", "SUBMARINE" },
+						
+							["VALUE_CHANGE_TABLE"] 	= 
+							{
+								{"TopSpeedForward",				"48"}, 	-- Original "18"
+								{"UnderwaterEngineMaxSpeed",	"48"}, 	-- Original "18"
+
+							}
+						}
+					} 
+				}
+			}
+		}
+	}	
+}
+--NOTE: ANYTHING NOT in table NMS_MOD_DEFINITION_CONTAINER IS IGNORED AFTER THE SCRIPT IS LOADED
+--IT IS BETTER TO ADD THINGS AT THE TOP IF YOU NEED TO
+--DON'T ADD ANYTHING PASS THIS POINT HERE
