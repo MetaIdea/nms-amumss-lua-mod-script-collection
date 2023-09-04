509bc8b1f67e18001cb7d879aec2db366ba91dde
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-09-03 23:59:27 -0600

    Update _MOD_DUD_RainFX_v443-A.lua

--- Babscoole/JasonDudes Mods/MODs with additional files/_MOD_DUD_RainFX_v443-A.lua
+++ Babscoole/JasonDudes Mods/MODs with additional files/_MOD_DUD_RainFX_v443-A.lua
@@ -2,10 +2,10 @@
 
 NMS_MOD_DEFINITION_CONTAINER =
 {
-["MOD_FILENAME"]            = "_MOD_DUD_RainFX_v4151.pak",
+["MOD_FILENAME"]            = "_MOD_DUD_RainFX_v443-A.pak",
 ["MOD_AUTHOR"]              = "jasondude7116",
 ["LUA_AUTHOR"]              = "Babscoole",
-["NMS_VERSION"]             = "4.15",
+["NMS_VERSION"]             = "4.43",
 ["GLOBAL_INTEGER_TO_FLOAT"] = "FORCE",
 ["MODIFICATIONS"]           =
     {
