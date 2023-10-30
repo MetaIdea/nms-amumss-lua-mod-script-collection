ef59cc86700ecc28560bf1778108e1f293835a20
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-10-30 09:16:01 -0600

    Update lMonk Freighter Hangar Interior

---
+++ Babscoole/BioShipNoFlyingAnim.lua
@@ -0,0 +1,38 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]    = "BioShipNoFlyingAnim.pak",
+["MOD_AUTHOR"]      = "Babscoole & Mjstral",
+["NMS_VERSION"]     = "4.23",
+["MODIFICATIONS"]   =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"] = "MODELS\COMMON\SPACECRAFT\S-CLASS\BIOPARTS\COCKPIT\COCKPITA\ENTITIES\COCKPIT_A.ENTITY.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {"Anim","FLYINGIDLE"},
+                            ["VALUE_CHANGE_TABLE"] = {{"Filename", ""},}
+                        },
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {"Anim","IDLE"},
+                            ["VALUE_CHANGE_TABLE"] = {{"Filename", ""},}
+                        },
+                    }
+                },
+                {
+                    ["MBIN_FILE_SOURCE"] = "MODELS\COMMON\SPACECRAFT\S-CLASS\BIOPARTS\ENGINE\ENGINE_A\ENTITIES\ENGINEA.ENTITY.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {"Anim","FLYINGIDLE"},
+                            ["VALUE_CHANGE_TABLE"] = {{"Filename", ""},}
+                        },
+                    }
+                },
+            }
+        }
+    }
+}
