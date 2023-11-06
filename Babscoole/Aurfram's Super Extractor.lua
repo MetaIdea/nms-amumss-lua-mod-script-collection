dd75fd3498f879af6f2008828eab171e7dd664e8
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-11-06 12:16:08 -0700

    Create Aurfram's Super Extractor.lua

---
+++ Babscoole/Aurfram's Super Extractor.lua
@@ -0,0 +1,36 @@
+MULTI = 3
+
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]  = "Aurfram's Super Extractor "..MULTI.."x.pak",
+["MOD_AUTHOR"]    = "Aurfram",
+["LUA_AUTHOR"]    = "Babscoole",
+["NMS_VERSION"]   = "4.45",
+["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"]  = "METADATA\REALITY\TABLES\BASEBUILDINGOBJECTSTABLE.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["FOREACH_SKW_GROUP"] =
+                            {
+                                {"ID", "U_EXTRACTOR_S"},
+                                {"ID", "U_GASEXTRACTOR"},
+                            },
+                            ["MATH_OPERATION"]  = "*",
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"Rate",    MULTI},
+                                {"Storage", MULTI},
+                            }
+                        },
+                    }
+                },
+            }
+        }
+    }
+}
