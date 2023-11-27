2aa6897290d32a5e1e90eacf65d23540fa57f7c0
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-11-26 21:16:48 -0700

    Some updates

---
+++ Babscoole/Space Station Invisible Wall Mod.lua
@@ -0,0 +1,28 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]  = "Space Station Invisible Wall Mod.pak",
+["MOD_AUTHOR"]    = "JovianStone",
+["LUA_AUTHOR"]    = "Babscoole",
+["NMS_VERSION"]   = "4.45",
+["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"]  = "MODELS\SPACE\SPACESTATION\MODULARPARTS\DOCK\ENTRANCESECTION.SCENE.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {"Type", "COLLISION", "Name", "HEIGHT"},
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"Value", "0.5"},
+                            }
+                        },
+                    }
+                },
+            }
+        },
+    }
+}
