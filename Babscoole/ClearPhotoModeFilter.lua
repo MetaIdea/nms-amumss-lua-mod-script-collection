bdbfdda9e5fc44cfc9c6098c51ee7d2d120f76af
Babscoole <82686995+Babscoole@users.noreply.github.com>
2023-11-30 06:02:23 -0700

    Create ClearPhotoModeFilter.lua

---
+++ Babscoole/ClearPhotoModeFilter.lua
@@ -0,0 +1,29 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]    = "ClearPhotoModeFilter.pak",
+["MOD_AUTHOR"]      = "Pervicacius",
+["LUA_AUTHOR"]      = "Babscoole",
+["NMS_VERSION"]     = "4.45",
+["MODIFICATIONS"]   =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"]  = "METADATA\EFFECTS\SCREENFILTERS.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {"SpaceStation", "GcScreenFilterData.xml"},
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"LocText",               "UI_BB_HOVER_CLEAR"},
+                                {"SelectableInPhotoMode", "True"}
+                            }
+                        },
+                    }
+                }
+            }
+        }
+    }
+}
