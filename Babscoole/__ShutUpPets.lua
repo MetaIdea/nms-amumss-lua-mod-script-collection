7e1eaa3bf3f9cbd4c6e58d7ba4d10560fe1f1364
Babscoole <82686995+Babscoole@users.noreply.github.com>
2024-02-08 10:19:28 -0700

    add ShutUpPets

---
+++ Babscoole/__ShutUpPets.lua
@@ -0,0 +1,40 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]  = "__ShutUpPets.pak",
+["MOD_AUTHOR"]    = "Revale",
+["LUA_AUTHOR"]    = "Babscoole",
+["NMS_VERSION"]   = "4,48",
+["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"] = "GCCREATUREGLOBALS.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"PetChatCooldown", "1800"},
+                            }
+                        },
+                    }
+                },
+                {
+                    ["MBIN_FILE_SOURCE"] = "METADATA\UI\SPECIALTEXTPUNCTUATIONDELAYDATA.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["REPLACE_TYPE"] = "ALL",
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"ChatChance", "0"},
+                            }
+                        },
+                    }
+                },
+            }
+        },
+    }
+}
