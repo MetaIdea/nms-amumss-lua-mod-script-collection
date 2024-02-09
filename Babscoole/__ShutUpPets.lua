866582e19475066266cbab62856826aada0aef01
Babscoole <82686995+Babscoole@users.noreply.github.com>
2024-02-08 17:39:38 -0700

    Update __ShutUpPets.lua

--- Babscoole/__ShutUpPets.lua
+++ Babscoole/__ShutUpPets.lua
@@ -3,7 +3,7 @@ NMS_MOD_DEFINITION_CONTAINER =
 ["MOD_FILENAME"]  = "__ShutUpPets.pak",
 ["MOD_AUTHOR"]    = "Revale",
 ["LUA_AUTHOR"]    = "Babscoole",
-["NMS_VERSION"]   = "4,48",
+["NMS_VERSION"]   = "4.48",
 ["MODIFICATIONS"] =
     {
         {
@@ -16,7 +16,9 @@ NMS_MOD_DEFINITION_CONTAINER =
                         {
                             ["VALUE_CHANGE_TABLE"] =
                             {
-                                {"PetChatCooldown", "1800"},
+                                {"PetChatCooldown",     "999999"},
+                                {"PetTickleChatChance", "0"},
+                                {"PetTreatChatChance",  "0"},
                             }
                         },
                     }
@@ -30,6 +32,43 @@ NMS_MOD_DEFINITION_CONTAINER =
                             ["VALUE_CHANGE_TABLE"] =
                             {
                                 {"ChatChance", "0"},
+                            }
+                        },
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {"Eat", "GcPetBehaviourData.xml"},
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"ReactiveBehaviour", "False"},
+                            }
+                        },
+                    }
+                },
+                {
+                    ["MBIN_FILE_SOURCE"] = "METADATA\GAMESTATE\MESSAGES\STATUSMESSAGEDEFINITIONS.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["PRECEDING_KEY_WORDS"] = {"PetVocabulary"},
+                            ["REPLACE_TYPE"] = "ALL",
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"OddsOfProcReplacement", "0"},
+                            }
+                        },
+                        {
+                            ["PRECEDING_KEY_WORDS"] = {"PetChatTemplates"},
+                            ["REPLACE_TYPE"] = "ALL",
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"OddsOfProcReplacement", "0"},
+                            }
+                        },
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {"Id", "PET_CHAT"},
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"AddPetNamePrefix", "False"},
+                                {"PostLocally",      "False"},
                             }
                         },
                     }
