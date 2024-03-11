d9702b93f61934253e5f011677b3af7414ef1827
Babscoole <82686995+Babscoole@users.noreply.github.com>
2024-03-10 21:07:07 -0600

    Lasagna updates and a few others

--- Babscoole/_MOD_Frigate_repair.lua
+++ Babscoole/_MOD_Frigate_repair.lua
@@ -3,61 +3,33 @@ NMS_MOD_DEFINITION_CONTAINER =
 ["MOD_FILENAME"]  = "_MOD_Frigate_repair.pak",
 ["MOD_AUTHOR"]    = "Democrity",
 ["LUA_AUTHOR"]    = "Babscoole",
-["NMS_VERSION"]   = "4.15",
+["NMS_VERSION"]   = "4.52",
 ["MODIFICATIONS"] =
     {
         {
             ["MBIN_CHANGE_TABLE"] =
             {
                 {
-                    ["MBIN_FILE_SOURCE"] = "MODELS\COMMON\SPACECRAFT\FRIGATES\COMMONPARTS\INTERACTIONS\CARGOBAY\ENTITIES\CARGOBAY.ENTITY.MBIN",
+                    ["MBIN_FILE_SOURCE"] = 
+                    {
+                        "MODELS\COMMON\SPACECRAFT\FRIGATES\COMMONPARTS\INTERACTIONS\CARGOBAY\ENTITIES\CARGOBAY.ENTITY.MBIN",
+                        "MODELS\COMMON\SPACECRAFT\FRIGATES\COMMONPARTS\INTERACTIONS\CARGOCONTAINER\ENTITIES\MAINTENANCEINTERACTION.ENTITY.MBIN",
+                        "MODELS\COMMON\SPACECRAFT\FRIGATES\COMMONPARTS\INTERACTIONS\ENGINEREACTOR\ENTITIES\MAINTENANCEINTERACTION.ENTITY.MBIN",
+                        "MODELS\COMMON\SPACECRAFT\FRIGATES\COMMONPARTS\INTERACTIONS\MAINBATTERY\ENTITIES\MAINTENANCEINTERACTION.ENTITY.MBIN",
+                        "MODELS\COMMON\SPACECRAFT\FRIGATES\COMMONPARTS\INTERACTIONS\MAINTENANCEPOINT\ENTITIES\MAINTENANCEPOINT.ENTITY.MBIN",
+                        "MODELS\COMMON\SPACECRAFT\FRIGATES\COMMONPARTS\INTERACTIONS\TORPEDOCONTAINER\ENTITIES\MAINTENANCEINTERACTION.ENTITY.MBIN",
+                        "MODELS\COMMON\SPACECRAFT\FRIGATES\COMMONPARTS\INTERACTIONS\TORPEDOCONTFRONTL\ENTITIES\MAINTENANCEINTERACTION.ENTITY.MBIN",
+                        "MODELS\COMMON\SPACECRAFT\FRIGATES\COMMONPARTS\INTERACTIONS\TORPEDOCONTFRONTR\ENTITIES\MAINTENANCEINTERACTION.ENTITY.MBIN",
+                        "MODELS\COMMON\SPACECRAFT\FRIGATES\COMMONPARTS\SCENES\MAINBATTERY\ENTITIES\TURRET.ENTITY.MBIN",
+                        
+                    },
                     ["EXML_CHANGE_TABLE"] =
                     {
                         {
-                            ["SPECIAL_KEY_WORDS"] = {"Id", "MAINT_FRIG8"},
+                            ["PRECEDING_KEY_WORDS"] = {"GcMaintenanceComponentData.xml"},
                             ["VALUE_CHANGE_TABLE"] =
                             {
                                 {"DamageStatus", "Repaired"},
-                            }
-                        },
-                    }
-                },
-                {
-                    ["MBIN_FILE_SOURCE"] = "MODELS\COMMON\SPACECRAFT\FRIGATES\COMMONPARTS\INTERACTIONS\CARGOCONTAINER\ENTITIES\MAINTENANCEINTERACTION.ENTITY.MBIN",
-                    ["EXML_CHANGE_TABLE"] =
-                    {
-                        {
-                            ["SPECIAL_KEY_WORDS"] = {"Id", "MAINT_FRIG7"},
-                            ["VALUE_CHANGE_TABLE"] =
-                            {
-                                {"DamageStatus",          "Repaired"},
-                                {"CompletionRequirement", "NoRequirement"},
-                            }
-                        },
-                    }
-                },
-                {
-                    ["MBIN_FILE_SOURCE"] = "MODELS\COMMON\SPACECRAFT\FRIGATES\COMMONPARTS\INTERACTIONS\ENGINEREACTOR\ENTITIES\MAINTENANCEINTERACTION.ENTITY.MBIN",
-                    ["EXML_CHANGE_TABLE"] =
-                    {
-                        {
-                            ["SPECIAL_KEY_WORDS"] = {"Id", "MAINT_FRIG7"},
-                            ["VALUE_CHANGE_TABLE"] =
-                            {
-                                {"DamageStatus", "Repaired"},
-                            }
-                        },
-                    }
-                },
-                {
-                    ["MBIN_FILE_SOURCE"] = "MODELS\COMMON\SPACECRAFT\FRIGATES\COMMONPARTS\INTERACTIONS\MAINTENANCEPOINT\ENTITIES\MAINTENANCEPOINT.ENTITY.MBIN",
-                    ["EXML_CHANGE_TABLE"] =
-                    {
-                        {
-                            ["SPECIAL_KEY_WORDS"] = {"Id", "MAINT_FRIG7"},
-                            ["VALUE_CHANGE_TABLE"] =
-                            {
-                                {"DamageStatus",          "Repaired"},
                                 {"CompletionRequirement", "NoRequirement"},
                             }
                         },
