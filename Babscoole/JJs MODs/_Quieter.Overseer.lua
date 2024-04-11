c467cd6e74842f7de0afe5abe3887ee5bcfa3dfa
Babscoole <82686995+Babscoole@users.noreply.github.com>
2024-04-11 10:10:12 -0600

    Create _Quieter.Overseer.lua

---
+++ Babscoole/JJs MODs/_Quieter.Overseer.lua
@@ -0,0 +1,42 @@
+NMS_MOD_DEFINITION_CONTAINER =
+{
+["MOD_FILENAME"]  = "_Quieter.Overseer.pak",
+["MOD_AUTHOR"]    = "JJhookah",
+["LUA_AUTHOR"]    = "Babscoole",
+["NMS_VERSION"]   = "4.64",
+["MODIFICATIONS"] =
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] =
+            {
+                {
+                    ["MBIN_FILE_SOURCE"] = "MODELS\SPACE\SPACESTATION\MODULARPARTSTYPEB\DOCK\BACK_SECTION_ORB.SCENE.MBIN",
+                    ["EXML_CHANGE_TABLE"] =
+                    {
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {"Name", "REFStationOrbEyeVFX", "Name", "SCENEGRAPH"},
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"Value", "ODELS\EFFECTS\STATION\STATIONORBEYE.SCENE.MBIN"},
+                            }
+                        },
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {"Name", "OrbData", "Name", "ATTACHMENT"},
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"Value", "ODELS\SPACE\SPACESTATION\MODULARPARTSTYPEB\DOCK\BACK_SECTION_ORB\ENTITIES\ORBDATA.ENTITY.MBIN"},
+                            }
+                        },
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {"Name", "TopVFX", "Name", "SCENEGRAPH"},
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"Value", "ODELS\EFFECTS\STATION\STATIONORBTOP.SCENE.MBIN"},
+                            }
+                        },
+                    }
+                },
+            }
+        }
+    },
+}
