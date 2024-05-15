5c2cf4b5e539157457ca8d5646df8676d46b045c
Babscoole <82686995+Babscoole@users.noreply.github.com>
2024-05-15 03:45:26 -0600

    4.65 updates

--- Babscoole/No wires - CleanESTCockpit.lua
+++ Babscoole/No wires - CleanESTCockpit.lua
@@ -10,36 +10,36 @@ NMS_MOD_DEFINITION_CONTAINER =
             ["MBIN_CT"] =
             {
                 {
-                    ["MBIN_FS"] = "MODELS\COMMON\SPACECRAFT\SENTINELSHIP\SENTINELCOCKPIT.SCENE.MBIN",
+                    ["MBIN_FS"]  = "MODELS\COMMON\SPACECRAFT\SENTINELSHIP\SENTINELCOCKPIT.SCENE.MBIN",
                     ["EXML_CT"] =
                     {
                         {
-                            ["SPECIAL_KEY_WORDS"] =
+                            ["FSKWG"] =
                             {
-                                {"Name", "SUB1Cards5"},
-                                {"Name", "MonFrontCables"},
-                                {"Name", "CableSpinnerL"},
-                                {"Name", "CableSpinnerR"},
-                                {"Name", "SentinelCableL"},
-                                {"Name", "SentinelCableR"},
-                                {"Name", "HijackCables"},
-                                {"Name", "FloorCables"},
-                                {"Name", "FloorCablesL"},
-                                {"Name", "FloorCablesR"},
-                                {"Name", "SideCable"},
-                                {"Name", "SkinnedCablesL"},
-                                {"Name", "SkinnedCablesR"},
+                                {"Name", "SUB1Cards5",},
+                                {"Name", "MonFrontCables",},
+                                {"Name", "CableSpinnerL",},
+                                {"Name", "CableSpinnerR",},
+                                {"Name", "SentinelCableL",},
+                                {"Name", "SentinelCableR",},
+                                {"Name", "HijackCables",},
+								{"Name", "FloorCables",},
+								{"Name", "FloorCablesL",},
+								{"Name", "FloorCablesR",},
+								{"Name", "SideCable",},
+								{"Name", "SkinnedCablesL",},
+								{"Name", "SkinnedCablesR",},
                             },
                             ["REMOVE"] = "SECTION",
                         },
                     }
                 },
                 {
-                    ["MBIN_FS"] = "MODELS\COMMON\SPACECRAFT\SENTINELSHIP\COCKPITCHAIR.SCENE.MBIN",
+                    ["MBIN_FS"]  = "MODELS\COMMON\SPACECRAFT\SENTINELSHIP\COCKPITCHAIR.SCENE.MBIN",
                     ["EXML_CT"] =
                     {
                         {
-                            ["SKW"] = {"Name", "dables2"},
+                            ["SKW"] = {"Name", "dables2",},
                             ["REMOVE"] = "SECTION",
                         },
                     }
--- Babscoole/Some wires -CleanERCockpit.lua
+++ Babscoole/Some wires -CleanERCockpit.lua
@@ -10,30 +10,30 @@ NMS_MOD_DEFINITION_CONTAINER =
             ["MBIN_CT"] =
             {
                 {
-                    ["MBIN_FS"] = "MODELS\COMMON\SPACECRAFT\SENTINELSHIP\SENTINELCOCKPIT.SCENE.MBIN",
+                    ["MBIN_FS"]  = "MODELS\COMMON\SPACECRAFT\SENTINELSHIP\SENTINELCOCKPIT.SCENE.MBIN",
                     ["EXML_CT"] =
                     {
                         {
-                            ["SPECIAL_KEY_WORDS"] =
+                            ["FSKWG"] =
                             {
-                                {"Name", "SUB1Cards5"},
-                                {"Name", "MonFrontCables"},
-                                {"Name", "CableSpinnerL"},
-                                {"Name", "CableSpinnerR"},
-                                {"Name", "SentinelCableL"},
-                                {"Name", "SentinelCableR"},
-                                {"Name", "HijackCables"},
+                                {"Name", "SUB1Cards5",},
+                                {"Name", "MonFrontCables",},
+                                {"Name", "CableSpinnerL",},
+                                {"Name", "CableSpinnerR",},
+                                {"Name", "SentinelCableL",},
+                                {"Name", "SentinelCableR",},
+                                {"Name", "HijackCables",},
                             },
                             ["REMOVE"] = "SECTION",
                         },
                     }
                 },
                 {
-                    ["MBIN_FS"] = "MODELS\COMMON\SPACECRAFT\SENTINELSHIP\COCKPITCHAIR.SCENE.MBIN",
+                    ["MBIN_FS"]  = "MODELS\COMMON\SPACECRAFT\SENTINELSHIP\COCKPITCHAIR.SCENE.MBIN",
                     ["EXML_CT"] =
                     {
                         {
-                            ["SKW"] = {"Name", "dables2"},
+                            ["SKW"] = {"Name", "dables2",},
                             ["REMOVE"] = "SECTION",
                         },
                     }
