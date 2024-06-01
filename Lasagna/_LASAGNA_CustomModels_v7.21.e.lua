5614732c2e026ddef716efc7f72c9dcb47f79a76
Babscoole <82686995+Babscoole@users.noreply.github.com>
2024-06-01 16:26:19 -0600

    Lasagna updates

---
+++ Lasagna/_LASAGNA_CustomModels_v7.21.e.lua
@@ -0,0 +1,13 @@
+--Dummy script - Place CustomModels folder (via. decompiled CustomModels pak) in
+--AMUMSS's GlobalMEFTI folder, this in ModScript, then run BUILDMOD
+
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+	["MOD_FILENAME"] 			= "_LASAGNA_CustomModels_v7.21.e.pak",
+	["MOD_AUTHOR"]				= "goosetehmoose",
+	["NMS_VERSION"]				= "",
+	["MODIFICATIONS"] 			= 
+	{
+	}	
+}
+
