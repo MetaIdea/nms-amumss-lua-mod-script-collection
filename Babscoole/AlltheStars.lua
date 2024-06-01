71a4cfcaf504d87daa26d654b98780c35f4223ce
Babscoole <82686995+Babscoole@users.noreply.github.com>
2024-06-01 14:38:13 -0600

    4.71 updates

---
+++ Babscoole/AlltheStars.lua
@@ -0,0 +1,29 @@
+NMS_MOD_DEFINITION_CONTAINER = 
+{
+["MOD_FILENAME"]  = "starproject.pak",
+["MOD_AUTHOR"]    = "VonBraunZ, CodenameAwesome, and darkmessenger84",
+["LUA_AUTHOR"]    = "Babscoole",
+["NMS_VERSION"]   = "4.71",
+["MODIFICATIONS"] = 
+    {
+        {
+            ["MBIN_CHANGE_TABLE"] = 
+            { 
+                {
+                    ["MBIN_FILE_SOURCE"] = "GCGALAXYGLOBALS.GLOBAL.MBIN",
+                    ["EXML_CHANGE_TABLE"] = 
+                    {
+                        {
+                            ["INTEGER_TO_FLOAT"] = "FORCE",
+                            ["REPLACE_TYPE"] = "ALL",                           
+                            ["VALUE_CHANGE_TABLE"] = 
+                            {
+                                {"StarGenerationThreshold", "0"}, -- Original "0.68"
+                            }
+                        },                  
+                    }
+                },
+            }
+        },
+    }   
+}
