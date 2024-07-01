8b7ffdf6304a39b03f404100c6396678ca73013d
Babscoole <82686995+Babscoole@users.noreply.github.com>
2024-07-01 00:06:28 -0600

    Anti-phobias updates

--- Babscoole/Anti-Ophidiophobia.lua
+++ Babscoole/Anti-Ophidiophobia.lua
@@ -1,22 +1,15 @@
-DelCreature = {"SEASNAKE","FLYINGSNAKE","SANDWORM"}
+ReplaceCreature = {"SEASNAKE","FLYINGSNAKE","SANDWORM"}
 
 NMS_MOD_DEFINITION_CONTAINER =
 {
 ["MOD_FILENAME"]  = "Anti-Ophidiophobia.pak",
 ["MOD_AUTHOR"]    = "Babscoole",
-["NMS_VERSION"]   = "4.71",
+["NMS_VERSION"]   = "4.73",
 ["MODIFICATIONS"] =
     {
         {
             ["MBIN_CHANGE_TABLE"] =
             {
-                {
-                    ["MBIN_FILE_SOURCE"] = "METADATA\SIMULATION\ECOSYSTEM\CREATUREDATATABLE.MBIN",
-                    ["EXML_CHANGE_TABLE"] =
-                    {
-
-                    },
-                },
                 {
                     ["MBIN_FILE_SOURCE"] = "METADATA\SIMULATION\ECOSYSTEM\CREATUREFILENAMETABLE.MBIN",
                     ["EXML_CHANGE_TABLE"] =
@@ -29,18 +22,16 @@ NMS_MOD_DEFINITION_CONTAINER =
     },
 }
 
-local DelCreatureTable  = NMS_MOD_DEFINITION_CONTAINER["MODIFICATIONS"][1]["MBIN_CHANGE_TABLE"][1]["EXML_CHANGE_TABLE"]
-local DelCreatureTable2 = NMS_MOD_DEFINITION_CONTAINER["MODIFICATIONS"][1]["MBIN_CHANGE_TABLE"][2]["EXML_CHANGE_TABLE"]
-for i=1,#DelCreature do
-    local value = DelCreature[i]
-    DelCreatureTable[#DelCreatureTable +1] =
-                        {
-                            ["SPECIAL_KEY_WORDS"] = {"Id", value},
-                            ["REMOVE"] = "SECTION"
-                        }
-    DelCreatureTable2[#DelCreatureTable2 +1] =
+local ReplaceCreatureTable  = NMS_MOD_DEFINITION_CONTAINER["MODIFICATIONS"][1]["MBIN_CHANGE_TABLE"][1]["EXML_CHANGE_TABLE"]
+
+for i=1,#ReplaceCreature do
+    local value = ReplaceCreature[i]
+    ReplaceCreatureTable[#ReplaceCreatureTable +1] =
                         {
                             ["SPECIAL_KEY_WORDS"] = {"ID", value},
-                            ["REMOVE"] = "SECTION"
+                            ["VALUE_CHANGE_TABLE"] =
+                            {
+                                {"Filename", "MODELS/PLANETS/CREATURES/GRUNTRIG/GRUNT.SCENE.MBIN"},
+                            }
                         }
 end
