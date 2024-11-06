670d73aa36954ac65b9d1d843d3895d952f89bcc
Babscoole <82686995+Babscoole@users.noreply.github.com>
2024-11-05 18:00:44 -0700

    5.22 uipdates

---
+++ AltF4/AltF4_UnlockableElixirTree.lua
@@ -0,0 +1,56 @@
+local ELIXIR = [[
+        <Property value="GcUnlockableItemTree.xml">
+          <Property name="Title" value="Elixir" />
+          <Property name="CostTypeID" value="NANITES" />
+          <Property name="Root" value="GcUnlockableItemTreeNode.xml">
+            <Property name="Unlockable" value="PORTAL_JUICE" />
+            <Property name="Children">
+              <Property value="GcUnlockableItemTreeNode.xml">
+                <Property name="Unlockable" value="BOSS_JUICE" />
+                <Property name="Children" />
+              </Property>
+            </Property>
+          </Property>
+        </Property>
+]]
+
+NMS_MOD_DEFINITION_CONTAINER = {
+    ["MOD_FILENAME"] = "AltF4_UnlockableElixirTree.pak",
+    ["MOD_AUTHOR"] = "AltF4",
+    ["LUA_AUTHOR"] = "AltF4",
+    ["NMS_VERSION"] = "5.22",
+    ["MOD_DESCRIPTION"] = "Add Elixir to Synthesis Laboratory unlockable item tree.",
+    ["MODIFICATIONS"] = {
+        {
+            ["MBIN_CHANGE_TABLE"] = {
+                {
+                    ["MBIN_FILE_SOURCE"] = "METADATA\REALITY\TABLES\UNLOCKABLEITEMTREES.MBIN",
+                    ["EXML_CHANGE_TABLE"] = {
+                        {
+                            ["PRECEDING_KEY_WORDS"] = {"CraftProducts","Trees"},
+                            ["ADD"] = ELIXIR,
+                        },
+                    }
+                },
+            },
+        },
+        {
+            ["MBIN_CHANGE_TABLE"] = {
+                {
+                    ["MBIN_FILE_SOURCE"] = "METADATA\REALITY\TABLES\NMS_REALITY_GCPRODUCTTABLE.MBIN",
+                    ["EXML_CHANGE_TABLE"] = {
+                        {
+                            ["SPECIAL_KEY_WORDS"] = {
+                              {"ID","PORTAL_JUICE"},
+                              {"ID","BOSS_JUICE"},
+                            },
+                            ["VALUE_CHANGE_TABLE"] = {
+                              {"RecipeCost","4"},
+                            }
+                        },
+                    }
+                },
+            },
+        },
+    }
+}
