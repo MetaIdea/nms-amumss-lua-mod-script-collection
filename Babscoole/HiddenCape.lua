2122e905fea49566af941392b0f27c5a774d8503
Babscoole <82686995+Babscoole@users.noreply.github.com>
2024-12-16 10:58:32 -0700

    DUDs update

--- Babscoole/HiddenCape.lua
+++ Babscoole/HiddenCape.lua
@@ -1,28 +1,28 @@
-NMS_MOD_DEFINITION_CONTAINER =
+NMS_MOD_DEFINITION_CONTAINER = 
 {
-["MOD_FILENAME"]  = "HiddenCape.pak",
-["MOD_AUTHOR"]    = "Wavermack",
-["LUA_AUTHOR"]    = "Babscoole",
-["NMS_VERSION"]   = "5.00",
-["MODIFICATIONS"] =
-    {
-        {
-            ["MBIN_CHANGE_TABLE"] =
-            {
-                {
-                    ["MBIN_FILE_SOURCE"] = "MODELS\COMMON\PLAYER\PLAYERCHARACTER\PLAYERCHARACTER\CLOTHTESTMAT5.MATERIAL.MBIN",
-                    ["EXML_CHANGE_TABLE"] =
-                    {
-                        {
-                            ["VALUE_CHANGE_TABLE"] =
-                            {
-                                {"Class",      "Transparent"},
-                                {"CastShadow", "False"},
-                            }
-                        },
-                    }
-                },
-            }
-        }
-    }
-}
+["MOD_FILENAME"]	= "HiddenCape.pak",
+["MOD_AUTHOR"]		= "Wavermack",
+["LUA_AUTHOR"]		= "Babscoole",
+["NMS_VERSION"]		= "5.01.1",
+["MODIFICATIONS"]	= 
+	{
+		{
+			["MBIN_CHANGE_TABLE"]	= 
+			{ 
+				{
+					["MBIN_FILE_SOURCE"]	= "MODELS\COMMON\PLAYER\PLAYERCHARACTER\PLAYERCHARACTER\CLOTHTESTMAT5.MATERIAL.MBIN",
+					["EXML_CHANGE_TABLE"]	= 
+					{
+						{
+							["VALUE_CHANGE_TABLE"]	= 
+							{	
+								{ "Class",		"Transparent" },
+								{ "CastShadow",	"False" },
+							}
+						}
+					}
+				}
+			}
+		}
+	}	
+}	
