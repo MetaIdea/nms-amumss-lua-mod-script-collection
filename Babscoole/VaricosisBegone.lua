81af54764bb32f10c4611739f8df9f48b97a2ad1
Babscoole <82686995+Babscoole@users.noreply.github.com>
2025-07-29 01:05:58 -0600

    rem

--- Babscoole/VaricosisBegone.lua
+++ Babscoole/VaricosisBegone.lua
@@ -3,7 +3,7 @@ NMS_MOD_DEFINITION_CONTAINER =
 ["MOD_FILENAME"]  = "VaricosisBegone.pak",
 ["MOD_AUTHOR"]    = "LizardSoul",
 ["LUA_AUTHOR"]    = "Babscoole",
-["NMS_VERSION"]   = "3.98",
+["NMS_VERSION"]   = "5.75",
 ["MODIFICATIONS"] = 
 	{
 		{
@@ -11,12 +11,13 @@ NMS_MOD_DEFINITION_CONTAINER =
 			{
 				{
 					["MBIN_FILE_SOURCE"] = "TEXTURES\COMMON\WEAPONS\MULTITOOL\FLESHTILING.TEXTURE.MBIN",
-					["EXML_CHANGE_TABLE"] = 
+					["MXML_CHANGE_TABLE"] = 
 					{
 						{
+              ["SPECIAL_KEY_WORDS"] = {"Name", "VEINS"},
 							["VALUE_CHANGE_TABLE"] = 
 							{
-								{"Diffuse", ""} 
+								{"TextureName", ""} 
 							}
 						},						
 					}
